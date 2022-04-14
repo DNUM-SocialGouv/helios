9df311a1c36264b6df783e32140661dd61654fe1
Thierry Gonard <goth@octo.com>
2022-04-14 19:32:48 +0200

    Refacto: met le code des composants dans le dossier src/frontend et pas dans src/pages

---
+++ src/frontend/ui/accueil/Accueil.tsx
@@ -0,0 +1,9 @@
+export const Accueil = () => {
+
+  return (
+    <>
+      <h1>Accueil</h1>
+      <p>Ceci est la page d'accueil de l'application Helios</p>
+    </>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+import { Accueil } from '../frontend/ui/accueil/Accueil'
+
+const PageDAccueil: NextPage = () => {
+  return (
+    <Accueil />
+  )
+}
+
+export default PageDAccueil
