b4e6bb9e3cbb988e6b4c03aa31ca4eea851191ca
Thierry Gonard <goth@octo.com>
2022-04-14 19:28:34 +0200

    Met à disposition du front le conteneur de dépendances grâce à un contexte global

---
+++ src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import type { AppProps } from 'next/app'
+import '@gouvfr/dsfr/dist/dsfr/dsfr.min.css'
+import Head from 'next/head'
+
+import { DependenciesProvider } from '../frontend/ui/contexts/useDependencies'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta
+          content="width=device-width, initial-scale=1"
+          name="viewport"
+        />
+      </Head>
+      <DependenciesProvider>
+        <Component {...pageProps} />
+      </DependenciesProvider>
+    </>
+  )
+}
+
+export default MyApp
