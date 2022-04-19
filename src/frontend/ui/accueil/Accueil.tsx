eff387499b758c2917e7746f3fd10712d7203bbe
Thierry Gonard <goth@octo.com>
2022-04-19 10:03:35 +0200

    Escape sequence pour les apostrophes

--- src/frontend/ui/accueil/Accueil.tsx
+++ src/frontend/ui/accueil/Accueil.tsx
@@ -3,7 +3,7 @@ export const Accueil = () => {
   return (
     <>
       <h1>Accueil</h1>
-      <p>Ceci est la page d'accueil de l'application Helios</p>
+      <p>Ceci est la page d&apos;accueil de l&apos;application Helios</p>
     </>
   )
 }
