2b32772b32487672aa337a1f88f346a74759605c
Thierry Gonard <goth@octo.com>
2022-04-26 17:55:43 +0200

    (hel-9) wip

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -2,5 +2,6 @@ import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
   dsn: process.env['SENTRY_DSN'],
+  environment: process.env['SENTRY_ENV'] || process.env['NODE_ENV'],
   tracesSampleRate: 1.0,
 })
