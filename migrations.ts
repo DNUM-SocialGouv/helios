590124af61c0ea99254496c080b58911e888f93c
Thierry Gonard <goth@octo.com>
2022-05-16 17:52:50 +0200

    (hel-19) up

--- migrations.ts
+++ migrations.ts
@@ -1,33 +1,8 @@
 import { dependencies } from './src/data-crawler/infrastructure/dependencies'
-import { typeOrmOrm } from './src/data-crawler/infrastructure/gateways/orm/typeOrmOrm'
-import { EnvironmentVariables } from './src/data-crawler/métier/gateways/EnvironmentVariables'
 
 (async () => {
   try {
-    let dataSource
-
-    if (process.env['NODE_ENV'] === 'test') {
-      const environmentVariables: EnvironmentVariables = {
-        ORM_DEBUG: 'true',
-        POSTGRES_DB: 'helios',
-        POSTGRES_PASSWORD: 'h3li0s',
-        POSTGRES_PORT: '5433',
-        POSTGRES_USER: 'helios',
-        SENTRY_AUTH_TOKEN: '1234567890',
-        SENTRY_DSN: 'https://fake-sentry.io/11',
-        SFTP_HOST: 'localhost',
-        SFTP_IS_DEBUG: 'false',
-        SFTP_LOCAL_PATH: 'data_test',
-        SFTP_PASSWORD: 'fake_passw0rd',
-        SFTP_PORT: '22',
-        SFTP_PRIVATE_KEY: 'privateKey',
-        SFTP_USERNAME: 'usr_finess_ls',
-      }
-      dataSource = await typeOrmOrm(environmentVariables)
-    } else {
-      console.log('pas test')
-      dataSource = (await dependencies).database
-    }
+    const dataSource = (await dependencies).database
     await dataSource.runMigrations()
     await dataSource.destroy()
   } catch (error) {
