e4c9f5ca1cd980a5be07832e36ed69022a514c8d
Thierry Gonard <goth@octo.com>
2022-05-19 16:54:18 +0200

    (hel-41) wip

--- src/backend/infrastructure/gateways/TypeORMEntitéJuridiqueLoader.test.ts
+++ src/backend/infrastructure/gateways/TypeORMEntitéJuridiqueLoader.test.ts
@@ -1,3 +1,6 @@
+import { Repository } from 'typeorm'
+
+import { DateMiseÀJourSourceModel, SourceDeDonnées } from '../../../database/models/DateMiseÀJourSourceModel'
 import { EntitéJuridiqueModel } from '../../../database/models/EntitéJuridiqueModel'
 import { EntitéJuridique } from '../../métier/entities/EntitéJuridique'
 import { getOrm } from '../../testHelper'
@@ -5,12 +8,24 @@ import { TypeORMEntitéJuridiqueLoader } from './TypeORMEntitéJuridiqueLoader'
 
 describe('Entité juridique loader', () => {
   const orm = getOrm()
+  let entitéJuridiqueRepository: Repository<EntitéJuridiqueModel>
+  let dateMiseÀJourSourceRepository: Repository<DateMiseÀJourSourceModel>
+
+  beforeAll(async () => {
+    entitéJuridiqueRepository = (await orm).getRepository(EntitéJuridiqueModel)
+    dateMiseÀJourSourceRepository = (await orm).getRepository(DateMiseÀJourSourceModel)
+  })
+
+  beforeEach(async () => {
+    await entitéJuridiqueRepository.query('DELETE FROM EntitéJuridique;')
+    await dateMiseÀJourSourceRepository.query('DELETE FROM DateMiseÀJourSource;')
+  })
 
   afterAll(async () => {
     await (await orm).destroy()
   })
 
-  it('charge par numéro FINESS', () => {
+  it('charge par numéro FINESS', async () => {
     // GIVEN
     const entitéJuridique = new EntitéJuridiqueModel()
     entitéJuridique.adresseAcheminement = '75000 Paris'
@@ -22,6 +37,12 @@ describe('Entité juridique loader', () => {
     entitéJuridique.raisonSociale = 'Nom de l’entité juridique'
     entitéJuridique.téléphone = '0123456789'
     await entitéJuridiqueRepository.insert(entitéJuridique)
+    await dateMiseÀJourSourceRepository.insert([
+      {
+        dernièreMiseÀJour: '20220514',
+        source: SourceDeDonnées.FINESS,
+      },
+    ])
 
     const numéroFINESS = '012345678'
     const typeORMEntitéJuridiqueLoader = new TypeORMEntitéJuridiqueLoader(orm)
@@ -30,14 +51,14 @@ describe('Entité juridique loader', () => {
       adresseNuméroVoie: '6',
       adresseTypeVoie: 'AV',
       adresseVoie: 'rue de la Paix',
-      dateMiseAJourSource: '20220514',
+      dateMiseAJourSource: '2022-05-14',
       libelléStatutJuridique: 'statut',
       numéroFinessEntitéJuridique: numéroFINESS,
       raisonSociale: 'Nom de l’entité juridique',
       téléphone: '0123456789',
     }
     // WHEN
-    const entitéJuridiqueChargée = typeORMEntitéJuridiqueLoader.chargeParNuméroFINESS(numéroFINESS)
+    const entitéJuridiqueChargée = await typeORMEntitéJuridiqueLoader.chargeParNuméroFINESS(numéroFINESS)
 
     // THEN
     expect(entitéJuridiqueChargée).toStrictEqual(entitéJuridiqueAttendue)
--- src/backend/infrastructure/gateways/TypeORMEntitéJuridiqueLoader.ts
+++ src/backend/infrastructure/gateways/TypeORMEntitéJuridiqueLoader.ts
@@ -1,5 +1,6 @@
 import { DataSource } from 'typeorm'
 
+import { DateMiseÀJourSourceModel, SourceDeDonnées } from '../../../database/models/DateMiseÀJourSourceModel'
 import { EntitéJuridiqueModel } from '../../../database/models/EntitéJuridiqueModel'
 import { EntitéJuridique } from '../../métier/entities/EntitéJuridique'
 
@@ -7,8 +8,24 @@ export class TypeORMEntitéJuridiqueLoader {
   constructor(private readonly orm: Promise<DataSource>) {}
 
   async chargeParNuméroFINESS(numéroFINESS: string): Promise<EntitéJuridique> {
-    await(await this.orm)
+    const entitéJuridiqueModel = await(await this.orm)
       .getRepository(EntitéJuridiqueModel)
       .findOneBy({ numéroFinessEntitéJuridique: numéroFINESS })
+
+    const dateDeMiseAJourModel = await(await this.orm)
+      .getRepository(DateMiseÀJourSourceModel)
+      .findOneBy({ source: SourceDeDonnées.FINESS })
+
+    return {
+      adresseAcheminement: entitéJuridiqueModel?.adresseAcheminement,
+      adresseNuméroVoie: entitéJuridiqueModel?.adresseNuméroVoie,
+      adresseTypeVoie: entitéJuridiqueModel?.adresseTypeVoie,
+      adresseVoie: entitéJuridiqueModel?.adresseVoie,
+      dateMiseAJourSource: dateDeMiseAJourModel?.dernièreMiseÀJour,
+      libelléStatutJuridique: entitéJuridiqueModel?.libelléStatutJuridique,
+      numéroFinessEntitéJuridique: entitéJuridiqueModel?.numéroFinessEntitéJuridique,
+      raisonSociale: entitéJuridiqueModel?.raisonSociale,
+      téléphone: entitéJuridiqueModel?.téléphone,
+    }
   }
 }
