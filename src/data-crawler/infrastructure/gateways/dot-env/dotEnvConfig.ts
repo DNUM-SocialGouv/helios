3fce2209f4a8efc04034cdfdb86b597c24490eda
Fabien Mercier <fabien.mercier@octo.com>
2022-05-20 10:47:28 +0200

    wip

--- src/data-crawler/infrastructure/gateways/dot-env/dotEnvConfig.ts
+++ src/data-crawler/infrastructure/gateways/dot-env/dotEnvConfig.ts
@@ -1,6 +1,14 @@
 import dotenv from 'dotenv-defaults'
 
 export const dotEnvConfig = () => {
+  if (process.env.NODE_ENV === 'test') {
+    dotenv.config({
+      defaults: './.env',
+      encoding: 'utf8',
+      path: './.env.test',
+    })
+  }
+
   if (process.env.NODE_ENV !== 'production') {
     dotenv.config({
       defaults: './.env',
