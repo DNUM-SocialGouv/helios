8fe87242bf11a30e3c6e099e74b96874e3c4a550
Sofia Calcagno <10255447+sofcalca@users.noreply.github.com>
2022-05-20 11:27:00 +0200

    (hel-41) wip 2022-05-20T11:27:00

--- src/backend/métier/use-cases/RécupèreLEntitéJuridiqueUseCase.test.ts
+++ src/backend/métier/use-cases/RécupèreLEntitéJuridiqueUseCase.test.ts
@@ -31,4 +31,33 @@ describe('La récupération d’une entité juridique', () => {
     expect(mockedChargeParNuméroFINESS).toHaveBeenCalledWith(numéroFINESS)
     expect(mockedChargeParNuméroFINESS).toHaveBeenCalledTimes(1)
   })
+
+  it('récupère la fiche identité de l’entité juridique', async () => {
+    // GIVEN
+    const numéroFINESS = '123456789'
+    const entitéJuridique: EntitéJuridique = {
+      adresseAcheminement: '75000 Paris',
+      adresseNuméroVoie: '6',
+      adresseTypeVoie: 'AV',
+      adresseVoie: 'rue de la Paix',
+      dateMiseAJourSource: '20220514',
+      libelléStatutJuridique: 'statut',
+      numéroFinessEntitéJuridique: numéroFINESS,
+      raisonSociale: 'Nom de l’entité juridique',
+      téléphone: '0123456789',
+    }
+    const mockedChargeParNuméroFINESS = jest.fn(async () => {
+      return entitéJuridique
+    })
+    const établissementJuridiqueLoader: EntitéJuridiqueLoader = { chargeParNuméroFINESS: mockedChargeParNuméroFINESS }
+    const récupèreLEntitéJuridiqueUseCase = new RécupèreLEntitéJuridiqueUseCase(établissementJuridiqueLoader)
+
+    // WHEN
+    const ficheIdentitéRécupérée = await récupèreLEntitéJuridiqueUseCase.exécute(numéroFINESS)
+
+    // THEN
+    expect(ficheIdentitéRécupérée).toStrictEqual(entitéJuridique)
+    expect(mockedChargeParNuméroFINESS).toHaveBeenCalledWith(numéroFINESS)
+    expect(mockedChargeParNuméroFINESS).toHaveBeenCalledTimes(1)
+  })
 })
--- src/data-crawler/infrastructure/gateways/entité-juridique-loader/FinessXMLEntitésJuridiquesLoader.test.ts
+++ src/data-crawler/infrastructure/gateways/entité-juridique-loader/FinessXMLEntitésJuridiquesLoader.test.ts
@@ -128,13 +128,13 @@ describe('Récupération des entités juridiques de la source de données FINESS
     )
   })
 
-  it('renseigner N/A lorsque la valeur d’un champ n’est pas renseignée', () => {
+  it('renseigner une string vide lorsque la valeur d’un champ n’est pas renseignée', () => {
     // GIVEN
     const xml = `<?xml version="1.0" encoding="UTF-8"?>
     <fluxfiness xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
       <structureej>
         <nofiness>010008407</nofiness>
-        <rs>CH DU HAUT BUGEY</rs>
+        <rs xsi:nil="true"/>
         <rslongue>CENTRE HOSPITALIER DU HAUT BUGEY</rslongue>
         <complrs xsi:nil="true"/>
         <numvoie>1</numvoie>
@@ -151,7 +151,7 @@ describe('Récupération des entités juridiques de la source de données FINESS
         <libdepartement>AIN</libdepartement>
         <codepostal>01117</codepostal>
         <codepays xsi:nil="true"/>
-        <telephone>0474731001</telephone>
+        <telephone></telephone>
         <telecopie>0474731002</telecopie>
         <statutjuridique>14</statutjuridique>
         <libstatutjuridique>Etablissement Public Intercommunal d'Hospitalisation</libstatutjuridique>
@@ -165,6 +165,43 @@ describe('Récupération des entités juridiques de la source de données FINESS
         <codeape>8730A</codeape>
         <datecrea>2001-01-01</datecrea>
         <datemaj>2020-02-04</datemaj>
+        <datefermeture xsi:nil="true"/>
+        <typefermeture xsi:nil="true"/>
+        <qualifcreation>GEN</qualifcreation>
+      </structureej>
+      <structureej>
+        <nofiness>590000741</nofiness>
+        <rs>HOPITAL PRIVE DE VILLENEUVE D'ASCQ</rs>
+        <rslongue>HOPITAL PRIVE DE VILLENEUVE D'ASCQ</rslongue>
+        <complrs xsi:nil="true"/>
+        <numvoie>20</numvoie>
+        <typvoie>AV</typvoie>
+        <voie>DE LA RECONNAISSANCE</voie>
+        <compvoie xsi:nil="true"/>
+        <compldistrib xsi:nil="true"/>
+        <lieuditbp>QUARTIER DU RECUEIL</lieuditbp>
+        <libellepays xsi:nil="true"/>
+        <commune>009</commune>
+        <ligneacheminement>59650 VILLENEUVE D ASCQ</ligneacheminement>
+        <libcommune>VILLENEUVE D ASCQ</libcommune>
+        <departement>59</departement>
+        <libdepartement>NORD</libdepartement>
+        <codepostal>59650</codepostal>
+        <codepays xsi:nil="true"/>
+        <telephone>0826666900</telephone>
+        <telecopie>0320995678</telecopie>
+        <statutjuridique>73</statutjuridique>
+        <libstatutjuridique>Société Anonyme (S.A.)</libstatutjuridique>
+        <libcourtstatutjuridique>Société Anonyme</libcourtstatutjuridique>
+        <categetab xsi:nil="true"/>
+        <libcategetab xsi:nil="true"/>
+        <libcourtcategetab xsi:nil="true"/>
+        <siren>476780333</siren>
+        <datemodifsiren>2012-09-26</datemodifsiren>
+        <originemodifsiren>SIRETISATION</originemodifsiren>
+        <codeape>8610Z</codeape>
+        <datecrea>2001-01-01</datecrea>
+        <datemaj>2012-09-14</datemaj>
         <datefermeture xsi:nil="true"/>
         <typefermeture xsi:nil="true"/>
         <qualifcreation>GEN</qualifcreation>
@@ -187,8 +224,19 @@ describe('Récupération des entités juridiques de la source de données FINESS
           dateMiseAJourSource: '20211214',
           libelléStatutJuridique: "Etablissement Public Intercommunal d'Hospitalisation",
           numéroFinessEntitéJuridique: '010008407',
-          raisonSociale: 'N/A',
-          téléphone: 'N/A',
+          raisonSociale: '',
+          téléphone: '',
+        },
+        {
+          adresseAcheminement: '59650 VILLENEUVE D ASCQ',
+          adresseNuméroVoie: '20',
+          adresseTypeVoie: 'AV',
+          adresseVoie: 'DE LA RECONNAISSANCE',
+          dateMiseAJourSource: '20211214',
+          libelléStatutJuridique: 'Société Anonyme (S.A.)',
+          numéroFinessEntitéJuridique: '590000741',
+          raisonSociale: "HOPITAL PRIVE DE VILLENEUVE D'ASCQ",
+          téléphone: '0826666900',
         },
       ]
     )
