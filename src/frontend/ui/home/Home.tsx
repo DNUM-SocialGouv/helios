d486d3db6ae7ef223ba3b577a47cb0142d7ba59e
Sofia Calcagno <10255447+sofcalca@users.noreply.github.com>
2022-05-24 17:40:35 +0200

    (hel-41) wip 2022-05-24T17:40:35

--- src/frontend/ui/home/Home.tsx
+++ src/frontend/ui/home/Home.tsx
@@ -1,33 +1,9 @@
-import Link from 'next/link'
-
-import { useDependencies } from '../commun/contexts/useDependencies'
-
 export const Home = () => {
-  const { paths } = useDependencies()
 
   return (
     <div className="fr-mt-5w">
       <h1>Accueil</h1>
       <p>Ceci est la page d’accueil de l’application Helios</p>
-      <p>
-        <Link
-          href={paths.ENTITÉ_JURIDIQUE + '/010008407'}
-          passHref
-        >
-          <a>
-            Lien vers CH DU HAUT BUGEY
-          </a>
-        </Link>
-        <br />
-        <Link
-          href={paths.ENTITÉ_JURIDIQUE + '/590000741'}
-          passHref
-        >
-          <a>
-            Lien vers HOPITAL PRIVE DE VILLENEUVE D’ASCQ
-          </a>
-        </Link>
-      </p>
     </div>
   )
 }
