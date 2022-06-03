32cbf8cecb1598a4ded1f4b93910b5c677ee775d
Thierry Gonard <goth@octo.com>
2022-06-03 14:47:11 +0200

    (hel-27) [WIP] Commit by Thierry on 2022-06-03T14:47:11

--- src/frontend/ui/home/Home.test.tsx
+++ src/frontend/ui/home/Home.test.tsx
@@ -22,11 +22,27 @@ describe('page accueil', () => {
     // THEN
     const entitésJuridique = screen.getByRole('region', { name: wording.TITRE_LISTE_DES_ENTITÉS_JURIDIQUES })
     const listeDesEntitésJuridiques = within(entitésJuridique).getAllByRole('listitem')
-    expect(listeDesEntitésJuridiques).toHaveLength(1)
+    expect(listeDesEntitésJuridiques).toHaveLength(5)
     const entitéJuridique1 = within(listeDesEntitésJuridiques[0]).getByRole('link')
     expect(entitéJuridique1).toHaveAttribute('href', `${paths.ENTITÉ_JURIDIQUE}/750050759`)
     expect(within(entitéJuridique1).getByText('- 750 050 759 - CANSSM FILIERIS')).toBeInTheDocument()
-  //  const abréviationÉtablissementTerritorial = within(entitéJuridique1).getByText('ET', { selector: 'abbr' })
-  //  expect(abréviationÉtablissementTerritorial).toHaveAttribute('title', 'Établissement territorial')
+    const abréviationEntitéJuridique = within(entitéJuridique1).getByText('EJ', { selector: 'abbr' })
+    expect(abréviationEntitéJuridique).toHaveAttribute('title', wording.ENTITÉ_JURIDIQUE)
+
+    const entitéJuridique2 = within(listeDesEntitésJuridiques[1]).getByRole('link')
+    expect(entitéJuridique2).toHaveAttribute('href', `${paths.ENTITÉ_JURIDIQUE}/750060758`)
+    expect(within(entitéJuridique2).getByText('- 750 060 758 - SARL DOMITYS NORD OUEST')).toBeInTheDocument()
+
+    const entitéJuridique3 = within(listeDesEntitésJuridiques[2]).getByRole('link')
+    expect(entitéJuridique3).toHaveAttribute('href', `${paths.ENTITÉ_JURIDIQUE}/010000289`)
+    expect(within(entitéJuridique3).getByText('- 010 000 289 - MAISON DE REPOS VIEUX MOULIN')).toBeInTheDocument()
+
+    const entitéJuridique4 = within(listeDesEntitésJuridiques[3]).getByRole('link')
+    expect(entitéJuridique4).toHaveAttribute('href', `${paths.ENTITÉ_JURIDIQUE}/750721334`)
+    expect(within(entitéJuridique4).getByText('- 750 721 334 - CROIX ROUGE FRANCAISE')).toBeInTheDocument()
+
+    const entitéJuridique5 = within(listeDesEntitésJuridiques[4]).getByRole('link')
+    expect(entitéJuridique5).toHaveAttribute('href', `${paths.ENTITÉ_JURIDIQUE}/240015842`)
+    expect(within(entitéJuridique5).getByText('- 240 015 842 - COMMUNAUTE DE COMMUNES DU PAYS VERNOIS')).toBeInTheDocument()
   })
 })
--- src/frontend/ui/home/Home.tsx
+++ src/frontend/ui/home/Home.tsx
@@ -3,17 +3,50 @@ import { ReactElement } from 'react'
 
 import { useDependencies } from '../commun/contexts/useDependencies'
 import { ListItem } from '../commun/ListItem/ListItem'
+import styles from './Home.module.css'
 import LogoEntitéJuridiqueNoir from './logo-entité-juridique-noir.svg'
 
 export const Home = () => {
-
   const { paths, wording } = useDependencies()
+
   const entitésJuridiques: {identifiant: ReactElement, numéroFiness: string}[] = [
     {
-      identifiant: <>EJ - 750 050 759 - CANSSM FILIERIS </>,
+      identifiant: <>
+        <abbr title={wording.ENTITÉ_JURIDIQUE}>EJ</abbr>
+        &nbsp;- 750 050 759 - CANSSM FILIERIS
+      </>,
       numéroFiness: '750050759',
     },
+    {
+      identifiant: <>
+        <abbr title={wording.ENTITÉ_JURIDIQUE}>EJ</abbr>
+        &nbsp;- 750 060 758 - SARL DOMITYS NORD OUEST
+      </>,
+      numéroFiness: '750060758',
+    },
+    {
+      identifiant: <>
+        <abbr title={wording.ENTITÉ_JURIDIQUE}>EJ</abbr>
+        &nbsp;- 010 000 289 - MAISON DE REPOS VIEUX MOULIN
+      </>,
+      numéroFiness: '010000289',
+    },
+    {
+      identifiant: <>
+        <abbr title={wording.ENTITÉ_JURIDIQUE}>EJ</abbr>
+        &nbsp;- 750 721 334 - CROIX ROUGE FRANCAISE
+      </>,
+      numéroFiness: '750721334',
+    },
+    {
+      identifiant: <>
+        <abbr title={wording.ENTITÉ_JURIDIQUE}>EJ</abbr>
+        &nbsp;- 240 015 842 - COMMUNAUTE DE COMMUNES DU PAYS VERNOIS
+      </>,
+      numéroFiness: '240015842',
+    },
   ]
+
   return (
     <section
       aria-label={wording.TITRE_LISTE_DES_ENTITÉS_JURIDIQUES}
@@ -22,7 +55,7 @@ export const Home = () => {
       <h3>
         {wording.ENTITÉS_JURIDIQUES}
       </h3>
-      <ol className="fr-raw-list fr-text--bold fr-raw-link">
+      <ol className={ styles['liste-entités-juridiques'] + ' fr-raw-list fr-text--bold fr-raw-link'}>
         {
           entitésJuridiques.map((entitéJuridique) =>
             <ListItem
@@ -40,7 +73,6 @@ export const Home = () => {
             />)
         }
       </ol>
-
     </section>
   )
 }
