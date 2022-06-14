973d2c846d45fab59a06a59ea96c489a3d619ccd
Mehdi <mehdi-etude@hotmail.com>
2022-06-14 16:33:46 +0200

    (hel-50) hello

--- lighthouserc.js
+++ lighthouserc.js
@@ -6,7 +6,10 @@ module.exports = {
         'categories:performance': ['warn', { minScore: 1 }],
       },
     },
-    collect: { numberOfRuns: 5 },
+    collect: {
+      startServerCommand: 'yarn build && yarn start',
+      url: ['http://localhost:3000'],
+    },
     upload: { target: 'temporary-public-storage' },
   },
 }
