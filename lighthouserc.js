e499493eb3ecff5623b49e4e47653e679726598a
Sofia Calcagno <10255447+sofcalca@users.noreply.github.com>
2022-06-15 12:00:30 +0200

    (hel-50) wip

--- lighthouserc.js
+++ lighthouserc.js
@@ -6,7 +6,22 @@ module.exports = {
         'categories:performance': ['warn', { minScore: 1 }],
       },
     },
-    collect: { url: ['http://localhost:3000/'] },
-    upload: { target: 'temporary-public-storage' },
+    collect: {
+      emulatedFormFactor: 'desktop',
+      url: [
+        'http://localhost:3000/',
+        'http://localhost:3000/entite-juridique/010008407',
+        'http://localhost:3000/etablissement-territorial-medico-social/010786036',
+        'http://localhost:3000/plan-du-site',
+        'http://localhost:3000/accessibilite',
+        'http://localhost:3000/mentions-legales',
+        'http://localhost:3000/donnees-personnelles',
+        'http://localhost:3000/gestion-des-cookies',
+      ],
+    },
+    upload: {
+      outputDir: 'lighthouse-ci-reports',
+      target: 'filesystem',
+    },
   },
 }
