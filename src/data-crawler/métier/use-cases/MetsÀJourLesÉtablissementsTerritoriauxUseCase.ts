fe2d67a1df51f8f207ee88d9177389e30b87b1d2
Thierry Gonard <goth@octo.com>
2022-06-17 10:09:08 +0200

    log de perfs

--- src/data-crawler/métier/use-cases/MetsÀJourLesÉtablissementsTerritoriauxUseCase.ts
+++ src/data-crawler/métier/use-cases/MetsÀJourLesÉtablissementsTerritoriauxUseCase.ts
@@ -15,18 +15,36 @@ export class MetsÀJourLesÉtablissementsTerritoriauxUseCase {
 
   async exécute(): Promise<void> {
     try {
+      const t0 = performance.now()
+
       const numéroFinessDesEntitésJuridiques = await this.entitéJuridiqueHeliosLoader.récupèreLeNuméroFinessDesEntitésJuridiques()
+      const t1 = performance.now()
+      console.log(`Temps de récupération des numéros finess des entités juridiques en ${t1 - t0}ms`)
+
       const établissementsTerritoriauxOuverts =
         await this.établissementTerritorialSourceExterneLoader.récupèreLesÉtablissementsTerritoriauxOuverts(numéroFinessDesEntitésJuridiques)
+      const t2 = performance.now()
+      console.log(`Temps de récupération des établissements territoriaux ouverts en ${t2 - t1}ms`)
+
       const établissementsTerritoriauxSauvegardés = await this.établissementTerritorialHeliosLoader.récupèreLeNuméroFinessDesÉtablissementsTerritoriaux()
+      const t3 = performance.now()
+      console.log(`Temps de récupération des numéros finess des établissements territoriaux sauvegardés en ${t3 - t2}ms`)
 
       const établissementsTerritoriauxÀSupprimer = this.extraisLesÉtablissementsTerritoriauxRécemmentFermés(
         établissementsTerritoriauxOuverts,
         établissementsTerritoriauxSauvegardés
       )
+      const t4 = performance.now()
+      console.log(`Temps de extraction des établissements territoriaux à supprimer en ${t4 - t3}ms`)
+
       await this.établissementTerritorialHeliosRepository.supprime(établissementsTerritoriauxÀSupprimer)
+      const t5 = performance.now()
+      console.log(`Temps de suppression des établissements territoriaux à supprimer en ${t5 - t4}ms`)
 
       await this.établissementTerritorialHeliosRepository.sauvegarde(établissementsTerritoriauxOuverts)
+      const t6 = performance.now()
+      console.log(`Temps de sauvegarde des établissements territoriaux ouverts en ${t6 - t5}ms`)
+
     } catch (error) {
       throw new HeliosError(error.message)
     }
--- src/data-crawler/métier/use-cases/TéléchargeLesDonnéesBrutesDeFinessUseCase.ts
+++ src/data-crawler/métier/use-cases/TéléchargeLesDonnéesBrutesDeFinessUseCase.ts
@@ -12,17 +12,22 @@ export class TéléchargeLesDonnéesBrutesDeFinessUseCase {
     const rawData = 'FINESS'
     const sftpPath = '../usr_finess/flux_finess'
     const finessLocalPath = 'finess'
-
+    const t0 = performance.now()
     try {
       await this.downloadDataSource.exécute(rawData, sftpPath, finessLocalPath)
     } catch (error) {
       throw new HeliosError(error.message)
     }
+    const t1 = performance.now()
+    console.log(`Temps de téléchargement des données brutes de FINESS: ${t1 - t0}ms`)
 
     try {
       await this.unzipRawData.exécute(rawData, finessLocalPath)
     } catch (error) {
       throw new HeliosError(error.message)
     }
+    const t2 = performance.now()
+    console.log(`Temps de décompression des données brutes de FINESS: ${t2 - t1}ms`)
+
   }
 }
