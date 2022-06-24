7bfbd1579a2604558f93d80b45822125d849901d
Sofia Calcagno <10255447+sofcalca@users.noreply.github.com>
2022-06-24 13:42:41 +0200

    (hel-79) supprimer les tests ne correspondant pas au ticket

--- data-crawler/tests/acceptances.py
+++
@@ -1,9 +0,0 @@
-class DataCrawlerTest:
-    def test_sauvegarde_les_données_de_ann_errd_ej_et(self):
-        # GIVEN
-        chemin_vers_les_fichiers = 'données'
-
-        # WHEN
-        crée_la_table_activité_médico_social(chemin_vers_les_fichiers)
-
-        # THEN
