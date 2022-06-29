75fa30e4ca4211599e4a0edf80b75bd2235c7039
Thierry Gonard <goth@octo.com>
2022-06-29 14:10:06 +0200

    (hel-107) [WIP] Commit by Thierry on 2022-06-29T14:10:06

---
+++ datacrawler/ajoute_les_activités_des_établissements_médico_sociaux.py
@@ -0,0 +1,18 @@
+from functools import partial
+
+from sqlalchemy.engine import Engine
+
+from datacrawler.extract.lecteur_csv import lis_le_fichier_csv
+from datacrawler.extract.lecteur_sql import récupère_les_numéros_finess_des_établissements_de_la_base
+from datacrawler.load.sauvegarde_les_activités_des_établissements_médico_sociaux import sauvegarde_les_activités_des_établissements_médico_sociaux
+from datacrawler.transform.transforme_les_activités_des_établissements_médico_sociaux import transforme_les_activités_des_établissements_médico_sociaux
+
+
+def ajoute_les_activités_des_établissements_médico_sociaux(chemin_du_fichier: str, base_de_données: Engine):
+    récupère_les_numéros_finess_des_établissements_connus = partial(récupère_les_numéros_finess_des_établissements_de_la_base, base_de_données=base_de_données)
+
+    activités_des_établissements_médico_sociaux = transforme_les_activités_des_établissements_médico_sociaux(
+        chemin_du_fichier, récupère_les_numéros_finess_des_établissements_connus, lis_le_fichier_csv
+    )
+
+    sauvegarde_les_activités_des_établissements_médico_sociaux(base_de_données, activités_des_établissements_médico_sociaux)
