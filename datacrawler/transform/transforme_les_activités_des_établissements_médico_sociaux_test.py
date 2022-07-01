7bca303c498168bee463dc692dfaae0df7345741
Sofia Calcagno <10255447+sofcalca@users.noreply.github.com>
2022-07-01 13:35:09 +0200

    (hel-107) wip 2022-07-01T13:35:08

--- datacrawler/transform/transforme_les_activités_des_établissements_médico_sociaux_test.py
+++ datacrawler/transform/transforme_les_activités_des_établissements_médico_sociaux_test.py
@@ -100,6 +100,7 @@ class TestTransformeLesActivitésDesÉtablissementsMédicoSociaux:
             )
                 .set_index(index_des_activités_médico_sociales)
         )
+        # typecheck ignoré. Voir https://github.com/pandas-dev/pandas-stubs/issues/56
         pd.testing.assert_frame_equal(données_transformées, data_frame_attendu, check_index_type=False)  # type: ignore
 
     def test_supprime_les_lignes_ne_mentionnant_pas_l_année(self) -> None:
@@ -152,6 +153,7 @@ class TestTransformeLesActivitésDesÉtablissementsMédicoSociaux:
             )
                 .set_index(index_des_activités_médico_sociales)
         )
+        # typecheck ignoré. Voir https://github.com/pandas-dev/pandas-stubs/issues/56
         pd.testing.assert_frame_equal(données_transformées, data_frame_attendu, check_index_type=False)  # type: ignore
 
     def test_renseigne_la_ligne_même_si_aucun_taux_n_est_renseigné(self) -> None:
@@ -194,6 +196,7 @@ class TestTransformeLesActivitésDesÉtablissementsMédicoSociaux:
                 }
             ],
         ).set_index(index_des_activités_médico_sociales)
+        # typecheck ignoré. Voir https://github.com/pandas-dev/pandas-stubs/issues/56
         pd.testing.assert_frame_equal(données_transformées, data_frame_attendu, check_index_type=False)  # type: ignore
 
     def test_ne_considère_qu_une_seule_fois_un_même_couple_année_numéro_finess(self) -> None:
@@ -243,6 +246,7 @@ class TestTransformeLesActivitésDesÉtablissementsMédicoSociaux:
                 }
             ],
         ).set_index(index_des_activités_médico_sociales)
+        # typecheck ignoré. Voir https://github.com/pandas-dev/pandas-stubs/issues/56
         pd.testing.assert_frame_equal(données_transformées, data_frame_attendu, check_index_type=False)  # type: ignore
 
     def test_ne_renvoie_pas_les_établissements_non_présents_en_base(self) -> None:
