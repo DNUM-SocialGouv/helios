b4a36d718f34f13c3b753cfe03114e9082447eb8
Fabien Mercier <fabien.mercier@octo.com>
2022-07-07 14:33:11 +0200

    (hel-127) ajouter des ET d'exemple sur la home

--- src/frontend/ui/home/Home.tsx
+++ src/frontend/ui/home/Home.tsx
@@ -13,35 +13,35 @@ export const Home = () => {
     {
       identifiant: <>
         <abbr title={wording.ENTITÉ_JURIDIQUE}>EJ</abbr>
-        &nbsp;- 750 050 759 - CANSSM FILIERIS
+        &nbsp;- 750 050 759 - CANSSM FILIERIS (avec que des ET sanitaires)
       </>,
       numéroFiness: '750050759',
     },
     {
       identifiant: <>
         <abbr title={wording.ENTITÉ_JURIDIQUE}>EJ</abbr>
-        &nbsp;- 750 060 758 - SARL DOMITYS NORD OUEST
+        &nbsp;- 750 060 758 - SARL DOMITYS NORD OUEST (avec que des ET médico-sociaux)
       </>,
       numéroFiness: '750060758',
     },
     {
       identifiant: <>
         <abbr title={wording.ENTITÉ_JURIDIQUE}>EJ</abbr>
-        &nbsp;- 500 020 839 - GCSMS &quot;PRESQU&apos;ÎLE&quot;
+        &nbsp;- 500 020 839 - GCSMS &quot;PRESQU&apos;ÎLE&quot; (sans ET)
       </>,
       numéroFiness: '500020839',
     },
     {
       identifiant: <>
         <abbr title={wording.ENTITÉ_JURIDIQUE}>EJ</abbr>
-        &nbsp;- 750 721 334 - CROIX ROUGE FRANCAISE
+        &nbsp;- 750 721 334 - CROIX ROUGE FRANCAISE (avec beaucoup d’ET)
       </>,
       numéroFiness: '750721334',
     },
     {
       identifiant: <>
         <abbr title={wording.ENTITÉ_JURIDIQUE}>EJ</abbr>
-        &nbsp;- 210 012 142 - CTRE HOSPITALIER DE LA HAUTE COTE D OR
+        &nbsp;- 210 012 142 - CTRE HOSPITALIER DE LA HAUTE COTE D OR (avec un nom d’établissement très long)
       </>,
       numéroFiness: '210012142',
     },
