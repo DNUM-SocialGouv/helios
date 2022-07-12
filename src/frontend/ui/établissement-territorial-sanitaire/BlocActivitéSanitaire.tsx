5996f12c4f469814617e47dd5235635de04eb60d
Mehdi <mehdi-etude@hotmail.com>
2022-07-12 12:00:24 +0200

    (hel-116) hello

--- src/frontend/ui/établissement-territorial-sanitaire/BlocActivitéSanitaire.tsx
+++ src/frontend/ui/établissement-territorial-sanitaire/BlocActivitéSanitaire.tsx
@@ -1,6 +1,7 @@
 import { Bloc } from '../commun/Bloc/Bloc'
 import { useDependencies } from '../commun/contexts/useDependencies'
 import { IndicateurGraphique } from '../commun/IndicateurGraphique/IndicateurGraphique'
+import { ContenuNombreDeSéjourMCO } from './InfoBulle/ContenuNombreDeSéjourMCO'
 import { ÉtablissementTerritorialSanitaireViewModel } from './ÉtablissementTerritorialSanitaireViewModel'
 
 type BlocActivitéSanitaireType = {
@@ -14,7 +15,10 @@ export const BlocActivitéSanitaire = ({ établissementTerritorialSanitaireViewM
     <Bloc titre={wording.TITRE_BLOC_ACTIVITÉ}>
       <ul>
         <IndicateurGraphique
-          contenuInfoBulle={<></>}
+          contenuInfoBulle={<ContenuNombreDeSéjourMCO
+            dateDeMiseÀJour={établissementTerritorialSanitaireViewModel.dateDeMiseÀJour}
+            source={wording.DIAMANT}
+          />}
           dateDeMiseÀJour={établissementTerritorialSanitaireViewModel.dateDeMiseÀJour}
           identifiant="activite-0"
           nomDeLIndicateur={wording.NOMBRE_DE_SÉJOUR_MCO}
--- src/frontend/ui/établissement-territorial-sanitaire/PageÉtablissementTerritorialSanitaireActivité.test.tsx
+++ src/frontend/ui/établissement-territorial-sanitaire/PageÉtablissementTerritorialSanitaireActivité.test.tsx
@@ -1,4 +1,4 @@
-import {screen, within} from '@testing-library/react'
+import { fireEvent, screen, within } from '@testing-library/react'
 
 import { ÉtablissementTerritorialSanitaireViewModelTestBuilder } from '../../test-builder/ÉtablissementTerritorialSanitaireViewModelTestBuilder'
 import { fakeFrontDependencies, renderFakeComponent } from '../../testHelper'
@@ -21,5 +21,52 @@ describe('La page Établissement territorial Sanitaire - Bloc activité', () => 
     expect(dateMiseAJour[0]).toBeInTheDocument()
     const abréviation = within(indicateurs[0]).getAllByText('DIAMANT', { selector: 'abbr' })
     expect(abréviation[0]).toHaveAttribute('title', 'Décisionnel Inter ARS pour la Maîtrise et ANTicipation')
+    const détails = within(indicateurs[0]).getByRole('button', { name: wording.DÉTAILS })
+    expect(détails).toHaveAttribute('aria-controls', 'nom-info-bulle-activite-0')
+    expect(détails).toHaveAttribute('data-fr-opened', 'false')
+  })
+
+  it('affiche le contenu de l’info bulle après avoir cliqué sur le bouton "détails"', () => {
+    // GIVEN
+    renderFakeComponent(<PageÉtablissementTerritorialSanitaire établissementTerritorialViewModel={établissementTerritorialSanitaire} />)
+    const activité = screen.getByRole('region', { name: wording.TITRE_BLOC_ACTIVITÉ })
+    const indicateurs = within(activité).getAllByRole('listitem')
+    const détails = within(indicateurs[0]).getByRole('button', { name: wording.DÉTAILS })
+
+    // WHEN
+    fireEvent.click(détails)
+
+    // THEN
+    expect(détails).toHaveAttribute('data-fr-opened', 'true')
+    const infoBulle = screen.getByRole('dialog', { name: wording.NOMBRE_DE_SÉJOUR_MCO })
+    const fermer = within(infoBulle).getByRole('button', { name: wording.FERMER })
+    expect(fermer).toBeInTheDocument()
+    const élémentsDeCompréhension = within(infoBulle).getByRole('region', { name: wording.ÉLÉMENTS_DE_COMPRÉHENSION })
+    expect(élémentsDeCompréhension).toBeInTheDocument()
+    const fréquence = within(infoBulle).getByRole('region', { name: wording.FRÉQUENCE })
+    expect(fréquence).toBeInTheDocument()
+    const modeDeCalcul = within(infoBulle).getByRole('region', { name: wording.MODE_DE_CALCUL })
+    expect(modeDeCalcul).toBeInTheDocument()
+    const sources = within(infoBulle).getByRole('region', { name: wording.SOURCES })
+    expect(sources).toBeInTheDocument()
+    const informationsComplémentaires = within(infoBulle).getByRole('region', { name: wording.INFOS_COMPLÉMENTAIRES })
+    expect(informationsComplémentaires).toBeInTheDocument()
+  })
+
+  it('ferme l’info bulle après avoir cliqué sur le bouton "Fermer"', () => {
+    // GIVEN
+    renderFakeComponent(<PageÉtablissementTerritorialSanitaire établissementTerritorialViewModel={établissementTerritorialSanitaire} />)
+    const activité = screen.getByRole('region', { name: wording.TITRE_BLOC_ACTIVITÉ })
+    const indicateurs = within(activité).getAllByRole('listitem')
+    const détails = within(indicateurs[0]).getByRole('button', { name: wording.DÉTAILS })
+    fireEvent.click(détails)
+    const infoBulle = screen.getByRole('dialog', { name: wording.NOMBRE_DE_SÉJOUR_MCO })
+    const fermer = within(infoBulle).getByRole('button', { name: wording.FERMER })
+
+    // WHEN
+    fireEvent.click(fermer)
+
+    // THEN
+    expect(détails).toHaveAttribute('data-fr-opened', 'false')
   })
 })
