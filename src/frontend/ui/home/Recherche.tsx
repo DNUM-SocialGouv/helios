ef30da54a682b9058fc88bbd3193abc1600ef15e
Fabien Mercier <fabien.mercier@octo.com>
2022-07-22 10:59:59 +0200

    (hel-130) wip

--- src/frontend/ui/home/Recherche.tsx
+++ src/frontend/ui/home/Recherche.tsx
@@ -1,54 +1,54 @@
 import Image from 'next/image'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import { useDependencies } from '../commun/contexts/useDependencies'
 import '@gouvfr/dsfr/dist/component/button/button.min.css'
 import '@gouvfr/dsfr/dist/component/input/input.min.css'
 import '@gouvfr/dsfr/dist/component/search/search.min.css'
 import '@gouvfr/dsfr/dist/component/tile/tile.min.css'
-import LogoÉtablissementTerritorialMédicoSocial from '../entité-juridique/liste-des-établissements/logo-établissement-territorial-médico-social-noir.svg'
-import LogoÉtablissementTerritorialSanitaire from '../entité-juridique/liste-des-établissements/logo-établissement-territorial-sanitaire-noir.svg'
-import LogoEntitéJuridiqueNoir from './logo-entité-juridique-noir.svg'
 import styles from './Recherche.module.css'
+import { RechercheViewModel } from './RechercheViewModel'
 
 export const Recherche = () => {
-  const { wording } = useDependencies()
+  const { paths, wording } = useDependencies()
   const nombreResultat = 12
   const recherche = 'Centre hospitalier de Saint Brieuc'
-  const [resultatReçu, setResultatReçu] = useState(false)
+  const [rechercheDemandée, setRechercheDemandée] = useState(false)
+  const [résultats, setRésultats] = useState<RechercheViewModel[]>([])
 
-  // TODO: faire un viewModel
-  const résultats = Array(12)
-    .fill({
-      commune: 'Saint-Brieuc',
-      département: 'Côtes d’Armor',
-      numéroFiness: '220000020',
-      raisonSociale: 'CENTRE HOSPITALIER DE SAINT BRIEUC',
-      type: 'Médico-social',
-    }, 0, 4)
-    .fill({
-      commune: 'Saint-Brieuc',
-      département: 'Côtes d’Armor',
-      numéroFiness: '220000020',
-      raisonSociale: 'CENTRE HOSPITALIER DE SAINT BRIEUC',
-      type: 'Sanitaire',
-    }, 4, 8)
-    .fill({
-      commune: 'Saint-Brieuc',
-      département: 'Côtes d’Armor',
-      numéroFiness: '220000020',
-      raisonSociale: 'CENTRE HOSPITALIER DE SAINT BRIEUC',
-      type: 'Entité Juridique',
-    }, 8, 12)
-
-  const afficherLogo = (type: string): string => {
-    if (type === 'Médico-social') {
-      return LogoÉtablissementTerritorialMédicoSocial
-    } else if (type === 'Sanitaire') {
-      return LogoÉtablissementTerritorialSanitaire
-    }
-    return LogoEntitéJuridiqueNoir
+  const rechercher = (event) => {
+    event.preventDefault()
+    setRechercheDemandée(true)
   }
+
+  useEffect(() => {
+    // fetch
+
+    const résultats = Array(12)
+      .fill({
+        commune: 'Saint-Brieuc',
+        département: 'Côtes d’Armor',
+        numéroFiness: '010003598',
+        raisonSociale: 'CENTRE HOSPITALIER DE SAINT BRIEUC',
+        type: 'Médico-social',
+      }, 0, 4)
+      .fill({
+        commune: 'Saint-Brieuc',
+        département: 'Côtes d’Armor',
+        numéroFiness: '010005239',
+        raisonSociale: 'CENTRE HOSPITALIER DE SAINT BRIEUC',
+        type: 'Sanitaire',
+      }, 4, 8)
+      .fill({
+        commune: 'Saint-Brieuc',
+        département: 'Côtes d’Armor',
+        numéroFiness: '010008407',
+        raisonSociale: 'CENTRE HOSPITALIER DE SAINT BRIEUC',
+        type: 'Entité Juridique',
+      }, 8, 12)
+
+    setRésultats(résultats.map((résultat) => new RechercheViewModel(résultat, paths)))
+  }, [rechercheDemandée])
 
   return (
     <>
@@ -81,49 +81,45 @@ export const Recherche = () => {
             />
             <button
               className="fr-btn"
-              onClick={(e) => {
-                e.preventDefault();setResultatReçu(true)
-              }}
+              onClick={rechercher}
             >
               {wording.RECHERCHE_LABEL}
             </button>
           </form>
         </section>
       </div>
-      {resultatReçu &&
+      {rechercheDemandée &&
         <section aria-label={wording.RECHERCHE_RESULTAT}>
           <p className="fr-h6 fr-mt-4w">
             {wording.RECHERCHE_NOMBRE_RESULTAT(nombreResultat, recherche)}
           </p>
-          <ul className="fr-grid-row fr-grid-row--gutters">
-            {résultats.map((résultat, index) => (
+          <ul className={'fr-grid-row fr-grid-row--gutters ' + styles['tuiles']}>
+            {résultats.map((résultatViewModel, index) => (
               <li
                 className="fr-col-3"
-                key={résultat.numéroFiness + index}
+                key={résultatViewModel.numéroFiness + index}
               >
-                <div className="fr-tile fr-enlarge-link fr-tile--horizontal">
+                <div className="fr-tile fr-enlarge-link fr-tile--horizontal-md">
                   <div className="fr-tile__body">
                     <h2 className="fr-tile__title">
                       <a
                         className="fr-tile__link"
-                        href=""
+                        href={résultatViewModel.construisLeLien()}
                       >
-                        {résultat.numéroFiness}
-                        {' - '}
-                        {résultat.raisonSociale}
+                        {résultatViewModel.titre}
                       </a>
                     </h2>
                     <p className="fr-tile__desc">
-                      {résultat.département + ', ' + résultat.commune}
+                      {résultatViewModel.départementEtCommune}
                     </p>
                   </div>
-                  <div className="fr-tile__img">
+                  <div className={styles['tuile']}>
                     <Image
                       alt=""
                       className="fr-responsive-img"
-                      height="27"
-                      src={afficherLogo(résultat.type)}
-                      width="27"
+                      height="40"
+                      src={résultatViewModel.afficheLeLogo()}
+                      width="40"
                     />
                   </div>
                 </div>
