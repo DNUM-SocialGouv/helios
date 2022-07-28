da3fac22f6319a3ab3d60a1166d421ec4b07424a
Thierry Gonard <goth@octo.com>
2022-07-28 10:30:41 +0200

    (hel-173) Crée et enregistre le model pour les màj des fichiers ources

---
+++ database/models/DateMiseÀJourFichierSourceModel.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, PrimaryColumn } from 'typeorm'
+
+export enum FichierSource {
+  FINESS_CS1400101 = 'finess_cs1400101',
+  FINESS_CS1400102 = 'finess_cs1400102',
+  DIAMANT_ANN_ERRD_EJ_ET = 'diamant_ann_errd_ej_et',
+  DIAMANT_ANN_MS_TDP_ET = 'diamant_ann_ms_tdp_et',
+  DIAMANT_MEN_PMSI_ANNUEL = 'diamant_men_pmsi_annuel',
+  DIAMANT_ANN_RPU = 'diamant_ann_rpu',
+}
+
+@Entity({ name: 'date_mise_a_jour_fichier_source' })
+export class DateMiseÀJourFichierSourceModel {
+  @Column({ name: 'derniere_mise_a_jour', type: 'date' })
+  public dernièreMiseÀJour!: string
+
+  @PrimaryColumn({
+    enum: FichierSource,
+    enumName: 'fichier_source',
+    type: 'enum',
+  })
+  public fichier!: FichierSource
+}
