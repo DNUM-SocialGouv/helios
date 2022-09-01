4b27ec69d3b58e733915a8b6ce52e78074e3749c
Thierry Gonard <goth@octo.com>
2022-09-01 16:30:50 +0200

    (hel-182) wip

--- database/migrations.ts
+++ database/migrations.ts
@@ -9,7 +9,6 @@ import { typeOrmOrm } from '../datacrawler/legacy/infrastructure/gateways/orm/ty
     dotEnvConfig()
     const environmentVariables = new NodeEnvironmentVariables(logger)
     const orm = await typeOrmOrm(environmentVariables)
-    await orm.undoLastMigration()
     await orm.runMigrations()
     await orm.destroy()
   } catch (error) {
