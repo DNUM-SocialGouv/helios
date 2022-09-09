872e8280a510c095289393267707f8d2c95188a4
sofcalca <10255447+sofcalca@users.noreply.github.com>
2022-09-09 11:12:42 +0200

    (hel-202) indexer communes et départements pour la recherche (#142)
    Co-authored-by: Thierry <103272598+goth-octo@users.noreply.github.com>

---
+++ database/migrations/1662644327376-AjoutDesCommunesEtDépartementsDansLaRecherche.js
@@ -0,0 +1,51 @@
+class AjoutDesCommunesEtDépartementsDansLaRecherche1662644327376 {
+
+  async up(queryRunner) {
+    await queryRunner.query(
+      `CREATE OR REPLACE VIEW recherche AS
+    SELECT
+      numero_finess_entite_juridique AS numero_finess,
+      raison_sociale,
+      'Entité juridique' AS type,
+      to_tsvector('unaccent_helios', raison_sociale || ' ' || numero_finess_entite_juridique || ' ' || commune || ' ' || departement) AS termes,
+      commune,
+      departement
+    FROM entite_juridique
+    UNION ALL
+    SELECT
+      numero_finess_etablissement_territorial AS numero_finess,
+      raison_sociale,
+      domaine::text AS type,
+      to_tsvector('unaccent_helios', raison_sociale || ' ' || numero_finess_etablissement_territorial || ' ' || commune || ' ' || departement) AS termes,
+      commune,
+      departement
+    FROM etablissement_territorial;`
+    )
+  }
+
+  async down(queryRunner) {
+    await queryRunner.query(
+      `CREATE OR REPLACE VIEW recherche AS
+    SELECT
+      numero_finess_entite_juridique AS numero_finess,
+      raison_sociale,
+      'Entité juridique' AS type,
+      to_tsvector('unaccent_helios', raison_sociale || ' ' || numero_finess_entite_juridique) AS termes,
+      commune,
+      departement
+    FROM entite_juridique
+    UNION ALL
+    SELECT
+      numero_finess_etablissement_territorial AS numero_finess,
+      raison_sociale,
+      domaine::text AS type,
+      to_tsvector('unaccent_helios', raison_sociale || ' ' || numero_finess_etablissement_territorial) AS termes,
+      commune,
+      departement
+    FROM etablissement_territorial;`
+    )
+  }
+
+}
+
+module.exports = AjoutDesCommunesEtDépartementsDansLaRecherche1662644327376
