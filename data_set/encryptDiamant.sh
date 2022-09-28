77483472233d656424d408960dfb52c56bb07247
Sofia Calcagno <10255447+sofcalca@users.noreply.github.com>
2022-09-28 19:12:23 +0200

    Changes

--- data_set/encryptDiamant.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-echo "$DIAMANT_PUBLIC_KEY" | base64 --decode | gpg --import
-
-for csv_diamant in ./data_set/diamant/*
-do
-gpg --output ./data_set/diamant_chiffré/"$(basename $csv_diamant)".gpg --encrypt --recipient clef_dev_helios $csv_diamant
-done
