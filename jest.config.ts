132cc5e92ea4c1107ee31f5ab83f58cfbae3fa4b
Fabien - MEFA <nerocro@gmail.com>
2022-10-03 09:20:42 +0200

    (hel-193) mise en place du mutation testing une nuit dans la semaine (#164)
    Co-authored-by: Fabien Mercier <fabien.mercier@octo.com>

--- jest.config.ts
+++ jest.config.ts
@@ -34,6 +34,11 @@ async function jestConfig() {
   const nextJestConfig = await createJestConfig(customJestConfig)()
   // /node_modules/ is the first pattern
   nextJestConfig.transformIgnorePatterns[0] = '/node_modules/(?!uuid)/'
+
+  // Stryker ne fonctionne pas à cause de l'async https://github.com/stryker-mutator/stryker-js/issues/3480
+  // Pour que les mutants naissent, il faut générer ce fichier donc lancer au moins un test
+  require('fs').writeFileSync('jest-for-mutation-testing.json', JSON.stringify(nextJestConfig, null, 2))
+
   return nextJestConfig
 }
 
