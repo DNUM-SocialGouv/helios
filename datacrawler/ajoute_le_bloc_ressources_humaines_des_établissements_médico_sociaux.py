6778a3a120da2934a36ad7e92281a0b7888d93a8
Thierry Gonard <goth@octo.com>
2022-10-19 18:12:08 +0200

    (hel-301) wip

--- datacrawler/ajoute_le_bloc_ressources_humaines_des_établissements_médico_sociaux.py
+++ datacrawler/ajoute_le_bloc_ressources_humaines_des_établissements_médico_sociaux.py
@@ -19,17 +19,41 @@ from datacrawler.transform.transforme_le_bloc_ressources_humaines_des_établisse
     transforme_les_données_des_ressources_humaines,
 )
 from datacrawler.transform.équivalences_diamant_helios import (
+    colonnes_à_lire_bloc_ressources_humaines_ann_ca_ej_et,
+    colonnes_à_lire_bloc_ressources_humaines_ann_errd_ej_et,
     colonnes_à_lire_bloc_ressources_humaines_ann_ms_tdp_et,
     extrais_l_equivalence_des_types_des_colonnes,
+    équivalences_diamant_ann_ca_ej_et_ressources_humaines_helios,
+    équivalences_diamant_ann_errd_ej_et_ressources_humaines_helios,
     équivalences_diamant_ann_ms_tdp_et_ressources_humaines_helios,
 )
 
 
 def ajoute_le_bloc_ressources_humaines_des_établissements_médico_sociaux(
+    chemin_du_fichier_ann_ca_ej_et: str,
+    chemin_du_fichier_ann_errd_ej_et: str,
     chemin_du_fichier_ann_ms_tdp_et: str,
     base_de_données: Engine,
     logger: Logger,
 ) -> None:
+    logger.info("[DIAMANT] Récupère les nombres d'ETP réalisés des établissements médico-sociaux sous cadre ERRD")
+    données_ann_errd_ej_et = lis_le_fichier_csv(
+        chemin_du_fichier_ann_errd_ej_et,
+        colonnes_à_lire_bloc_ressources_humaines_ann_errd_ej_et,
+        extrais_l_equivalence_des_types_des_colonnes(équivalences_diamant_ann_errd_ej_et_ressources_humaines_helios),
+    )
+    logger.info(f"[DIAMANT] {données_ann_errd_ej_et.shape[0]} lignes trouvées dans le fichier ANN_ERRD_EJ_ET")
+    date_du_fichier_ann_errd_ej_et = extrais_la_date_du_nom_de_fichier_diamant(chemin_du_fichier_ann_errd_ej_et)
+
+    logger.info("[DIAMANT] Récupère les nombres d'ETP réalisés des établissements médico-sociaux sous cadre CA")
+    données_ann_ca_ej_et = lis_le_fichier_csv(
+        chemin_du_fichier_ann_ca_ej_et,
+        colonnes_à_lire_bloc_ressources_humaines_ann_ca_ej_et,
+        extrais_l_equivalence_des_types_des_colonnes(équivalences_diamant_ann_ca_ej_et_ressources_humaines_helios),
+    )
+    logger.info(f"[DIAMANT] {données_ann_ca_ej_et.shape[0]} lignes trouvées dans le fichier ANN_CA_EJ_ET")
+    date_du_fichier_ann_ca_ej_et = extrais_la_date_du_nom_de_fichier_diamant(chemin_du_fichier_ann_ca_ej_et)
+
     logger.info("[DIAMANT] Récupère les données ressources humaines des établissements médico-sociaux")
     données_ann_ms_tdp_et = lis_le_fichier_csv(
         chemin_du_fichier_ann_ms_tdp_et,
@@ -43,6 +67,8 @@ def ajoute_le_bloc_ressources_humaines_des_établissements_médico_sociaux(
 
     taux_d_absenteismes_des_établissements_médico_sociaux = transforme_les_données_des_ressources_humaines(
         filtre_les_données_sur_les_n_dernières_années(données_ann_ms_tdp_et, NOMBRE_D_ANNÉES_MAX_D_ANTÉRIORITÉ_DES_DONNÉES_MÉDICO_SOCIALES),
+        filtre_les_données_sur_les_n_dernières_années(données_ann_errd_ej_et, NOMBRE_D_ANNÉES_MAX_D_ANTÉRIORITÉ_DES_DONNÉES_MÉDICO_SOCIALES),
+        filtre_les_données_sur_les_n_dernières_années(données_ann_ca_ej_et, NOMBRE_D_ANNÉES_MAX_D_ANTÉRIORITÉ_DES_DONNÉES_MÉDICO_SOCIALES),
         numéros_finess_des_établissements_connus,
         logger,
     )
@@ -54,7 +80,11 @@ def ajoute_le_bloc_ressources_humaines_des_établissements_médico_sociaux(
             connection,
             TABLES_DES_RESSOURCES_HUMAINES_MÉDICO_SOCIAL,
             taux_d_absenteismes_des_établissements_médico_sociaux,
-            [(FichierSource.DIAMANT_ANN_MS_TDP_ET, date_du_fichier_ann_ms_tdp_et)],
+            [
+                (FichierSource.DIAMANT_ANN_MS_TDP_ET, date_du_fichier_ann_ms_tdp_et),
+                (FichierSource.DIAMANT_ANN_ERRD_EJ_ET, date_du_fichier_ann_errd_ej_et),
+                (FichierSource.DIAMANT_ANN_CA_EJ_ET, date_du_fichier_ann_ca_ej_et),
+            ],
             logger,
         )
 
@@ -63,10 +93,22 @@ if __name__ == "__main__":
     logger_helios, variables_d_environnement = initialise_les_dépendances()
     base_de_données_helios = create_engine(variables_d_environnement["DATABASE_URL"])
     fichiers = os.listdir(variables_d_environnement["DIAMANT_DATA_PATH"])
+    chemin_local_du_fichier_ann_ca_ej_et = os.path.join(
+        variables_d_environnement["DIAMANT_DATA_PATH"], trouve_le_nom_du_fichier_diamant(fichiers, "ANN_CA_EJ_ET", logger_helios)
+    )
+    chemin_local_du_fichier_ann_errd_ej_et = os.path.join(
+        variables_d_environnement["DIAMANT_DATA_PATH"], trouve_le_nom_du_fichier_diamant(fichiers, "ANN_ERRD_EJ_ET", logger_helios)
+    )
     chemin_local_du_fichier_ann_ms_tdp_et = os.path.join(
         variables_d_environnement["DIAMANT_DATA_PATH"], trouve_le_nom_du_fichier_diamant(fichiers, "ANN_MS_TDP_ET", logger_helios)
     )
     logger_helios.info(
         f"""[DIAMANT] Cherche les données des ressources humaines pour les ET médico sociaux dans le fichier {chemin_local_du_fichier_ann_ms_tdp_et}"""
     )
-    ajoute_le_bloc_ressources_humaines_des_établissements_médico_sociaux(chemin_local_du_fichier_ann_ms_tdp_et, base_de_données_helios, logger_helios)
+    ajoute_le_bloc_ressources_humaines_des_établissements_médico_sociaux(
+        chemin_local_du_fichier_ann_ca_ej_et,
+        chemin_local_du_fichier_ann_errd_ej_et,
+        chemin_local_du_fichier_ann_ms_tdp_et,
+        base_de_données_helios,
+        logger_helios,
+    )
