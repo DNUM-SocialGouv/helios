f93f1a6b9b54b82d5a6b80c42e7be2f565454870
Thierry Gonard <goth@octo.com>
2022-10-25 10:38:40 +0200

    (hel-257) [WIP] Commit by Thierry on 2022-10-25T10:38:40

--- src/frontend/ui/établissement-territorial-médico-social/bloc-ressources-humaines/ÉtablissementTerritorialRessourcesHumainesMédicoSocialViewModel.tsx
+++ src/frontend/ui/établissement-territorial-médico-social/bloc-ressources-humaines/ÉtablissementTerritorialRessourcesHumainesMédicoSocialViewModel.tsx
@@ -4,7 +4,8 @@ import { GraphiqueViewModel } from '../../commun/Graphique/GraphiqueViewModel'
 import { StringFormater } from '../../commun/StringFormater'
 
 export class ÉtablissementTerritorialRessourcesHumainesMédicoSocialViewModel extends GraphiqueViewModel {
-  private readonly ratioHistogrammeHorizontal = 2
+  private readonly RATIO_HISTOGRAMME_HORIZONTAL = 2
+  private readonly SEUIL_DU_TAUX_DE_ROTATION_DU_PERSONNEL_ATYPIQUE: number = 50
 
   constructor(
     private readonly ressourcesHumainesMédicoSocial: ÉtablissementTerritorialMédicoSocialRessourcesHumaines[],
@@ -14,7 +15,8 @@ export class ÉtablissementTerritorialRessourcesHumainesMédicoSocialViewModel e
   }
 
   public get lesDonnéesRessourcesHumainesNeSontPasRenseignées(): boolean {
-    return !this.leNombreDEtpRéaliséEstIlRenseigné
+    return !this.leNombreDEtpRéaliséEstIlRenseigné &&
+      !this.leTauxDeRotationDuPersonnelEstIlRenseigné
   }
 
   private get leNombreDEtpRéaliséEstIlRenseigné(): boolean {
@@ -37,7 +39,7 @@ export class ÉtablissementTerritorialRessourcesHumainesMédicoSocialViewModel e
       couleursDeLHistogramme,
       libellésDesValeurs,
       libellésDesTicks,
-      this.ratioHistogrammeHorizontal,
+      this.RATIO_HISTOGRAMME_HORIZONTAL,
       this.wording.ANNÉE,
       this.wording.NOMBRE_D_ETP_TOTAL_RÉALISÉ_SANS_ABRÉVIATION,
       annéesManquantes
@@ -48,6 +50,54 @@ export class ÉtablissementTerritorialRessourcesHumainesMédicoSocialViewModel e
     return StringFormater.formateLaDate(this.ressourcesHumainesMédicoSocial[0].nombreDEtpRéalisés.dateMiseÀJourSource)
   }
 
+  private get leTauxDeRotationDuPersonnelEstIlRenseigné(): boolean {
+    return this.ressourcesHumainesMédicoSocial.some((ressourceHumaine) => ressourceHumaine.tauxDeRotationDuPersonnel.valeur !== null)
+  }
+
+  public get tauxDeRotationDuPersonnel(): JSX.Element {
+    const [valeurs, années] = this.construisLesTauxDeRotationDuPersonnel()
+    const construisLaCouleurDeLaBarreDeLHistogramme = (valeur: number, année: string | number) => {
+      if (!this.leTauxDeRotationDuPersonnelEstIlDansLesBornesAcceptables(valeur)) {
+        return {
+          premierPlan: this.couleurDuFondHistogrammeDeDépassement,
+          secondPlan : this.couleurSecondPlanHistogrammeDeDépassement,
+        }
+      }
+
+      if (this.estCeLAnnéePassée(année)) {
+        return {
+          premierPlan: this.couleurDuFondHistogrammePrimaire,
+          secondPlan: this.couleurDuFond,
+        }
+      }
+
+      return {
+        premierPlan: this.couleurDuFondHistogrammeSecondaire,
+        secondPlan: this.couleurDuFond,
+      }
+    }
+    const libellésDesValeurs = Array(valeurs.length).fill({ couleur: this.couleurDuFond })
+    const libellésDesTicks = années.map((année) => ({ tailleDePolice: this.estCeLAnnéePassée(année) ? this.policeGrasse : this.policeNormale }))
+
+    return this.afficheUnHistogrammeVertical(
+      valeurs,
+      années,
+      this.construisLesCouleursDeLHistogramme(valeurs, années, construisLaCouleurDeLaBarreDeLHistogramme),
+      libellésDesValeurs,
+      libellésDesTicks,
+      this.wording.ANNÉE,
+      this.wording.TAUX_DE_ROTATION_DU_PERSONNEL
+    )
+  }
+
+  private leTauxDeRotationDuPersonnelEstIlDansLesBornesAcceptables(valeur: number) {
+    return valeur >= 0 && valeur <= this.SEUIL_DU_TAUX_DE_ROTATION_DU_PERSONNEL_ATYPIQUE
+  }
+
+  public get dateDeMiseÀJourDuTauxDeRotationDuPersonnel(): string {
+    return StringFormater.formateLaDate(this.ressourcesHumainesMédicoSocial[0].tauxDeRotationDuPersonnel.dateMiseÀJourSource)
+  }
+
   private construisLesNombresDEtpRéalisés(): number[][] {
     const valeurs: number[] = []
     const années: number[] = []
@@ -56,6 +106,20 @@ export class ÉtablissementTerritorialRessourcesHumainesMédicoSocialViewModel e
       if (valeur !== null) {
         années.push(ressourceHumaineMédicoSocial.année)
         valeurs.push(valeur)
+      }
+    })
+
+    return [valeurs, années]
+  }
+
+  private construisLesTauxDeRotationDuPersonnel(): number[][] {
+    const valeurs: number[] = []
+    const années: number[] = []
+    this.ressourcesHumainesMédicoSocial.forEach((ressourceHumaineMédicoSocial: ÉtablissementTerritorialMédicoSocialRessourcesHumaines) => {
+      const valeur = ressourceHumaineMédicoSocial.tauxDeRotationDuPersonnel.valeur
+      if (valeur !== null) {
+        années.push(ressourceHumaineMédicoSocial.année)
+        valeurs.push(StringFormater.transformeEnTaux(valeur))
       }
     })
 
