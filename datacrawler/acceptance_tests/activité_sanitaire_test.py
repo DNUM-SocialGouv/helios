7ba0b7e5d8feb903a58f0c33f284f95a5a441d41
miranda.lin-guignard <miranda.w.lin@gmail.com>
2023-02-03 15:47:20 +0100

    (hel-352) python test

--- datacrawler/acceptance_tests/activité_sanitaire_test.py
+++ datacrawler/acceptance_tests/activité_sanitaire_test.py
@@ -8,7 +8,8 @@ from numpy import NaN
 
 import datacrawler
 from datacrawler.ajoute_les_activités_des_établissements_sanitaires import ajoute_les_activités_des_établissements_sanitaires
-from datacrawler.load.nom_des_tables import TABLE_DES_ACTIVITÉS_DES_ÉTABLISSEMENTS_SANITAIRES, TABLE_DES_MISES_À_JOUR_DES_FICHIERS_SOURCES, FichierSource
+from datacrawler.load.nom_des_tables import TABLE_DES_ACTIVITÉS_DES_ÉTABLISSEMENTS_SANITAIRES, \
+    TABLE_DES_MISES_À_JOUR_DES_FICHIERS_SOURCES, FichierSource, TABLE_DES_ACTIVITÉS_SANITAIRES_DES_ENTITES_JURIDIQUES
 from datacrawler.test_helpers import (
     base_de_données_test,
     mocked_logger,
@@ -184,3 +185,39 @@ class TestAjouteLesActivitésDesÉtablissementsSanitaires:
             f"""SELECT * FROM {TABLE_DES_MISES_À_JOUR_DES_FICHIERS_SOURCES} WHERE fichier = '{FichierSource.DIAMANT_ANN_RPU.value}'"""
         )
         assert date_du_fichier_ann_rpu.fetchone() == (date(2020, 1, 1), FichierSource.DIAMANT_ANN_RPU.value)
+
+
+    @freeze_time("2022-01-14")
+    def test_sauvegarde_agregation_activite_sanitaire_des_ET_dans_EJ(self) -> None:
+        # GIVEN
+        chemin_du_fichier_men_pmsi_annuel = "data_set/diamant/MEN_PMSI_ANNUEL_2022_06_07.CSV"
+        chemin_du_fichier_ann_rpu = "data_set/diamant/ANN_RPU_2022_06_23.CSV"
+        sauvegarde_une_entité_juridique_en_base("010008407", base_de_données_test)
+        sauvegarde_un_établissement_en_base("010005239", "010008407", base_de_données_test)
+        sauvegarde_un_établissement_en_base("010786259", "010008407", base_de_données_test)
+
+        # WHEN
+        ajoute_les_activités_des_établissements_sanitaires(chemin_du_fichier_men_pmsi_annuel, chemin_du_fichier_ann_rpu, base_de_données_test, mocked_logger)
+
+        # THEN
+        activité_agrégé_attendue = pd.DataFrame(
+            {
+                "annee": [2017, 2018, 2019, 2020, 2021],
+                "numero_finess_entite_juridique": ["010008407", "010008407", "010008407", "010008407", "010008407"],
+                "nombre_sejours_partiels_medecine": [1.0, 3.0, 4.0, 5.0, 4.0],
+                "nombre_sejours_partiels_obstetrique": [10.0, NaN, NaN, NaN, NaN],
+                "nombre_sejours_partiels_chirurgie": [20.0, NaN, NaN, 20.0, NaN],
+                "nombre_sejours_complets_medecine": [255.0, 232.0, 231.0, 486.0, 231.0],
+                "nombre_sejours_complets_obstetrique": [10.0, NaN, NaN, 10.0, NaN],
+                "nombre_sejours_complets_chirurgie": [6.0, 10.0, 9.0, 15.0, 9.0],
+                "nombre_journees_completes_ssr": [1074.0, 1103.0, 1074.0, NaN, NaN],
+                "nombre_journees_partiels_ssr": [100.0, NaN, NaN, 100.0, NaN],
+                "nombre_journees_complete_psy": [200.0, NaN, NaN, NaN, NaN],
+                "nombre_journées_partielles_psy": [300.0, NaN, NaN, 300.0, NaN],
+                "nombre_passages_urgences": [10296.0, 24032.0, 23987.0, 33087.0, 25987.0],
+            }
+        )
+
+        activité_enregistrée = pd.read_sql_table(TABLE_DES_ACTIVITÉS_SANITAIRES_DES_ENTITES_JURIDIQUES, base_de_données_test)
+
+        pd.testing.assert_frame_equal(activité_enregistrée, activité_agrégé_attendue)
