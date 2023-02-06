3fe076a989316b2ac2f97b632df774d2ce2e3e89
Clément Dubois <clement.dubois@octo.com>
2023-02-06 15:33:12 +0100

    (hel-352) lint

--- datacrawler/agrégation_activité_sanitaire.py
+++ datacrawler/agrégation_activité_sanitaire.py
@@ -12,7 +12,7 @@ def agrège_les_activités_sanitaire_des_entités_juridiques(base_de_données: E
     activités_sanitaires = pd.read_sql_table(TABLE_DES_ACTIVITÉS_DES_ÉTABLISSEMENTS_SANITAIRES, base_de_données, index_col="index")
     finess = récupère_les_numéros_finess_des_établissements_de_la_base(base_de_données)
     activités_avec_entitie_juridique = pd.merge(activités_sanitaires, finess, on="numero_finess_etablissement_territorial")
-    
+
     agrégation_activités = agrège_les_activités_dans_les_entites_juridiques(activités_avec_entitie_juridique)
 
     with base_de_données.begin() as connection:
