193e7f59d62010aabb06b72c3ae24f9534858bdb
miranda.lin-guignard <miranda.w.lin@gmail.com>
2023-02-06 12:04:49 +0100

    (hel-352) add sum Nan + value test

--- datacrawler/transform/entite_juridique/bloc_activités/agrège_les_activités_dans_les_entites_juridiques.py
+++ datacrawler/transform/entite_juridique/bloc_activités/agrège_les_activités_dans_les_entites_juridiques.py
@@ -6,15 +6,15 @@ def agrège_les_activités_dans_les_entites_juridiques(sanitaires: pd.DataFrame)
         sanitaires.drop(columns=["numero_finess_etablissement_territorial"])
         .groupby("annee", as_index=False)
         .agg({"numero_finess_entite_juridique": "first",
-              "nombre_sejours_partiels_medecine": "sum",
-              "nombre_passages_urgences": "sum",
-              "nombre_sejours_partiels_obstetrique": "sum",
-              "nombre_sejours_partiels_chirurgie": "sum",
-              "nombre_sejours_complets_medecine": "sum",
-              "nombre_sejours_complets_chirurgie": "sum",
-              "nombre_sejours_complets_obstetrique": "sum",
-              "nombre_journees_completes_ssr": "sum",
-              "nombre_journees_partiels_ssr": "sum",
-              "nombre_journees_complete_psy": "sum",
-              "nombre_journées_partielles_psy": "sum"})
+              "nombre_sejours_partiels_medecine": lambda x: x.sum(skipna=False),
+              "nombre_passages_urgences": lambda x: x.sum(skipna=False),
+              "nombre_sejours_partiels_obstetrique": lambda x: x.sum(skipna=False),
+              "nombre_sejours_partiels_chirurgie": lambda x: x.sum(skipna=False),
+              "nombre_sejours_complets_medecine": lambda x: x.sum(skipna=False),
+              "nombre_sejours_complets_chirurgie": lambda x: x.sum(skipna=False),
+              "nombre_sejours_complets_obstetrique": lambda x: x.sum(skipna=False),
+              "nombre_journees_completes_ssr": lambda x: x.sum(skipna=False),
+              "nombre_journees_partiels_ssr": lambda x: x.sum(skipna=False),
+              "nombre_journees_complete_psy": lambda x: x.sum(skipna=False),
+              "nombre_journées_partielles_psy": lambda x: x.sum(skipna=False)})
     )
--- datacrawler/transform/entite_juridique/bloc_activités/agrège_les_activités_dans_les_entites_juridiques_test.py
+++ datacrawler/transform/entite_juridique/bloc_activités/agrège_les_activités_dans_les_entites_juridiques_test.py
@@ -1,3 +1,5 @@
+from numpy import NaN
+
 import pandas as pd
 
 import datacrawler
@@ -174,5 +176,89 @@ class TestAgrègeLesActivitesDansLesEntitesJuridiques:
              "nombre_journees_partiels_ssr": [2],
              "nombre_journees_complete_psy": [1],
              "nombre_journées_partielles_psy": [3]
+             })
+        pd.testing.assert_frame_equal(activités_agrégées, activités_agrégées_attendues)
+
+    def test_renvoit_les_activites_de_deux_etablissements_pour_une_annee_avec_NaN(self) -> None:
+        # GIVEN
+        activites_entite_juridique_sanitaires = pd.DataFrame(
+            {
+                "annee": ["2020", "2020"],
+                "numero_finess_entite_juridique": ["111111111", "111111111"],
+                "numero_finess_etablissement_territorial": ["222222222", "333333333"],
+                "nombre_sejours_partiels_medecine": [NaN, NaN],
+                "nombre_passages_urgences": [NaN, NaN],
+                "nombre_sejours_partiels_obstetrique": [NaN, NaN],
+                "nombre_sejours_partiels_chirurgie": [NaN, NaN],
+                "nombre_sejours_complets_medecine": [NaN, NaN],
+                "nombre_sejours_complets_chirurgie": [NaN, NaN],
+                "nombre_sejours_complets_obstetrique": [NaN, NaN],
+                "nombre_journees_completes_ssr": [NaN, NaN],
+                "nombre_journees_partiels_ssr": [NaN, NaN],
+                "nombre_journees_complete_psy": [NaN, NaN],
+                "nombre_journées_partielles_psy": [NaN, NaN]
+            }
+        )
+
+        # WHEN
+        activités_agrégées = agrège_les_activités_dans_les_entites_juridiques(activites_entite_juridique_sanitaires)
+
+        # THEN
+        activités_agrégées_attendues = pd.DataFrame(
+            {"annee": ["2020"],
+             "numero_finess_entite_juridique": ["111111111"],
+             "nombre_sejours_partiels_medecine": [NaN],
+             "nombre_passages_urgences": [NaN],
+             "nombre_sejours_partiels_obstetrique": [NaN],
+             "nombre_sejours_partiels_chirurgie": [NaN],
+             "nombre_sejours_complets_medecine": [NaN],
+             "nombre_sejours_complets_chirurgie": [NaN],
+             "nombre_sejours_complets_obstetrique": [NaN],
+             "nombre_journees_completes_ssr": [NaN],
+             "nombre_journees_partiels_ssr": [NaN],
+             "nombre_journees_complete_psy": [NaN],
+             "nombre_journées_partielles_psy": [NaN]
+             })
+        pd.testing.assert_frame_equal(activités_agrégées, activités_agrégées_attendues)
+
+    def test_renvoit_les_activites_de_deux_etablissements_pour_une_annee_avec_NaN_et_chiffre(self) -> None:
+        # GIVEN
+        activites_entite_juridique_sanitaires = pd.DataFrame(
+            {
+                "annee": ["2020", "2020"],
+                "numero_finess_entite_juridique": ["111111111", "111111111"],
+                "numero_finess_etablissement_territorial": ["222222222", "333333333"],
+                "nombre_sejours_partiels_medecine": [NaN, 1],
+                "nombre_passages_urgences": [NaN, 2],
+                "nombre_sejours_partiels_obstetrique": [NaN, 3],
+                "nombre_sejours_partiels_chirurgie": [NaN, 4],
+                "nombre_sejours_complets_medecine": [NaN, 5],
+                "nombre_sejours_complets_chirurgie": [NaN, 6],
+                "nombre_sejours_complets_obstetrique": [NaN, 7],
+                "nombre_journees_completes_ssr": [NaN, 8],
+                "nombre_journees_partiels_ssr": [NaN, 9],
+                "nombre_journees_complete_psy": [NaN, 0],
+                "nombre_journées_partielles_psy": [NaN, 1]
+            }
+        )
+
+        # WHEN
+        activités_agrégées = agrège_les_activités_dans_les_entites_juridiques(activites_entite_juridique_sanitaires)
+
+        # THEN
+        activités_agrégées_attendues = pd.DataFrame(
+            {"annee": ["2020"],
+             "numero_finess_entite_juridique": ["111111111"],
+             "nombre_sejours_partiels_medecine": [1.0],
+             "nombre_passages_urgences": [2.0],
+             "nombre_sejours_partiels_obstetrique": [3.0],
+             "nombre_sejours_partiels_chirurgie": [4.0],
+             "nombre_sejours_complets_medecine": [5.0],
+             "nombre_sejours_complets_chirurgie": [6.0],
+             "nombre_sejours_complets_obstetrique": [7.0],
+             "nombre_journees_completes_ssr": [8.0],
+             "nombre_journees_partiels_ssr": [9.0],
+             "nombre_journees_complete_psy": [0.0],
+             "nombre_journées_partielles_psy": [1.0]
              })
         pd.testing.assert_frame_equal(activités_agrégées, activités_agrégées_attendues)
