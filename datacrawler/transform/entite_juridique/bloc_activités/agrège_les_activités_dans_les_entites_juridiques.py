9050ec841b09efae8bdae5581bda5f080cb6b011
Clément Dubois <clement.dubois@octo.com>
2023-02-06 12:27:16 +0100

    (hel-352) Tous les tests somme

--- datacrawler/transform/entite_juridique/bloc_activités/agrège_les_activités_dans_les_entites_juridiques.py
+++ datacrawler/transform/entite_juridique/bloc_activités/agrège_les_activités_dans_les_entites_juridiques.py
@@ -4,17 +4,6 @@ import pandas as pd
 def agrège_les_activités_dans_les_entites_juridiques(sanitaires: pd.DataFrame):
     return (
         sanitaires.drop(columns=["numero_finess_etablissement_territorial"])
-        .groupby("annee", as_index=False)
-        .agg({"numero_finess_entite_juridique": "first",
-              "nombre_sejours_partiels_medecine": lambda x: x.sum(skipna=False),
-              "nombre_passages_urgences": lambda x: x.sum(skipna=False),
-              "nombre_sejours_partiels_obstetrique": lambda x: x.sum(skipna=False),
-              "nombre_sejours_partiels_chirurgie": lambda x: x.sum(skipna=False),
-              "nombre_sejours_complets_medecine": lambda x: x.sum(skipna=False),
-              "nombre_sejours_complets_chirurgie": lambda x: x.sum(skipna=False),
-              "nombre_sejours_complets_obstetrique": lambda x: x.sum(skipna=False),
-              "nombre_journees_completes_ssr": lambda x: x.sum(skipna=False),
-              "nombre_journees_partiels_ssr": lambda x: x.sum(skipna=False),
-              "nombre_journees_complete_psy": lambda x: x.sum(skipna=False),
-              "nombre_journées_partielles_psy": lambda x: x.sum(skipna=False)})
+        .groupby(["annee", "numero_finess_entite_juridique"], as_index=False)
+        .sum(min_count=1)
     )
