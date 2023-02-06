8fe8af3f2d6cac2e8d8a5513d0651d4529a74322
miranda.lin-guignard <miranda.w.lin@gmail.com>
2023-02-06 09:55:09 +0100

    (hel-352) added other fields to the tests

--- datacrawler/transform/entite_juridique/bloc_activités/agrège_les_activités_dans_les_entites_juridiques.py
+++ datacrawler/transform/entite_juridique/bloc_activités/agrège_les_activités_dans_les_entites_juridiques.py
@@ -1,9 +1,20 @@
 import pandas as pd
 
 
-def agrège_les_activités_dans_les_entites_juridiques(sanitaires):
+def agrège_les_activités_dans_les_entites_juridiques(sanitaires: pd.DataFrame):
     return (
         sanitaires.drop(columns=["numero_finess_etablissement_territorial"])
         .groupby("annee", as_index=False)
-        .agg({"numero_finess_entite_juridique": "first", "nombre_sejours_partiels_medecine": "sum"})
+        .agg({"numero_finess_entite_juridique": "first",
+              "nombre_sejours_partiels_medecine": "sum",
+              "nombre_passages_urgences": "sum",
+              "nombre_sejours_partiels_obstetrique": "sum",
+              "nombre_sejours_partiels_chirurgie": "sum",
+              "nombre_sejours_complets_medecine": "sum",
+              "nombre_sejours_complets_chirurgie": "sum",
+              "nombre_sejours_complets_obstetrique": "sum",
+              "nombre_journees_completes_ssr": "sum",
+              "nombre_journees_partiels_ssr": "sum",
+              "nombre_journees_complete_psy": "sum",
+              "nombre_journées_partielles_psy": "sum"})
     )
--- datacrawler/transform/entite_juridique/bloc_activités/agrège_les_activités_dans_les_entites_juridiques_test.py
+++ datacrawler/transform/entite_juridique/bloc_activités/agrège_les_activités_dans_les_entites_juridiques_test.py
@@ -1,84 +1,178 @@
 import pandas as pd
 
-from datacrawler.transform.entite_juridique.bloc_activités.agrège_les_activités_dans_les_entites_juridiques import (
-    agrège_les_activités_dans_les_entites_juridiques,
-)
+import datacrawler
+from datacrawler.transform.entite_juridique.bloc_activités.agrège_les_activités_dans_les_entites_juridiques import \
+    agrège_les_activités_dans_les_entites_juridiques
 
 
-def test_renvoit_un_dataframe_vide_si_pas_d_activites_ET_sanitaires():
-    # GIVEN
-    activites_entite_juridique_sanitaires = pd.DataFrame(
-        {"annee": [], "numero_finess_entite_juridique": [], "numero_finess_etablissement_territorial": [], "nombre_sejours_partiels_medecine": []}
-    )
+class TestAgrègeLesActivitesDansLesEntitesJuridiques:
 
-    # WHEN
-    activités_agrégées = agrège_les_activités_dans_les_entites_juridiques(activites_entite_juridique_sanitaires)
+    def test_renvoit_un_dataframe_vide_si_pas_d_activites_ET_sanitaires(self) -> None:
+        # GIVEN
+        activites_entite_juridique_sanitaires = pd.DataFrame(
+            {"annee": [],
+             "numero_finess_entite_juridique": [],
+             "numero_finess_etablissement_territorial": [],
+             "nombre_sejours_partiels_medecine": [],
+             "nombre_passages_urgences": [],
+             "nombre_sejours_partiels_obstetrique": [],
+             "nombre_sejours_partiels_chirurgie": [],
+             "nombre_sejours_complets_medecine": [],
+             "nombre_sejours_complets_chirurgie": [],
+             "nombre_sejours_complets_obstetrique": [],
+             "nombre_journees_completes_ssr": [],
+             "nombre_journees_partiels_ssr": [],
+             "nombre_journees_complete_psy": [],
+             "nombre_journées_partielles_psy": [],
+             }
+        )
 
-    # THEN
-    activités_agrégées_attendues = pd.DataFrame(
-        {
-            "annee": [],
-            "numero_finess_entite_juridique": [],
-            "nombre_sejours_partiels_medecine": [],
-        }
-    )
-    pd.testing.assert_frame_equal(activités_agrégées, activités_agrégées_attendues)
+        # WHEN
+        activités_agrégées = agrège_les_activités_dans_les_entites_juridiques(activites_entite_juridique_sanitaires)
 
+        # THEN
+        activités_agrégées_attendues = pd.DataFrame(
+            {
+                "annee": [],
+                "numero_finess_entite_juridique": [],
+                "nombre_sejours_partiels_medecine": [],
+                "nombre_passages_urgences": [],
+                "nombre_sejours_partiels_obstetrique": [],
+                "nombre_sejours_partiels_chirurgie": [],
+                "nombre_sejours_complets_medecine": [],
+                "nombre_sejours_complets_chirurgie": [],
+                "nombre_sejours_complets_obstetrique": [],
+                "nombre_journees_completes_ssr": [],
+                "nombre_journees_partiels_ssr": [],
+                "nombre_journees_complete_psy": [],
+                "nombre_journées_partielles_psy": [],
+            }
+        )
+        pd.testing.assert_frame_equal(activités_agrégées, activités_agrégées_attendues)
 
-def test_renvoit_les_activités_d_un_etablissement_pour_une_annee():
-    # GIVEN
-    activites_entite_juridique_sanitaires = pd.DataFrame(
-        {
-            "annee": ["2020"],
-            "numero_finess_entite_juridique": ["111111111"],
-            "numero_finess_etablissement_territorial": ["222222222"],
-            "nombre_sejours_partiels_medecine": [10],
-        }
-    )
+    def test_renvoit_les_activités_d_un_etablissement_pour_une_annee(self) -> None:
+        # GIVEN
+        activites_entite_juridique_sanitaires = pd.DataFrame(
+            {
+                "annee": ["2020"],
+                "numero_finess_entite_juridique": ["111111111"],
+                "numero_finess_etablissement_territorial": ["222222222"],
+                "nombre_sejours_partiels_medecine": [10],
+                "nombre_passages_urgences": [1000],
+                "nombre_sejours_partiels_obstetrique": [1],
+                "nombre_sejours_partiels_chirurgie": [2],
+                "nombre_sejours_complets_medecine": [20],
+                "nombre_sejours_complets_chirurgie": [10],
+                "nombre_sejours_complets_obstetrique": [5],
+                "nombre_journees_completes_ssr": [500],
+                "nombre_journees_partiels_ssr": [1],
+                "nombre_journees_complete_psy": [1],
+                "nombre_journées_partielles_psy": [1]
+            }
+        )
 
-    # WHEN
-    activités_agrégées = agrège_les_activités_dans_les_entites_juridiques(activites_entite_juridique_sanitaires)
+        # WHEN
+        activités_agrégées = agrège_les_activités_dans_les_entites_juridiques(activites_entite_juridique_sanitaires)
 
-    # THEN
-    activités_agrégées_attendues = pd.DataFrame({"annee": ["2020"], "numero_finess_entite_juridique": ["111111111"], "nombre_sejours_partiels_medecine": [10]})
-    pd.testing.assert_frame_equal(activités_agrégées, activités_agrégées_attendues)
+        # THEN
+        activités_agrégées_attendues = pd.DataFrame(
+            {"annee": ["2020"],
+             "numero_finess_entite_juridique": ["111111111"],
+             "nombre_sejours_partiels_medecine": [10],
+             "nombre_passages_urgences": [1000],
+             "nombre_sejours_partiels_obstetrique": [1],
+             "nombre_sejours_partiels_chirurgie": [2],
+             "nombre_sejours_complets_medecine": [20],
+             "nombre_sejours_complets_chirurgie": [10],
+             "nombre_sejours_complets_obstetrique": [5],
+             "nombre_journees_completes_ssr": [500],
+             "nombre_journees_partiels_ssr": [1],
+             "nombre_journees_complete_psy": [1],
+             "nombre_journées_partielles_psy": [1]
+             })
+        pd.testing.assert_frame_equal(activités_agrégées, activités_agrégées_attendues)
 
+    def test_renvoit_les_activites_d_un_etablissement_pour_deux_annees(self) -> None:
+        # GIVEN
+        activites_entite_juridique_sanitaires = pd.DataFrame(
+            {
+                "annee": ["2020", "2021"],
+                "numero_finess_entite_juridique": ["111111111", "111111111"],
+                "numero_finess_etablissement_territorial": ["222222222", "222222222"],
+                "nombre_sejours_partiels_medecine": [10, 20],
+                "nombre_passages_urgences": [1000, 2000],
+                "nombre_sejours_partiels_obstetrique": [1, 2],
+                "nombre_sejours_partiels_chirurgie": [2, 4],
+                "nombre_sejours_complets_medecine": [20, 20],
+                "nombre_sejours_complets_chirurgie": [10, 10],
+                "nombre_sejours_complets_obstetrique": [5, 15],
+                "nombre_journees_completes_ssr": [500, 1000],
+                "nombre_journees_partiels_ssr": [1, 1],
+                "nombre_journees_complete_psy": [1, 0],
+                "nombre_journées_partielles_psy": [1, 2]
+            }
+        )
 
-def test_renvoit_les_activites_d_un_etablissement_pour_deux_annees():
-    # GIVEN
-    activites_entite_juridique_sanitaires = pd.DataFrame(
-        {
-            "annee": ["2020", "2021"],
-            "numero_finess_entite_juridique": ["111111111", "111111111"],
-            "numero_finess_etablissement_territorial": ["222222222", "222222222"],
-            "nombre_sejours_partiels_medecine": [10, 20],
-        }
-    )
+        # WHEN
+        activités_agrégées = agrège_les_activités_dans_les_entites_juridiques(activites_entite_juridique_sanitaires)
 
-    # WHEN
-    activités_agrégées = agrège_les_activités_dans_les_entites_juridiques(activites_entite_juridique_sanitaires)
+        # THEN
+        activités_agrégées_attendues = pd.DataFrame(
+            {"annee": ["2020", "2021"],
+             "numero_finess_entite_juridique": ["111111111", "111111111"],
+             "nombre_sejours_partiels_medecine": [10, 20],
+             "nombre_passages_urgences": [1000, 2000],
+             "nombre_sejours_partiels_obstetrique": [1, 2],
+             "nombre_sejours_partiels_chirurgie": [2, 4],
+             "nombre_sejours_complets_medecine": [20, 20],
+             "nombre_sejours_complets_chirurgie": [10, 10],
+             "nombre_sejours_complets_obstetrique": [5, 15],
+             "nombre_journees_completes_ssr": [500, 1000],
+             "nombre_journees_partiels_ssr": [1, 1],
+             "nombre_journees_complete_psy": [1, 0],
+             "nombre_journées_partielles_psy": [1, 2]
+             }
+        )
+        pd.testing.assert_frame_equal(activités_agrégées, activités_agrégées_attendues)
 
-    # THEN
-    activités_agrégées_attendues = pd.DataFrame(
-        {"annee": ["2020", "2021"], "numero_finess_entite_juridique": ["111111111", "111111111"], "nombre_sejours_partiels_medecine": [10, 20]}
-    )
-    pd.testing.assert_frame_equal(activités_agrégées, activités_agrégées_attendues)
+    def test_renvoit_les_activites_de_deux_etablissements_pour_une_annee(self) -> None:
+        # GIVEN
+        activites_entite_juridique_sanitaires = pd.DataFrame(
+            {
+                "annee": ["2020", "2020"],
+                "numero_finess_entite_juridique": ["111111111", "111111111"],
+                "numero_finess_etablissement_territorial": ["222222222", "333333333"],
+                "nombre_sejours_partiels_medecine": [10, 20],
+                "nombre_passages_urgences": [1000, 2000],
+                "nombre_sejours_partiels_obstetrique": [1, 2],
+                "nombre_sejours_partiels_chirurgie": [2, 4],
+                "nombre_sejours_complets_medecine": [20, 20],
+                "nombre_sejours_complets_chirurgie": [10, 10],
+                "nombre_sejours_complets_obstetrique": [5, 15],
+                "nombre_journees_completes_ssr": [500, 1000],
+                "nombre_journees_partiels_ssr": [1, 1],
+                "nombre_journees_complete_psy": [1, 0],
+                "nombre_journées_partielles_psy": [1, 2]
+            }
+        )
 
+        # WHEN
+        activités_agrégées = agrège_les_activités_dans_les_entites_juridiques(activites_entite_juridique_sanitaires)
 
-def test_renvoit_les_activites_de_deux_etablissements_pour_une_annee():
-    # GIVEN
-    activites_entite_juridique_sanitaires = pd.DataFrame(
-        {
-            "annee": ["2020", "2020"],
-            "numero_finess_entite_juridique": ["111111111", "111111111"],
-            "numero_finess_etablissement_territorial": ["222222222", "333333333"],
-            "nombre_sejours_partiels_medecine": [10, 20],
-        }
-    )
-
-    # WHEN
-    activités_agrégées = agrège_les_activités_dans_les_entites_juridiques(activites_entite_juridique_sanitaires)
-
-    # THEN
-    activités_agrégées_attendues = pd.DataFrame({"annee": ["2020"], "numero_finess_entite_juridique": ["111111111"], "nombre_sejours_partiels_medecine": [30]})
-    pd.testing.assert_frame_equal(activités_agrégées, activités_agrégées_attendues)
+        # THEN
+        activités_agrégées_attendues = pd.DataFrame(
+            {"annee": ["2020"],
+             "numero_finess_entite_juridique": ["111111111"],
+             "nombre_sejours_partiels_medecine": [30],
+             "nombre_passages_urgences": [3000],
+             "nombre_sejours_partiels_obstetrique": [3],
+             "nombre_sejours_partiels_chirurgie": [6],
+             "nombre_sejours_complets_medecine": [40],
+             "nombre_sejours_complets_chirurgie": [20],
+             "nombre_sejours_complets_obstetrique": [20],
+             "nombre_journees_completes_ssr": [1500],
+             "nombre_journees_partiels_ssr": [2],
+             "nombre_journees_complete_psy": [1],
+             "nombre_journées_partielles_psy": [3]
+             })
+        pd.testing.assert_frame_equal(activités_agrégées, activités_agrégées_attendues)
