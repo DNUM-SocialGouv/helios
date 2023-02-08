012200921793f62d00ee6d4df8b90dfebfaca618
Clément Dubois <clement.dubois@octo.com>
2023-02-08 16:06:21 +0100

    (hel-353)/fix last migration

--- database/migrations/1675867340170-RenommerColonnePassageUrgenceActivitesEntiteJuridique.ts
+++ database/migrations/1675867340170-RenommerColonnePassageUrgenceActivitesEntiteJuridique.ts
@@ -3,13 +3,13 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class RenommerColonnePassageUrgenceActivitesEntiteJuridique1675867340170 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-        ALTER TABLE helios.public.activite_sanitaire_entite_juridique
+        ALTER TABLE activite_sanitaire_entite_juridique
             RENAME COLUMN nombre_passages_urgences to nombre_passage_urgence`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-        ALTER TABLE helios.public.activite_sanitaire_entite_juridique
+        ALTER TABLE activite_sanitaire_entite_juridique
             RENAME COLUMN nombre_passage_urgence to nombre_passages_urgences`);
   }
 }
