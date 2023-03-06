aa794dff6e3f063a2f116f2995fb1f1d68385380
Clément Dubois <clement.dubois@octo.com>
2023-03-06 18:22:17 +0100

    add test bypass feature toggle

--- src/frontend/utils/featureToggle.test.ts
+++ src/frontend/utils/featureToggle.test.ts
@@ -3,7 +3,7 @@ import { buildFeatureToggleConfig, FEATURE_NAME } from "./featureToggle";
 describe("isFeatureEnabled", () => {
   it("doit renvoyer true si la fonctionnalité n'a pas de feature flipping", () => {
     // GIVEN
-    const isFeatureEnabled = buildFeatureToggleConfig([], []);
+    const isFeatureEnabled = buildFeatureToggleConfig([], [])();
 
     // WHEN
     const result = isFeatureEnabled(FEATURE_NAME.COMPTE_RESULTAT_EJ);
@@ -14,7 +14,7 @@ describe("isFeatureEnabled", () => {
 
   it("doit renvoyer false si la fonctionnalité est feature flippée et qu'elle n'est pas activée", () => {
     // GIVEN
-    const isFeatureEnabled = buildFeatureToggleConfig([FEATURE_NAME.COMPTE_RESULTAT_EJ], []);
+    const isFeatureEnabled = buildFeatureToggleConfig([FEATURE_NAME.COMPTE_RESULTAT_EJ], [])();
 
     // WHEN
     const result = isFeatureEnabled(FEATURE_NAME.COMPTE_RESULTAT_EJ);
@@ -25,7 +25,18 @@ describe("isFeatureEnabled", () => {
 
   it("doit renvoyer true si la fonctionnalité est feature flippée et qu'elle est pas activée", () => {
     // GIVEN
-    const isFeatureEnabled = buildFeatureToggleConfig([FEATURE_NAME.COMPTE_RESULTAT_EJ], [FEATURE_NAME.COMPTE_RESULTAT_EJ]);
+    const isFeatureEnabled = buildFeatureToggleConfig([FEATURE_NAME.COMPTE_RESULTAT_EJ], [FEATURE_NAME.COMPTE_RESULTAT_EJ])();
+
+    // WHEN
+    const result = isFeatureEnabled(FEATURE_NAME.COMPTE_RESULTAT_EJ);
+
+    // THEN
+    expect(result).toBe(true);
+  });
+
+  it("doit toujours renvoyer true si on bypass le feature toggle via une query", () => {
+    // GIVEN
+    const isFeatureEnabled = buildFeatureToggleConfig([FEATURE_NAME.COMPTE_RESULTAT_EJ], [])({ feature_toggle: "off" });
 
     // WHEN
     const result = isFeatureEnabled(FEATURE_NAME.COMPTE_RESULTAT_EJ);
