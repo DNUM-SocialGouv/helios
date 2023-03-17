c7148b70c0b5145cc7ffd903c2f57b7979fa42bb
miranda.lin-guignard <miranda.w.lin@gmail.com>
2023-03-17 11:21:30 +0100

    (hel-431) fix cartographie text

--- src/frontend/ui/home/Cartographie/Cartographie.tsx
+++ src/frontend/ui/home/Cartographie/Cartographie.tsx
@@ -5,17 +5,25 @@ import carteFrance from "./carte-france.svg";
 import "@gouvfr/dsfr/dist/component/card/card.min.css";
 import styles from "./Cartographie.module.css";
 import { GroupeBoutonRegions } from "./GroupeBoutonRegions";
+import { FEATURE_NAME } from "../../../utils/featureToggle";
 
 export const Cartographie = () => {
-  const { wording } = useDependencies();
+  const { wording, isFeatureEnabled } = useDependencies();
+  let title = "";
+  let cardTitle = wording.CARTOGRAPHIE;
+
+  if (!isFeatureEnabled(FEATURE_NAME.CARTO_FRANCE_METROPOLE)) {
+    title = wording.CARTOGRAPHIE;
+    cardTitle = wording.OFFRE_SANTÉ_PAR_REGION;
+  }
 
   return (
     <section aria-label={wording.CARTOGRAPHIE}>
-      <h2 className={styles["titre"]}>{wording.CARTOGRAPHIE}</h2>
+      <h2 className={styles["titre"]}>{title}</h2>
       <div className="fr-card fr-card--horizontal fr-card--no-arrow">
         <div className={"fr-card__body " + styles["body"]}>
           <div className="fr-card__content">
-            <h3 className="fr-card__title">{wording.OFFRE_SANTÉ_PAR_REGION}</h3>
+            <h3 className="fr-card__title">{cardTitle}</h3>
             <p className="fr-card__desc">{wording.CARTOGRAPHIE_DESCRIPTION}</p>
           </div>
           <div className="fr-card__footer">
