e56566ef42798a115b816a71cbed78f66068b4da
miranda.lin-guignard <miranda.w.lin@gmail.com>
2023-03-29 10:57:30 +0200

    (hel-415) remove unneccessary float

--- database/migrations/1680014929754-AjouteHAD.ts
+++ database/migrations/1680014929754-AjouteHAD.ts
@@ -14,10 +14,10 @@ export class AjouteHAD1680014929754 implements MigrationInterface {
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
             ALTER TABLE activite_sanitaire
-                DROP COLUMN nombre_sejours_had FLOAT;
+                DROP COLUMN nombre_sejours_had;
 
             ALTER TABLE activite_sanitaire_entite_juridique
-                DROP COLUMN nombre_sejours_had FLOAT;
+                DROP COLUMN nombre_sejours_had;
       `);
   }
 }
