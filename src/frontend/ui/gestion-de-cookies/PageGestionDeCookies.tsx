cd5835dd74c4609b2e68e7457371cd674ccf591e
miranda.lin-guignard <miranda.w.lin@gmail.com>
2023-04-05 11:32:24 +0200

    (hel-433) corrigé le titre et ajoute un lien

--- src/frontend/ui/gestion-de-cookies/PageGestionDeCookies.tsx
+++ src/frontend/ui/gestion-de-cookies/PageGestionDeCookies.tsx
@@ -1,11 +1,12 @@
 export const PageGestionDeCookies = () => {
   return (
     <>
-      <h1>Gestion de Cookies</h1>
+      <h1>Gestion des Cookies</h1>
       <h2>Pourquoi collecte-t-on des cookies?</h2>
       <p>
         Les cookies nous permettent de vous assurer une bonne expérience de navigation. Nous collectons uniquement un cookie Piano Analytics nous permettant de
-        mesurer le trafic, l’audience, les pages visitées, sur Helios lors de votre visite. Ce cookie est exemptés de consentement.
+        mesurer le trafic, l’audience, les pages visitées, sur Helios lors de votre visite. Ce cookie est{" "}
+        <a href="https://www.cnil.fr/fr/cookies-et-autres-traceurs/regles/cookies-solutions-pour-les-outils-de-mesure-daudience">exemptés de consentement </a>.
       </p>
       <p>
         Il nous permet d’analyser la façon dont vous utilisez le site internet. Les informations collectées anonymement permettent de mieux répondre à vos
