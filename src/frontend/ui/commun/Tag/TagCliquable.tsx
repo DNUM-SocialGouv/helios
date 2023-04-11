1c32a5d54ab3f8f401e1c89d4e29441c0cedb032
miranda.lin-guignard <miranda.w.lin@gmail.com>
2023-04-11 18:21:08 +0200

    (hel-445) reverted unused changes

--- src/frontend/ui/commun/Tag/TagCliquable.tsx
+++ src/frontend/ui/commun/Tag/TagCliquable.tsx
@@ -1,25 +1,17 @@
 import Link from "next/link";
 
-import styles from "./TagCliquable.module.css";
-
 type ActionneurDAccordéonProps = Readonly<{
   for: string;
   texteGras?: boolean;
   titre: string;
-  masquerET?: boolean;
 }>;
 
-export const TagCliquable = ({ for: identifiant, titre, texteGras = true, masquerET = false }: ActionneurDAccordéonProps) => {
-  let className = `fr-tag fr-text-label--grey ${texteGras ? "fr-text--bold" : ""} ` + styles["tag-actionnable"];
-  if (masquerET) {
-    className = "fr-tag fr-tag--sm";
-  }
-
+export const TagCliquable = ({ for: identifiant, titre, texteGras = true }: ActionneurDAccordéonProps) => {
   return (
     <Link
       aria-controls={identifiant}
       aria-expanded="false"
-      className={className}
+      className={`fr-tag fr-text-label--grey ${texteGras ? "fr-text--bold" : ""} \` + styles["tag-actionnable"]`}
       href="#"
       onClick={(event) => {
         event.preventDefault();
