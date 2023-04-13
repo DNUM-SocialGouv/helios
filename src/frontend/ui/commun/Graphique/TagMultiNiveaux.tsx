009a449daab696c05a7e640a770247e1834b54e1
Clément Dubois <clement.dubois@octo.com>
2023-04-13 17:21:03 +0200

    (hel-445) style

--- src/frontend/ui/commun/Graphique/TagMultiNiveaux.tsx
+++ src/frontend/ui/commun/Graphique/TagMultiNiveaux.tsx
@@ -1,9 +1,10 @@
-import { ReactElement } from "react";
+import Link from "next/link";
+import { ReactElement, useState } from "react";
 
-import stylesBlocAutorisationsEtCapacités from "../../établissement-territorial-sanitaire/bloc-autorisations/BlocAutorisationEtCapacitéSanitaire.module.css";
-import { BoutonLink } from "../Bouton/BoutonLink";
+import { useDependencies } from "../contexts/useDependencies";
 import { StringFormater } from "../StringFormater";
 import { Tag, TAG_SIZE, TagCliquable, TagGroup } from "../Tag";
+import style from "./TagMultiNiveaux.module.css";
 
 export type TagMultiNiveauxProps = {
   activites: Activite[];
@@ -39,28 +40,14 @@ export type Forme = {
 };
 
 export const TagMultiNiveaux = ({ activites }: TagMultiNiveauxProps): ReactElement => {
-  // TODO Change Masquer ET to a bouton + make text dynamic
-  // TODO BoutonLink needs to be reworked --> currently reusing what is inside TagCliquable, but with classname of buttons
-
   return (
-    <ul className={`${stylesBlocAutorisationsEtCapacités["liste-activités"]}`}>
+    <ul>
       {activites.map((activité) => (
         <li key={`activité-${activité.code}`}>
           <TagCliquable for={`autresActivités-accordion-${activité.code}`} titre={`${activité.libelle} [${activité.code}]`} />
-          <ul className=" fr-collapse niveau1" id={`autresActivités-accordion-${activité.code}`}>
+          <ul className="fr-collapse niveau1" id={`autresActivités-accordion-${activité.code}`}>
             {activité.modalites.map((modalité) => (
-              <li key={`modalité-${modalité.code}`}>
-                <TagCliquable
-                  for={`autorisations-accordion-${activité.code}-${modalité.code}`}
-                  texteGras={false}
-                  titre={`${modalité.libelle} [${modalité.code}]`}
-                />
-                <ul className="fr-collapse niveau2" id={`autorisations-accordion-${activité.code}-${modalité.code}`}>
-                  {modalité.forme.map((forme) => (
-                    <Modalites codeModalite={modalité.code} forme={forme} key={`forme-${forme.code}`} />
-                  ))}
-                </ul>
-              </li>
+              <Modalite codeActivite={activité.code} key={`modalité-${modalité.code}`} modalité={modalité} />
             ))}
           </ul>
         </li>
@@ -69,12 +56,44 @@ export const TagMultiNiveaux = ({ activites }: TagMultiNiveauxProps): ReactEleme
   );
 };
 
-const Modalites = ({ codeModalite, forme }: { codeModalite: string; forme: Forme }): ReactElement => {
+type AfficherLesEtProps = Readonly<{ for: string }>;
+const AfficherLesEt = ({ for: identifiant }: AfficherLesEtProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const textDisplay = isOpen ? `Masquer les ET concernés` : `Voir les ET concernés`;
+
+  return (
+    <button
+      aria-controls={identifiant}
+      aria-expanded={isOpen}
+      className={"fr-btn fr-btn--sm fr-btn--tertiary-no-outline fr-btn--icon-right " + (isOpen ? "fr-icon-arrow-up-s-line" : "fr-icon-arrow-down-s-line")}
+      data-fr-opened={isOpen}
+      onClick={() => {
+        setIsOpen(!isOpen);
+      }}
+    >
+      {textDisplay}
+    </button>
+  );
+};
+
+const Modalite = ({ modalité, codeActivite }: { codeActivite: string; modalité: Modalite }): ReactElement => {
+  return (
+    <li>
+      <TagCliquable for={`autorisations-accordion-${codeActivite}-${modalité.code}`} texteGras={false} titre={`${modalité.libelle} [${modalité.code}]`} />
+      <ul className="fr-collapse niveau2" id={`autorisations-accordion-${codeActivite}-${modalité.code}`}>
+        {modalité.forme.map((forme) => (
+          <Forme codeModalite={modalité.code} forme={forme} key={`forme-${forme.code}`} />
+        ))}
+      </ul>
+    </li>
+  );
+};
+const Forme = ({ codeModalite, forme }: { codeModalite: string; forme: Forme }): ReactElement => {
   return (
     <li>
       <Tag label={`${forme.libelle} [${forme.code}]`} size={TAG_SIZE.SM} withArrow />
-      <BoutonLink for={`autorisations-accordion-${codeModalite}-${forme.code}`} titre="ET" />
-      <ul className="fr-collapse niveau3" id={`autorisations-accordion-${codeModalite}-${forme.code}`}>
+      <AfficherLesEt for={`autorisations-accordion-${codeModalite}-${forme.code}`} />
+      <ul className={"fr-collapse niveau3" + style["liste-etablissements"]} id={`autorisations-accordion-${codeModalite}-${forme.code}`}>
         {forme.nom.map((autorisationEtablissement) => (
           <AutorisationEtablissement
             autorisations={autorisationEtablissement.autorisations}
@@ -88,10 +107,13 @@ const Modalites = ({ codeModalite, forme }: { codeModalite: string; forme: Forme
 };
 
 const AutorisationEtablissement = ({ etablissementTerritorial, autorisations }: AutorisationEtablissement): ReactElement => {
+  const { paths } = useDependencies();
   return (
-    <li>
-      <p>{etablissementTerritorial.numeroFiness + " - " + etablissementTerritorial.nom}</p>
-      <TagGroup label="authorisation">
+    <li className={style["etablissement"]}>
+      <Link href={paths.ÉTABLISSEMENT_TERRITORIAL_SANITAIRE + "/" + etablissementTerritorial.numeroFiness}>
+        {etablissementTerritorial.numeroFiness + " - " + etablissementTerritorial.nom}
+      </Link>
+      <TagGroup label="autorisations">
         {autorisations.map((autorisation) => {
           return (
             <Tag
