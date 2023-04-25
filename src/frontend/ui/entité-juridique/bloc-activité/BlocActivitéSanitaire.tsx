3e8ab54823bdf40ea262e1c7370d737447c7fc45
Clément Dubois <clement.dubois@octo.com>
2023-04-25 17:22:16 +0200

    (hel-161) Ne pas afficher l'indicateur vide pour le bloc activité + test + afficher les graphique s'il n'y a aucune donnée

--- src/frontend/ui/entité-juridique/bloc-activité/BlocActivitéSanitaire.tsx
+++ src/frontend/ui/entité-juridique/bloc-activité/BlocActivitéSanitaire.tsx
@@ -1,7 +1,6 @@
 import { FEATURE_NAME } from "../../../utils/featureToggle";
 import { Bloc } from "../../commun/Bloc/Bloc";
 import { useDependencies } from "../../commun/contexts/useDependencies";
-import { BlocIndicateurVide } from "../../commun/IndicateurGraphique/BlocIndicateurVide";
 import { GraphiqueNombreHAD } from "../../indicateur-métier/nombre-de-had/GraphiqueNombreHAD";
 import { GraphiqueNombreDeSejourMCO } from "../../indicateur-métier/nombre-de-sejour-mco/GraphiqueNombreDeSejourMCO";
 import { GraphiquePsySSR } from "../../indicateur-métier/nombre-journees-psy-ssr/GraphiquePsySSR";
@@ -15,25 +14,16 @@ type BlocActivitéSanitaireProps = Readonly<{
 export const BlocActivitéSanitaire = ({ entitéJuridiqueActivitéViewModel }: BlocActivitéSanitaireProps) => {
   const { wording } = useDependencies();
 
-  if (entitéJuridiqueActivitéViewModel.lesDonnéesActivitéNeSontPasRenseignées) {
-    return <BlocIndicateurVide title={wording.TITRE_BLOC_ACTIVITÉ} />;
-  }
-
   return (
     <Bloc titre={wording.TITRE_BLOC_ACTIVITÉ}>
       <ul className="indicateurs">
-        <GraphiqueNombreDeSejourMCO
-          estEntitéJuridique={true}
-          nombreDeSejourMCOViewModel={entitéJuridiqueActivitéViewModel.nombreDeSejourMCOViewModel}
-        ></GraphiqueNombreDeSejourMCO>
+        <GraphiqueNombreDeSejourMCO estEntitéJuridique={true} nombreDeSejourMCOViewModel={entitéJuridiqueActivitéViewModel.nombreDeSejourMCOViewModel} />
         <GraphiquePsySSR estEntitéJuridique={true} nombreJournéesPsySSRViewModel={entitéJuridiqueActivitéViewModel.nombreJourneesPsySSRViewModel} />
-        {entitéJuridiqueActivitéViewModel.nombreHADEstIlRenseigné() && <GraphiqueHAD entitéJuridiqueActivitéViewModel={entitéJuridiqueActivitéViewModel} />}
-        {entitéJuridiqueActivitéViewModel.nombrePassageUrgenceEstIlRenseigné() && (
-          <GraphiqueNombrePassageUrgence
-            estEntitéJuridique={true}
-            nombrePassageAuxUrgencesViewModel={entitéJuridiqueActivitéViewModel.nombreDePassageAuxUrgencesViewModel}
-          />
-        )}
+        <GraphiqueHAD entitéJuridiqueActivitéViewModel={entitéJuridiqueActivitéViewModel} />
+        <GraphiqueNombrePassageUrgence
+          estEntitéJuridique={true}
+          nombrePassageAuxUrgencesViewModel={entitéJuridiqueActivitéViewModel.nombreDePassageAuxUrgencesViewModel}
+        />
       </ul>
     </Bloc>
   );
