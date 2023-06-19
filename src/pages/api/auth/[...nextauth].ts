2d1facc0a7ed466f1a02e1c7f841fb7b69348664
NbyDev <n.benyahia@cat-amania.com>
2023-06-19 14:06:45 +0200

    (hel-494) first login version

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,65 @@
+import NextAuth from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+
+export default NextAuth({
+  pages: {
+    signIn: "/connexion"
+  },
+  providers: [
+    CredentialsProvider({
+      id: 'credentials',
+      name: 'Helios',
+      credentials: {
+        email: {
+          label: 'email',
+          type: 'email',
+        },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        try {
+
+          const authResponse = await fetch(`${process.env.NEXTAUTH_URL}/api/utilisateurs/login`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(credentials),
+          })
+console.log("authResponse", authResponse);
+
+          if (!authResponse.ok) {
+            return null;
+          }
+
+          const user = await authResponse.json();
+          return { id: user.utilisateur.code, ...user.utilisateur };
+        } catch (error) {
+          return null;
+        }
+      },
+    }),
+  ],
+  secret: "secret",
+  callbacks: {
+    async jwt({ token, user, account }) {
+      if (account && user) {
+        return {
+          ...token,
+          name: user.nom,
+        }
+      }
+
+      return token
+    },
+    async session({ session }) {
+      return session
+    },
+  },
+  theme: {
+    colorScheme: 'auto', // "auto" | "dark" | "light"
+    brandColor: '', // Hex color code #33FF5D
+  },
+  // Enable debug messages in the console if you are having problems
+  debug: process.env.NODE_ENV === 'development',
+})
