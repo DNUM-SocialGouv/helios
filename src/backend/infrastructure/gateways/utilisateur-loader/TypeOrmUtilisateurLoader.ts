116d58ad8b6486966debe983cce457bf612e2259
AnisHAKIMCATAMANIA <a.hakim@cat-amania.com>
2023-07-03 15:14:03 +0200

    (hel-503)  add bcrypt compare passeword

--- src/backend/infrastructure/gateways/utilisateur-loader/TypeOrmUtilisateurLoader.ts
+++ src/backend/infrastructure/gateways/utilisateur-loader/TypeOrmUtilisateurLoader.ts
@@ -1,8 +1,11 @@
 import { DataSource } from "typeorm";
-import { UtilisateurLoader } from "../../../métier/gateways/UtilisateurLoader";
-import { RésultatLogin } from "../../../métier/entities/Utilisateur/RésultatLogin";
-import { UtilisateurModel } from "../../../../../database/models/UtilisateurModel";
 
+import { UtilisateurModel } from "../../../../../database/models/UtilisateurModel";
+import { RésultatLogin } from "../../../métier/entities/Utilisateur/RésultatLogin";
+import { UtilisateurLoader } from "../../../métier/gateways/UtilisateurLoader";
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const bcrypt = require('bcrypt');
 
 export class TypeOrmUtilisateurLoader implements UtilisateurLoader {
     constructor(private readonly orm: Promise<DataSource>) { }
@@ -10,7 +13,7 @@ export class TypeOrmUtilisateurLoader implements UtilisateurLoader {
     async login(email: string, password: string): Promise<RésultatLogin> {
         const user = await (await this.orm).getRepository(UtilisateurModel).findOneBy({ email: email.trim() });
         if (user) {
-            return user?.password === password ? { utilisateur: user } : null
+            return bcrypt.compare(password, user.password) ? { utilisateur: user } : null
         } else {
             return null
         }
