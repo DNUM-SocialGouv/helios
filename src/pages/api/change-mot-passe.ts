5a5316dea8bf5112792dc6527c352d492bc476fe
AnisHAKIMCATAMANIA <a.hakim@cat-amania.com>
2023-07-05 13:11:40 +0200

    (hel-503) add  try catch

--- src/pages/api/change-mot-passe.ts
+++ src/pages/api/change-mot-passe.ts
@@ -4,17 +4,18 @@ import { changePasswordEndpoint } from "../../backend/infrastructure/controllers
 import { dependencies } from "../../backend/infrastructure/dependencies";
 
 export default async function handler(request: NextApiRequest, response: NextApiResponse) {
-  
+  try {
   if (request.method !== "POST") {
     response.status(405).send("Method not allowed");
   }
-
-  const { loginToken, password } = request.body;
-
-  const result = await changePasswordEndpoint(dependencies,loginToken, password); 
-  if (result) {
-    response.status(200).json(result);
-  } else {
-    response.status(400).json({'err':'error occured while changing passeword'}) 
-  }
+   const { loginToken, password } = request.body;
+   const result = await changePasswordEndpoint(dependencies,loginToken, password); 
+   if (result) {
+    return response.status(200).json(result);
+   } else {
+    return  response.status(400).json({'err':'error occured while changing passeword'}) 
+   }  
+ } catch (error) {
+  return response.status(500)
+ }
 }
