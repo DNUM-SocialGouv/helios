1c6d264af0c52c0538a30b8a557685653fe08cfd
AnisHAKIMCATAMANIA <a.hakim@cat-amania.com>
2023-07-06 15:12:37 +0200

    (hel-503) fix build error

--- src/frontend/ui/commun/validation.ts
+++ src/frontend/ui/commun/validation.ts
@@ -1,5 +1,5 @@
 export default function isEmail(emailAdress : string) : Boolean{
-    const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
   if (emailAdress.match(regex)) 
     return true; 
