2476d9ffa8b9108fc25b5c84e437ed7a5af048c6
Sébastien FERRE <s.ferre@cat-amania.com>
2023-07-06 17:36:58 +0200

    (hel-496) Script d'insertion des utilisateurs en base
    prise en compte des remarques du CI

--- datacrawler/import_utilisateurs.py
+++ datacrawler/import_utilisateurs.py
@@ -2,12 +2,12 @@ from datetime import datetime
 import hashlib
 from logging import Logger
 import pandas as pd
+from pandas import Series
 import sqlalchemy as db
-from sqlalchemy.engine import Engine, create_engine
-
+from sqlalchemy.engine import Engine, create_engine, Connection
+from sqlalchemy import Table
 
 from datacrawler.dependencies.dépendances import initialise_les_dépendances
-
 
 def import_des_utilisateurs(base_de_données: Engine, logger: Logger) -> None:
     utilisateur_data_file = variables_d_environnement["UTILISATEURS_DATA_PATH"]+"/Utilisateurs_Helios_importation.csv"
@@ -29,7 +29,7 @@ def import_des_utilisateurs(base_de_données: Engine, logger: Logger) -> None:
                 creer_utilisateur(connection, data, logger,db_users, db_roles, db_institutions)
 
 
-def modifier_utilisateur(connection, data, logger: Logger, db_users, db_roles, db_institutions) -> None:
+def modifier_utilisateur(connection: Connection, data: Series, logger: Logger, db_users: Table, db_roles: Table, db_institutions: Table) -> None:
     logger.info("Utilisateur en modification :" + data["E-mail"])
     get_institute_by_code = db.select([db_institutions.columns.inst_id]).filter_by(inst_code = data['Code institution'])
     institude_id = connection.execute(get_institute_by_code).fetchone()[0]
@@ -45,7 +45,7 @@ def modifier_utilisateur(connection, data, logger: Logger, db_users, db_roles, d
                 ut_role=id_role,
                 ut_date_modification=datetime.now()))
 
-def creer_utilisateur(connection, data, logger: Logger, db_users, db_roles, db_institutions) -> None:
+def creer_utilisateur(connection: Connection, data: Series, logger: Logger, db_users: Table, db_roles: Table, db_institutions: Table) -> None:
     logger.info("Utilisateur en création :" + data["E-mail"])
     hashing = hashlib.sha256()
     hashing.update(b"HeliosConnect-")
