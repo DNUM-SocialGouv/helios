d2d8e1802cce15fb68204be27a84d67351049ed4
Sébastien Ferré <116794000+s-ferre-cat@users.noreply.github.com>
2023-07-06 19:19:24 +0200

    Hel 496/script insertion utilisateurs (#337)
    * (hel-426) Fix taux de caf si pas de données (#330)
    
    * (hel-495) Interconnexion Helios
    
    correction script migration typeORM
    
    * (hel-496) Script d'insertion des utilisateurs en base
    
    ajout de la variable environnement UTILISATEURS_DATA_PATH indiquant le chemin du fichier à importer
    
    * (hel-496) Script d'insertion des utilisateurs en base
    
    modification du mdp par defaut ajout du code institution de chaque utilisateur
    
    * (hel-496) Script d'insertion des utilisateurs en base
    
    ajout du repertoir .vscode au gitignore
    
    * (hel-496) Script d'insertion des utilisateurs en base
    
    prise en compte des changement de la table institution
    
    * (hel-496) Script d'insertion des utilisateurs en base
    
    prise en compte des rremarque du CI
    
    * (hel-496) Script d'insertion des utilisateurs en base
    
    prise en compte des remarques du CI
    
    * (hel-496) Script d'insertion des utilisateurs en base
    
    prise en compte des remarques du CI
    
    * (hel-496) Script d'insertion des utilisateurs en base
    
    prise en compte des remarques du CI
    
    * (hel-496) Script d'insertion des utilisateurs en base
    
    prise en compte des remarques du CI

---
+++ datacrawler/import_utilisateurs.py
@@ -0,0 +1,73 @@
+from datetime import datetime
+import hashlib
+from logging import Logger
+import pandas as pd
+from pandas import Series
+import sqlalchemy as db
+from sqlalchemy.engine import Engine, create_engine, Connection
+from sqlalchemy import select
+from sqlalchemy import Table
+
+from datacrawler.dependencies.dépendances import initialise_les_dépendances
+
+def import_des_utilisateurs(base_de_données: Engine, logger: Logger) -> None:
+    utilisateur_data_file = variables_d_environnement["UTILISATEURS_DATA_PATH"]+"/Utilisateurs_Helios_importation.csv"
+    metadata = db.MetaData()
+
+    data_frame = pd.read_csv(utilisateur_data_file,encoding='utf-8', delimiter=";").astype(str)
+    db_users = db.Table('utilisateur', metadata, autoload=True, autoload_with=base_de_données)
+    db_roles = db.Table('role', metadata, autoload=True, autoload_with=base_de_données)
+    db_institutions = db.Table('institution', metadata, autoload=True, autoload_with=base_de_données)
+    with base_de_données.begin() as connection:
+        for user in data_frame.iterrows():
+            data = user[1].astype(str)
+            query = db_users.select().where(db_users.columns.ut_email == data["E-mail"])
+            rst = connection.execute(query).fetchone()
+
+            if rst:
+                modifier_utilisateur(connection, data, logger,db_users, db_roles, db_institutions)
+            else:
+                creer_utilisateur(connection, data, logger,db_users, db_roles, db_institutions)
+
+
+def modifier_utilisateur(connection: Connection, data: Series, logger: Logger, db_users: Table, db_roles: Table, db_institutions: Table) -> None:
+    logger.info("Utilisateur en modification :" + data["E-mail"])
+    get_institute_by_code = select(db_institutions.columns.inst_id).where(db_institutions.columns.inst_code == data['Code institution'])
+    institude_id = connection.execute(get_institute_by_code).fetchone()[0]
+    get_role_by_code = select(db_roles.columns.role_id).where(db_roles.columns.role_code == data['Code Rôle'])
+    id_role = connection.execute(get_role_by_code).fetchone()[0]
+    connection.execute(
+        db_users.update()
+        .where(db_users.columns.ut_email == data["E-mail"])
+        .values(ut_nom=data["Nom"],
+                ut_prenom=data["Prénom"],
+                ut_institution=institude_id,
+                ut_actif=True,
+                ut_role=id_role,
+                ut_date_modification=datetime.now()))
+
+def creer_utilisateur(connection: Connection, data: Series, logger: Logger, db_users: Table, db_roles: Table, db_institutions: Table) -> None:
+    logger.info("Utilisateur en création :" + data["E-mail"])
+    hashing = hashlib.sha256()
+    hashing.update(b"HeliosConnect-")
+    get_institute_by_code = select(
+        [db_institutions.columns.inst_id, db_institutions.columns.inst_code_geo]
+    ).where(db_institutions.columns.inst_code == data['Code institution'])
+    rst = connection.execute(get_institute_by_code).fetchone()
+    get_role_by_code = select(db_roles.columns.role_id).where(db_roles.columns.role_code == data['Code Rôle'])
+    id_role = connection.execute(get_role_by_code).fetchone()[0]
+    hashing.update(str(rst[1]).encode('utf-8'))
+    connection.execute(db_users.insert().values(ut_nom=data["Nom"],
+                                            ut_prenom=data["Prénom"],
+                                            ut_email=data["E-mail"],
+                                            ut_institution=rst[0],
+                                            ut_actif=True,
+                                            ut_role=id_role,
+                                            ut_password=hashing.hexdigest(),
+                                            ut_date_creation=datetime.now()))
+
+if __name__ == "__main__":
+    logger_helios, variables_d_environnement = initialise_les_dépendances()
+    base_de_données_helios = create_engine(variables_d_environnement["DATABASE_URL"])
+
+    import_des_utilisateurs(base_de_données_helios, logger_helios)
