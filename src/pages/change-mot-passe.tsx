2dedd87915c56ca98249dd2d77b46f02eebb7672
schlaifa <s.chlaifa@cat-amania.com>
2023-07-27 09:31:11 +0200

    (hel-512) check expired token

--- src/pages/change-mot-passe.tsx
+++ src/pages/change-mot-passe.tsx
@@ -1,5 +1,17 @@
+import { useRouter } from "next/router";
+
+import { checkToken } from "../backend/jwtHelper";
 import { PageChangeMotPasse } from "../frontend/ui/change-mot-passe/PageChangeMotPasse";
+import { Page404 } from "../frontend/ui/erreurs/Page404";
 
 export default function MotPasseOublie() {
-  return <PageChangeMotPasse />;
+  const router = useRouter();
+  const { loginToken } = router.query;
+  const info = checkToken(loginToken as string);
+  if (info?.email) {
+    return <PageChangeMotPasse />;
+  } else {
+    return <Page404 />
+  }
+
 }
