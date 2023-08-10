c690e34a63f71d06ecd097837f07570a23b33279
schlaifa <s.chlaifa@cat-amania.com>
2023-08-10 14:50:09 +0200

    (hel-521) fix code issues

--- src/pages/change-mot-passe.tsx
+++ src/pages/change-mot-passe.tsx
@@ -1,17 +1,5 @@
-import { useRouter } from "next/router";
-
-import { checkToken } from "../backend/jwtHelper";
 import { PageChangeMotPasse } from "../frontend/ui/change-mot-passe/PageChangeMotPasse";
-import { Page404 } from "../frontend/ui/erreurs/Page404";
 
 export default function MotPasseOublie() {
-  const router = useRouter();
-  const { loginToken } = router.query;
-  const info = checkToken(loginToken as string);
-  if (info?.email) {
-    return <PageChangeMotPasse />;
-  } else {
-    return <Page404 />
-  }
-
+  return <PageChangeMotPasse />;
 }
