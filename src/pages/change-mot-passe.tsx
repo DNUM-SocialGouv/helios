a00730c3fc2b5eec765330dc71f8c077880c91b1
schlaifa <s.chlaifa@cat-amania.com>
2023-08-10 17:17:26 +0200

    (hel-512) url expires within 72 hours

--- src/pages/change-mot-passe.tsx
+++ src/pages/change-mot-passe.tsx
@@ -1,5 +1,42 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
 import { PageChangeMotPasse } from "../frontend/ui/change-mot-passe/PageChangeMotPasse";
+import { useChangeMdp } from "../frontend/ui/change-mot-passe/useChangeMdp";
+import Spinner from "../frontend/ui/commun/Spinner/Spinner";
+import { Page404 } from "../frontend/ui/erreurs/Page404";
 
 export default function MotPasseOublie() {
-  return <PageChangeMotPasse />;
+  const router = useRouter();
+  const [token, setToken] = useState('');
+
+  const {
+    checkTokenService,
+    validToken,
+    isChecking,
+  } = useChangeMdp();
+
+
+  useEffect(() => {
+    if (router.isReady) {
+      const { loginToken } = router.query;
+      setToken(loginToken as string);
+    }
+  }, [router.isReady]);
+
+  if (token) {
+
+    checkTokenService(token);
+    return (
+      <>
+        {isChecking ? <Spinner /> : validToken ? (
+          <PageChangeMotPasse />
+        ) : (
+          <Page404 />
+        )}
+      </>
+    );
+  } else {
+    return null
+  }
 }
