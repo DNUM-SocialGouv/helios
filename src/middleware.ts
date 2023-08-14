7eddf21dd785c9d3cb0d48f8827ad1e30260be5f
schlaifa <s.chlaifa@cat-amania.com>
2023-08-14 11:10:57 +0200

    fix code issues

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,25 +8,25 @@ export default withAuth(
   function middleware(request: NextRequest) {
     const allowedIps = (process.env["WHITELIST_IP"] as string).split(",");
     const userIp = request.headers.get("x-forwarded-for") || (request.ip as string);
-  
+
     // Pour l'environnement de développement
     if (userIp === undefined) {
       return NextResponse.next();
     }
-  
+
     if (!allowedIps.includes(userIp)) {
       const url = request.nextUrl.clone();
       url.pathname = "/inaccessible";
       return NextResponse.rewrite(url);
     }
-  
+
     return NextResponse.next();
   },
   {
-  pages: {
-    signIn: "/connexion"
-  }
-})
+    pages: {
+      signIn: "/connexion"
+    }
+  })
 
 export const config = {
   matcher: [
@@ -36,6 +36,8 @@ export const config = {
     "/entite-juridique/:path*",
     "/etablissement-territorial-sanitaire/:path*",
     "/etablissement-territorial-medico-social/:path*",
+    "/profile",
+    "/change-mot-passe",
     "/accessibilite",
     "/donnees-personnelles",
     "/mentions-legales",
--- src/pages/change-mot-passe.tsx
+++ src/pages/change-mot-passe.tsx
@@ -1,42 +1,16 @@
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-
 import { ChangePwdPage } from "../frontend/ui/change-mot-passe/ChangePwdPage";
-import { useChangeMdp } from "../frontend/ui/change-mot-passe/useChangeMdp";
-import Spinner from "../frontend/ui/commun/Spinner/Spinner";
-import { Page404 } from "../frontend/ui/erreurs/Page404";
+import { useDependencies } from "../frontend/ui/commun/contexts/useDependencies";
+import { useBreadcrumb } from "../frontend/ui/commun/hooks/useBreadcrumb";
 
 export default function MotPasseOublie() {
-  const router = useRouter();
-  const [token, setToken] = useState('');
+  const { wording } = useDependencies();
 
-  const {
-    checkTokenService,
-    validToken,
-    isChecking,
-  } = useChangeMdp();
-
-
-  useEffect(() => {
-    if (router.isReady) {
-      const { loginToken } = router.query;
-      setToken(loginToken as string);
-    }
-  }, [router.isReady]);
-
-  if (token) {
-
-    checkTokenService(token);
-    return (
-      <>
-        {isChecking ? <Spinner /> : validToken ? (
-          <ChangePwdPage />
-        ) : (
-          <Page404 />
-        )}
-      </>
-    );
-  } else {
-    return null
-  }
+  useBreadcrumb([
+    {
+      label: wording.CHANGEMENT_MOT_PASSE_TITRE,
+      path: "",
+    },
+  ]);
+  return <ChangePwdPage />;
 }
+
