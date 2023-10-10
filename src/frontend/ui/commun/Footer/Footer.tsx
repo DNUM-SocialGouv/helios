67e5d926cc62c1dbc87a4224ec0104702aa79a87
schlaifa <s.chlaifa@cat-amania.com>
2023-10-10 11:05:38 +0200

    add new version

--- src/frontend/ui/commun/Footer/Footer.tsx
+++ src/frontend/ui/commun/Footer/Footer.tsx
@@ -99,7 +99,7 @@ export const Footer = () => {
               </a>
             </li>
             <li className="fr-footer__bottom-item">
-              <span className="fr-footer__bottom-link"> v1.0.0</span>
+              <span className="fr-footer__bottom-link"> v1.1.0</span>
             </li>
           </ul>
           <div className="fr-footer__bottom-copy">
