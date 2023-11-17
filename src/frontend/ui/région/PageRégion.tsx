e77d1b84662a5dad749c578ad90b7306bd662bc8
schlaifa <s.chlaifa@cat-amania.com>
2023-11-17 14:12:54 +0100

    (hel-555) cartographie outre mer regions

--- src/frontend/ui/région/PageRégion.tsx
+++ src/frontend/ui/région/PageRégion.tsx
@@ -1,12 +1,15 @@
 import Head from "next/head";
+import { useEffect, useState } from "react";
 
 import { useDependencies } from "../commun/contexts/useDependencies";
 import { useBreadcrumb } from "../commun/hooks/useBreadcrumb";
 import { BandeauCookies } from "../home/Bandeau/BandeauCookies";
-import { régions } from "./régions";
+import { GroupeOutreMerBoutonRegions } from "../home/Cartographie/GroupeOutreMerButtonRegions";
+import { régions, outreMerRegionsList } from "./régions";
 
 export const PageRégion = ({ région }: { région: string }) => {
   const labelDeLaRégion = régions[région].label;
+  const [arsRegions, setArsRegions] = useState(outreMerRegionsList);
   const { wording } = useDependencies();
   useBreadcrumb([
     {
@@ -15,6 +18,11 @@ export const PageRégion = ({ région }: { région: string }) => {
     },
   ]);
 
+  useEffect(() => {
+    const filtredList = outreMerRegionsList.filter((region) => region.label !== labelDeLaRégion);
+    setArsRegions(filtredList);
+  }, [labelDeLaRégion])
+
   return (
     <main className="fr-container--fluid">
       <Head>
@@ -22,6 +30,7 @@ export const PageRégion = ({ région }: { région: string }) => {
       </Head>
       <section>
         <h1 className="fr-container">{labelDeLaRégion}</h1>
+        {labelDeLaRégion !== 'France métropolitaine' && <GroupeOutreMerBoutonRegions regionsList={arsRegions} />}
         <BandeauCookies texte={wording.COOKIES_ATLASSANTÉ} />
         <iframe allowFullScreen height="650" src={régions[région].source} title={wording.régionAtlasSanté(régions[région].label)} width="100%" />
       </section>
--- src/frontend/ui/région/régions.ts
+++ src/frontend/ui/région/régions.ts
@@ -5,8 +5,21 @@ type Régions = Readonly<{
   }>;
 }>;
 
+export type OutreMerRegion = Readonly<{
+  key: string;
+  label: string;
+  text: string;
+  source: string;
+}>;
+
 const LIEN_FRANCE_METROPOLITAINE = "https://carto.atlasante.fr/1/helios_metropole.map";
 const LIEN_OUTRE_MER = "https://carto.atlasante.fr/1/helios_outremer.map";
+
+const LIEN_ARS1_GUADELOUPE = "https://carto.atlasante.fr/1/helios_outremer.map?object=R%C3%A9gion;insee_reg;01";
+const LIEN_ARS2_GUYANE = "https://carto.atlasante.fr/1/helios_outremer.map?object=R%C3%A9gion;insee_reg;02";
+const LIEN_ARS3_LA_REUNION = "https://carto.atlasante.fr/1/helios_outremer.map?object=R%C3%A9gion;insee_reg;03";
+const LIEN_ARS4_MATINIQUE = "https://carto.atlasante.fr/1/helios_outremer.map?object=R%C3%A9gion;insee_reg;04";
+const LIEN_ARS5_MAYOTTE = "https://carto.atlasante.fr/1/helios_outremer.map?object=R%C3%A9gion;insee_reg;05";
 
 export const régions: Régions = {
   "france-metropolitaine": {
@@ -17,4 +30,57 @@ export const régions: Régions = {
     label: "Outre-mer (DROM-COM)",
     source: `${LIEN_OUTRE_MER}`,
   },
+  "ARS1": {
+    label: "Carte de ARS 1",
+    source: `${LIEN_ARS1_GUADELOUPE}`,
+  },
+  "ARS2": {
+    label: "Carte de ARS 2",
+    source: `${LIEN_ARS2_GUYANE}`,
+  },
+  "ARS3": {
+    label: "Carte de ARS 3",
+    source: `${LIEN_ARS3_LA_REUNION}`,
+  },
+  "ARS4": {
+    label: "Carte de ARS 4",
+    source: `${LIEN_ARS4_MATINIQUE}`,
+  },
+  "ARS5": {
+    label: "Carte de ARS 5",
+    source: `${LIEN_ARS5_MAYOTTE}`,
+  },
 };
+
+export const outreMerRegionsList: OutreMerRegion[] = [
+  {
+    key: 'ARS1',
+    label: 'Carte de ARS 1',
+    source: `${LIEN_ARS1_GUADELOUPE}`,
+    text: 'Carte Guadeloupe'
+  },
+  {
+    key: 'ARS2',
+    label: 'Carte de ARS 2',
+    source: `${LIEN_ARS2_GUYANE}`,
+    text: 'Carte Guyane'
+  },
+  {
+    key: 'ARS3',
+    label: 'Carte de ARS 3',
+    source: `${LIEN_ARS3_LA_REUNION}`,
+    text: 'Carte La Réunion'
+  },
+  {
+    key: 'ARS4',
+    label: 'Carte de ARS 4',
+    source: `${LIEN_ARS4_MATINIQUE}`,
+    text: 'Carte Martinique'
+  },
+  {
+    key: 'ARS5',
+    label: 'Carte de ARS 5',
+    source: `${LIEN_ARS5_MAYOTTE}`,
+    text: 'Carte Mayotte'
+  }
+]
