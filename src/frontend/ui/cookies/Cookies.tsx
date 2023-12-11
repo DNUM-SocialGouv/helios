dc286cb042e142a925ef88d843bf66fa526f425e
Adolato <a.elechi@cat-amania.com>
2023-12-12 00:48:38 +0100

    (hel-571) hel-571/wording-footer-pages cookies

--- src/frontend/ui/cookies/Cookies.tsx
+++ src/frontend/ui/cookies/Cookies.tsx
@@ -1,15 +1,21 @@
 import Link from "next/link";
+import { ChangeEvent, useState } from "react";
 
 import styles from "./Cookies.module.css";
 
 export const Cookies = () => {
+  const [allowCookies, setAllowCookies] = useState("");
+
+  const onOptionChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setAllowCookies(e.target.value);
+  };
   return (
     <>
-      <button aria-controls="fr-modal-2" data-fr-opened="true" title="Supprimer">
+      <button aria-controls="fr-modal-cookies" data-fr-opened="true" title="Supprimer">
         <span aria-hidden="true" className="fr-icon-delete-line"></span>
       </button>
 
-      <dialog aria-labelledby="fr-modal-2-title" className={"fr-modal " + styles["unclickable-area"]} id="fr-modal-2">
+      <dialog aria-labelledby="fr-modal-cookies-title" className={"fr-modal " + styles["unclickable-area"]} id="fr-modal-cookies">
         <div className="fr-container fr-container--fluid fr-container-md ">
           <div className={"fr-grid-row fr-grid-row--left " + styles["cookies-modal"]}>
             <div className="fr-col-12 fr-col-md-8 fr-col-lg-6">
@@ -21,13 +27,13 @@ export const Cookies = () => {
                 <div className={"fr-modal__footer " + styles["fr-modal__footer"]}>
                   <ul className="fr-btns-group fr-btns-group--right fr-btns-group--inline-reverse fr-btns-group--inline-lg fr-btns-group--icon-left">
                     <li>
-                      <button aria-controls="fr-modal-2" className="fr-btn">
+                      <button aria-controls="fr-modal-cookies" className="fr-btn">
                         Tout accepter
                       </button>
                     </li>
 
                     <li>
-                      <button aria-controls="fr-modal-2" className="fr-btn">
+                      <button aria-controls="fr-modal-cookies" className="fr-btn">
                         Tout refuser
                       </button>
                     </li>
@@ -49,13 +55,18 @@ export const Cookies = () => {
         </div>
       </dialog>
 
-      <dialog className="fr-modal" id="fr-modal-privacyPolicy">
+      <dialog className={`fr-modal ${allowCookies === "" ? styles["unclickable-area"] : "fr-modal--opened"} `} id="fr-modal-privacyPolicy">
         <div className="fr-container fr-container--fluid fr-container-md">
           <div className="fr-grid-row fr-grid-row--center">
             <div className="fr-col-12 fr-col-md-8">
               <div className="fr-modal__body">
                 <div className="fr-modal__header">
-                  <button aria-controls="fr-modal-privacyPolicy" className="fr-btn--close fr-btn" title="Fermer la fenêtre modale">
+                  <button
+                    aria-controls={allowCookies === "" ? "fr-modal-cookies" : "fr-modal-privacyPolicy"}
+                    className="fr-btn--close fr-btn"
+                    title="Fermer la fenêtre modale"
+                    {...(allowCookies === "" ? { "data-fr-opened": "true" } : {})}
+                  >
                     Fermer
                   </button>
                 </div>
@@ -66,8 +77,19 @@ export const Cookies = () => {
                     <fieldset aria-labelledby="radio-inline-legend radio-inline-messages" className={styles["fr-fieldset"]}>
                       <div className={`${styles["fr-fieldset__element"]} ${styles["fr-fieldset__element--inline"]}`}>
                         <div className={styles["fr-radio-group"]}>
-                          <input className={`${styles["fr-radio-groupInputRadio"]}  }`} id="radio-inline-1" name="radio-inline" type="radio" />
-                          <label className={`${styles["fr-radio-groupInputRadioLabel"]} ${styles["InputRadioChecked"]}`} htmlFor="radio-inline-1">
+                          <input
+                            checked={allowCookies === "true"}
+                            className={`${styles["fr-radio-groupInputRadio"]}  }`}
+                            id="radio-inline-1"
+                            name="radio-inline"
+                            onChange={onOptionChange}
+                            type="radio"
+                            value="true"
+                          />
+                          <label
+                            className={`${styles["fr-radio-groupInputRadioLabel"]} ${allowCookies === "true" ? styles["InputRadioChecked"] : ""}`}
+                            htmlFor="radio-inline-1"
+                          >
                             Tout accepter
                           </label>
                         </div>
@@ -77,8 +99,19 @@ export const Cookies = () => {
                       </div>
                       <div className={`${styles["fr-fieldset__element"]} ${styles["fr-fieldset__element--inline"]}`}>
                         <div className={styles["fr-radio-group"]}>
-                          <input checked={true} className={styles["fr-radio-groupInputRadio"]} id="radio-inline-2" name="radio-inline" type="radio" />
-                          <label className={`${styles["fr-radio-groupInputRadioLabel"]} `} htmlFor="radio-inline-2">
+                          <input
+                            checked={allowCookies === "false"}
+                            className={styles["fr-radio-groupInputRadio"]}
+                            id="radio-inline-2"
+                            name="radio-inline"
+                            onChange={onOptionChange}
+                            type="radio"
+                            value="false"
+                          />
+                          <label
+                            className={`${styles["fr-radio-groupInputRadioLabel"]}  ${allowCookies === "false" ? styles["InputRadioChecked"] : ""}`}
+                            htmlFor="radio-inline-2"
+                          >
                             Tout refuser
                           </label>
                         </div>
@@ -87,14 +120,14 @@ export const Cookies = () => {
                     <p className={styles["label_input"]}>Préférences pour tous les services</p>
                   </div>
 
-                  <div className={`${styles["row-input"]} ${styles["row-input-sep"]}`}>
+                  <div className={`${styles["row-input"]} ${styles["row-input-sep-2"]}`}>
                     <h2 className={`fr-modal__title ${styles["fr-modal__title"]} }`}>Cookies obligatoires</h2>
 
                     <fieldset aria-labelledby="radio-inline-legend radio-inline-messages" className={styles["fr-fieldset"]}>
                       <div className={`${styles["fr-fieldset__element"]} ${styles["fr-fieldset__element--inline"]}`}>
                         <div className={styles["fr-radio-group"]}>
                           <input className={`${styles["fr-radio-groupInputRadio"]}  }`} disabled id="radio-inline-3" name="radio-inline" type="radio" />
-                          <label className={`${styles["fr-radio-groupInputRadioLabel"]} ${styles["InputRadioChecked"]}`} htmlFor="radio-inline-3">
+                          <label className={`${styles["fr-radio-groupInputRadioLabel"]} `} htmlFor="radio-inline-3">
                             Autoriser
                           </label>
                         </div>
@@ -113,8 +146,18 @@ export const Cookies = () => {
                     <fieldset aria-labelledby="radio-inline-legend radio-inline-messages" className={styles["fr-fieldset"]}>
                       <div className={`${styles["fr-fieldset__element"]} ${styles["fr-fieldset__element--inline"]}`}>
                         <div className={styles["fr-radio-group"]}>
-                          <input className={`${styles["fr-radio-groupInputRadio"]}  }`} id="radio-inline-1" name="radio-inline" type="radio" />
-                          <label className={`${styles["fr-radio-groupInputRadioLabel"]} ${styles["InputRadioChecked"]}`} htmlFor="radio-inline-1">
+                          <input
+                            className={`${styles["fr-radio-groupInputRadio"]}  }`}
+                            id="radio-inline-1"
+                            name="radio-inline"
+                            onChange={onOptionChange}
+                            type="radio"
+                            value="true"
+                          />
+                          <label
+                            className={`${styles["fr-radio-groupInputRadioLabel"]}  ${allowCookies === "true" ? styles["InputRadioChecked"] : ""}`}
+                            htmlFor="radio-inline-1"
+                          >
                             Tout accepter
                           </label>
                         </div>
@@ -124,8 +167,18 @@ export const Cookies = () => {
                       </div>
                       <div className={`${styles["fr-fieldset__element"]} ${styles["fr-fieldset__element--inline"]}`}>
                         <div className={styles["fr-radio-group"]}>
-                          <input checked={true} className={styles["fr-radio-groupInputRadio"]} id="radio-inline-2" name="radio-inline" type="radio" />
-                          <label className={`${styles["fr-radio-groupInputRadioLabel"]} `} htmlFor="radio-inline-2">
+                          <input
+                            className={styles["fr-radio-groupInputRadio"]}
+                            id="radio-inline-2"
+                            name="radio-inline"
+                            onChange={onOptionChange}
+                            type="radio"
+                            value="false"
+                          />
+                          <label
+                            className={`${styles["fr-radio-groupInputRadioLabel"]}  ${allowCookies === "false" ? styles["InputRadioChecked"] : ""}`}
+                            htmlFor="radio-inline-2"
+                          >
                             Tout refuser
                           </label>
                         </div>
@@ -134,20 +187,18 @@ export const Cookies = () => {
                     <p className={styles["label_input"]}>
                       <span className={`${styles["textBold"]} `}>AT internet</span>
                       <br />
-                      refusé / autorisé
+                      {allowCookies === "true" ? "interdit" : "autorisé"}
+                      <br />- {allowCookies === "true" ? "Ce service n'a pas installé de cookies." : "Ce service peut déposer 4 cookies."}
                       <br />
-                      Ce service n`a installé aucun cookie.
-                      <br />
-                      - Ce service peut déposer 4 cookies. <br />
-                      <Link href="/" target="_blank">
+                      <Link className="fr-mr-1w" href="https://tarteaucitron.io/en/service/atinternet" target="_blank">
                         En savoir plus
                       </Link>
                       -{" "}
-                      <Link href="/" target="_blank">
+                      <Link className="fr-ml-1w" href="https://www.atinternet.com" target="_blank">
                         Voir le site Officiel
                       </Link>{" "}
-                      <br /> <br />
-                      <Link href="/" target="_blank">
+                      <br /> <br /> <br />
+                      <Link href="https://tarteaucitron.io/fr/" target="_blank">
                         Site officiel de Tarte au citron
                       </Link>
                     </p>
