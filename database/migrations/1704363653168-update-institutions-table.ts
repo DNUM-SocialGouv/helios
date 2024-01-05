9414547823c4d4afdfe0acc368c168b4285734f3
Adolato <a.elechi@cat-amania.com>
2024-01-05 09:39:13 +0100

    fix pb http & add IF NOT EXISTS to create table migration

--- database/migrations/1704363653168-update-institutions-table.ts
+++ database/migrations/1704363653168-update-institutions-table.ts
@@ -1,28 +1,25 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
+import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class UpdateInstitutionsTable1704363653168 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    // create back-up r-------------------------------
+    await queryRunner.query("create table IF NOT EXISTS utilisateur_old as select * from utilisateur;");
+    await queryRunner.query("create table IF NOT EXISTS institution_old as select * from institution;");
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        // create back-up r-------------------------------
-        await queryRunner.query("create table utilisateur_old as select * from utilisateur;");
-        await queryRunner.query("create table institution_old as select * from institution;");
-
-        // attach users cat-amania to equipe projet (DNUM) 
-        await queryRunner.query(`UPDATE public.utilisateur
+    // attach users cat-amania to equipe projet (DNUM)
+    await queryRunner.query(`UPDATE public.utilisateur
         SET ut_institution= (select inst_id from institution where inst_code_geo = '00')
         WHERE ut_institution= (select inst_id from institution where inst_code = 'CAT')`);
 
-        // delete institution cat-amania
-        await queryRunner.query("DELETE FROM public.institution WHERE inst_code = 'CAT'");
+    // delete institution cat-amania
+    await queryRunner.query("DELETE FROM public.institution WHERE inst_code = 'CAT'");
 
-        // update institution DNUM ---> equipe projet
-        await queryRunner.query("UPDATE institution SET inst_libelle= 'Equipe projet' WHERE inst_code_geo = '00'");
+    // update institution DNUM ---> equipe projet
+    await queryRunner.query("UPDATE institution SET inst_libelle= 'Equipe projet' WHERE inst_code_geo = '00'");
+  }
 
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE utilisateur_old;`);
-        await queryRunner.query(`DROP TABLE institution_old;`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE utilisateur_old;`);
+    await queryRunner.query(`DROP TABLE institution_old;`);
+  }
 }
