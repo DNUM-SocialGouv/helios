77b16cb66b7c87eb856b9d3ff198f7c2b263c342
Adolato <a.elechi@cat-amania.com>
2024-01-05 16:13:16 +0100

    HEL-581/prevent double submit form

---
+++ database/migrations/1704467337579-RemoveDuplicatedEmailAndMakeItUnique.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class RemoveDuplicatedEmailAndMakeItUnique1704467337579 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
