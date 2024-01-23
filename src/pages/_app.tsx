fcec1946cf37bc7c89ef756f9965ff13edfa67e9
Adolato <a.elechi@cat-amania.com>
2024-01-23 17:46:38 +0100

    modal cookies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -25,6 +25,7 @@ import { UserContextProvider } from "../frontend/ui/commun/contexts/userContextP
 import { Footer } from "../frontend/ui/commun/Footer/Footer";
 import { Header } from "../frontend/ui/commun/Header/Header";
 import { resizeChartOnPrint } from "../plugins/resizeChartAtPrint";
+import { Cookies } from "../frontend/ui/cookies/Cookies";
 
 export default function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const router = useRouter();
@@ -76,6 +77,7 @@ export default function MyApp({ Component, pageProps: { session, ...pageProps } 
             <Header />
             <Component {...pageProps} />
             <Footer />
+            <Cookies />
             <Script src="/dsfr.module.min.js" strategy="lazyOnload" type="module"></Script>
             <Script noModule src="/dsfr.nomodule.min.js" strategy="lazyOnload" type="text/javascript"></Script>
             {process.env.NODE_ENV !== "development" && <Script src="/smarttag.js" strategy="beforeInteractive" />}
