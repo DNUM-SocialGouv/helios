bd9fc1b6da8c9864388421d76027eeec6caf89a4
schlaifa <136464784+schlaifa@users.noreply.github.com>
2024-01-25 12:24:05 +0100

    Sprint 28 (#425)
    * correct package dedupe problem
    
    * (hel-583) sort years (#420)
    
    * (Hel-584) positive values for compte de resultat using math abs (#419)
    
    * (hel-585) positive values for compte resultat errd ca indicateurs (#421)
    
    * (hel-585) positive values for compte resultat errd ca indicateurs
    
    * (hel-585) fix unit test
    
    * Hel 584/change compte resultat indicateur (#422)
    
    * (Hel-584) positive values for compte de resultat using math abs
    
    * fix unit test
    
    * (hel-582) fix wording (#423)
    
    * (hel-582) fix wording
    
    * (hel-582) fix test back error
    
    * Hel 571/mettre a jour les textes et liens dans le footer helios (#424)
    
    * modal cookies
    
    * (hel-571) fix eslint pb
    
    * (hel-571) add tarteaucitron
    
    * (hel-571) fix  length-zero-no-unit
    
    * clean code
    
    * v1.1.8
    
    ---------
    
    Co-authored-by: Adolato <148781510+Adolato@users.noreply.github.com>

--- src/frontend/ui/établissement-territorial-médico-social/bloc-budget-et-finances/compte-de-resultat/CompteDeResultatViewModel.tsx
+++ src/frontend/ui/établissement-territorial-médico-social/bloc-budget-et-finances/compte-de-resultat/CompteDeResultatViewModel.tsx
@@ -54,13 +54,13 @@ export class CompteDeResultatViewModel {
     const dépensesOuCharges = [];
     if (budgetEtFinance.cadreBudgétaire === CadreBudgétaire.CA_PA) {
       const totalDesCharges = budgetEtFinance.chargesEtProduits.charges as number;
-      dépensesOuCharges.push(totalDesCharges);
+      dépensesOuCharges.push(Math.abs(totalDesCharges));
     } else {
       const dépensesGroupeI = budgetEtFinance.recettesEtDépenses.dépensesGroupe1 as number;
       const dépensesGroupeII = budgetEtFinance.recettesEtDépenses.dépensesGroupe2 as number;
       const dépensesGroupeIII = budgetEtFinance.recettesEtDépenses.dépensesGroupe3 as number;
       const totalDesDépenses = dépensesGroupeI + dépensesGroupeII + dépensesGroupeIII;
-      dépensesOuCharges.push(totalDesDépenses, dépensesGroupeI, dépensesGroupeII, dépensesGroupeIII);
+      dépensesOuCharges.push(Math.abs(totalDesDépenses), Math.abs(dépensesGroupeI), Math.abs(dépensesGroupeII), Math.abs(dépensesGroupeIII));
     }
 
     return new HistogrammeData(
@@ -72,7 +72,7 @@ export class CompteDeResultatViewModel {
           data: dépensesOuCharges,
           backgroundColor: this.getLineColors(),
           label: this.entêtesDesAutresColonnes(budgetEtFinance)[0],
-          isError: dépensesOuCharges.map((depenses) => depenses > 0),
+          // isError: dépensesOuCharges.map((depenses) => depenses > 0),
         },
       ],
       StringFormater.formatInEuro
@@ -164,6 +164,8 @@ export class CompteDeResultatViewModel {
   }
 
   public get annéeInitiale() {
+    // const anneesTriees = this.lesAnnéesEffectivesDuCompteDeRésultat().sort((année1, année2) => année2 - année1);
+    // return anneesTriees[0];
     return this.budgetEtFinancesMédicoSocial[this.budgetEtFinancesMédicoSocial.length - 1]?.année;
   }
 
