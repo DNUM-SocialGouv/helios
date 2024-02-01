7f7a7311d319ec8722d039f962ce6636d5661287
Adolato <a.elechi@cat-amania.com>
2024-02-01 14:55:25 +0100

    HEL-593/Console_Administration-Modification_des_valeurs_du_champ_role

--- database/dataSource.ts
+++ database/dataSource.ts
@@ -49,6 +49,7 @@ import { AjouterSoftDeleteUtilisateur1701782042926 } from "./migrations/17017820
 import { UpdateInstitutionsTable1704363653168 } from "./migrations/1704363653168-update-institutions-table";
 import { UpdateUsersLastConnexionDate1704366840880 } from "./migrations/1704366840880-UpdateUsersLastConnexionDate";
 import { RemoveDuplicatedEmailAndMakeItUnique1704467337579 } from "./migrations/1704467337579-RemoveDuplicatedEmailAndMakeItUnique";
+import { ModificationDesValeursDuChampRole1706794831872 } from "./migrations/1706794831872-ModificationDesValeursDuChampRole";
 import { ajoutTableProfil1795731844298 } from "./migrations/1795731844278-ajoutTableProfil";
 import { updateProfileTable1796422585498 } from "./migrations/1796422585498-updateProfileTable";
 import { ActivitéMédicoSocialModel } from "./models/ActivitéMédicoSocialModel";
@@ -160,6 +161,7 @@ export default new DataSource({
     UpdateUsersLastConnexionDate1704366840880,
     UpdateInstitutionsTable1704363653168,
     RemoveDuplicatedEmailAndMakeItUnique1704467337579,
+    ModificationDesValeursDuChampRole1706794831872,
   ],
   type: "postgres",
   url: environmentVariables.DATABASE_URL,
