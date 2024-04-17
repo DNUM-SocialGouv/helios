754c18aa1471a03e4d73cf2ba7594c2f3c333d3b
schlaifa <s.chlaifa@cat-amania.com>
2024-04-17 16:32:23 +0200

    (hel-605) fix parametrage pour les inspections

--- src/frontend/ui/établissement-territorial-médico-social/bloc-activité/PageÉtablissementTerritorialMédicoSocialActivité.test.tsx
+++ src/frontend/ui/établissement-territorial-médico-social/bloc-activité/PageÉtablissementTerritorialMédicoSocialActivité.test.tsx
@@ -360,11 +360,7 @@ describe("La page établissement territorial médico-social - bloc activité", (
           budgetEtFinances: [],
           identité: ÉtablissementTerritorialMédicoSocialViewModelTestBuilder.identité,
           ressourcesHumaines: [],
-<<<<<<< HEAD
           qualite: { reclamations: [], evenementsIndesirables: [], inspectionsEtControles: { dateMiseAJourSource: '202-02-02', inspectionsEtControles: [] } }
-=======
-          qualite: { reclamations: [] }
->>>>>>> hel-605/parametrage-autorisations
         },
         wording,
         paths
