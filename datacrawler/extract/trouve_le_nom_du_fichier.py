47430eff4514ba102786b83b1a5363884f252994
schlaifa <s.chlaifa@cat-amania.com>
2024-04-24 11:09:50 +0200

    fichier sirec nouvel format

--- datacrawler/extract/trouve_le_nom_du_fichier.py
+++ datacrawler/extract/trouve_le_nom_du_fichier.py
@@ -22,7 +22,17 @@ def trouve_le_nom_du_fichier(fichiers: List[str], préfixe_du_fichier_recherché
 
     return fichiers_correspondants[0]
 
-def trouve_le_nom_du_fichier_sirec_sivss(fichiers: List[str], préfixe_du_fichier_recherché: str, logger: Logger) -> str:
+def trouve_le_nom_du_fichier_sirec(fichiers: List[str], préfixe_du_fichier_recherché: str, logger: Logger) -> str:
+    fichiers_correspondants = [
+        nom_de_fichier for nom_de_fichier in fichiers if re.match(f"{préfixe_du_fichier_recherché}_" + r"\d{8}", nom_de_fichier)
+    ]
+    if not fichiers_correspondants:
+        logger.fatal(f"Le fichier {préfixe_du_fichier_recherché} est introuvable parmi les fichiers téléchargés.")
+        raise FileNotFoundError()
+    fichiers_correspondants.sort(reverse=True)
+    return fichiers_correspondants[0]
+
+def trouve_le_nom_du_fichier_sivss_siicea(fichiers: List[str], préfixe_du_fichier_recherché: str, logger: Logger) -> str:
     fichiers_correspondants = [
         nom_de_fichier for nom_de_fichier in fichiers if re.match(f"{préfixe_du_fichier_recherché}_" + r"\d{12}", nom_de_fichier)
     ]
