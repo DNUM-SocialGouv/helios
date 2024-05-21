ff29173f989f4215c9d9af815866da573c72831a
Adolato <a.elechi@cat-amania.com>
2024-05-21 14:07:02 +0200

    HEL-528 fix pb mirgation

--- database/dataSource.ts
+++ database/dataSource.ts
@@ -55,11 +55,11 @@ import { AjoutFichierSourceSirec1708679781472 } from "./migrations/1708679781472
 import { AjoutEvenementsIndesirables1710326854362 } from "./migrations/1710326854362-AjoutEvenementsIndesirables";
 import { AjoutTableInspectionsControles1712743083892 } from "./migrations/1712743083892-AjoutTableInspectionsControles";
 import { AjouterTableBudgetEtFinancesSanitaire1714055066913 } from "./migrations/1714055066913-AjouterTableBudget_et_finances_sanitaire";
-import { AjoutBudgetEtFinanceAProfilETSanitaire1715688226682 } from "./migrations/1715688226682-AjoutBudgetEtFinanceAProfilETSanitaire";
 import { ajoutTableProfil1795731844298 } from "./migrations/1795731844278-ajoutTableProfil";
 import { updateProfileTable1796422585498 } from "./migrations/1796422585498-updateProfileTable";
 import { AddCreatedByToProfileTable1796792910177 } from "./migrations/1796792910177-AddCreatedByToProfileTable";
 import { ModificationValeurProfil1797341938070 } from "./migrations/1797341938070-modificationValeurProfil";
+import { AjoutBudgetEtFinanceAProfilETSanitaire1797688226682 } from "./migrations/1797688226682-AjoutBudgetEtFinanceAProfilETSanitaire";
 import { ActivitéMédicoSocialModel } from "./models/ActivitéMédicoSocialModel";
 import { ActivitéSanitaireEntitéJuridiqueModel } from "./models/ActivitéSanitaireEntitéJuridiqueModel";
 import { ActivitéSanitaireModel } from "./models/ActivitéSanitaireModel";
@@ -184,7 +184,7 @@ export default new DataSource({
     AjoutEvenementsIndesirables1710326854362,
     AjoutTableInspectionsControles1712743083892,
     AjouterTableBudgetEtFinancesSanitaire1714055066913,
-    AjoutBudgetEtFinanceAProfilETSanitaire1715688226682,
+    AjoutBudgetEtFinanceAProfilETSanitaire1797688226682,
     ModificationValeurProfil1797341938070
   ],
   type: "postgres",
