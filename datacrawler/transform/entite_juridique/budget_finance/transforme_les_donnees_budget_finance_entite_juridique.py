fce44954a90b3e86a7514dd698b5e6bbea567d67
schlaifa <s.chlaifa@cat-amania.com>
2024-06-06 13:20:07 +0200

    drop_duplicates budget_finance_entite_juridique

--- datacrawler/transform/entite_juridique/budget_finance/transforme_les_donnees_budget_finance_entite_juridique.py
+++ datacrawler/transform/entite_juridique/budget_finance/transforme_les_donnees_budget_finance_entite_juridique.py
@@ -24,5 +24,6 @@ def transform_les_donnees_budget_finance_entite_juridique(
     return (
         données_dernieres_5_annees[est_dans_finess]
         .rename(columns=extrais_l_equivalence_des_noms_des_colonnes(équivalences_diamant_quo_san_finance_buget_finance_helios))
+        .drop_duplicates(subset=index_du_bloc_budget_et_finances_entite_juridique)
         .set_index(index_du_bloc_budget_et_finances_entite_juridique)
     )
