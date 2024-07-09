3caa413facccc1139baffb314f3c3d36972fb0a3
schlaifa <s.chlaifa@cat-amania.com>
2024-07-09 11:23:04 +0200

    (hel-620) fix button design

--- src/frontend/ui/commun/IndicateurGraphique/BlocIndicateurVide.tsx
+++ src/frontend/ui/commun/IndicateurGraphique/BlocIndicateurVide.tsx
@@ -1,11 +1,11 @@
 import { Bloc } from "../Bloc/Bloc";
 import { useDependencies } from "../contexts/useDependencies";
 
-export function BlocIndicateurVide({ title, opnedBloc, toggelBlocs }: { title: string, opnedBloc?:boolean, toggelBlocs?: () => void }) {
+export function BlocIndicateurVide({ title, opnedBloc, toggelBlocs }: { title: string, opnedBloc?: boolean, toggelBlocs?: () => void }) {
   const { wording } = useDependencies();
 
   return (
-    <Bloc /*isExpandable={false}*/ opnedBloc={opnedBloc} titre={title} toggelBlocs={toggelBlocs}>
+    <Bloc isExpandable={false} opnedBloc={opnedBloc} titre={title} toggelBlocs={toggelBlocs}>
       {wording.INDICATEURS_VIDES}
     </Bloc>
   );
