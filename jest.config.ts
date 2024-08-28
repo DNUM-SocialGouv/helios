55f57bdab2876a25f6dd81207fc9504b0e2f5afc
schlaifa <s.chlaifa@cat-amania.com>
2024-08-28 09:46:09 +0200

    fix build error

--- jest.config.ts
+++ jest.config.ts
@@ -27,8 +27,7 @@ const customJestConfig = {
 async function jestConfig() {
   const nextJestConfig = await createJestConfig(customJestConfig)();
   // /node_modules/ is the first pattern
-  if (nextJestConfig.transformIgnorePatterns) nextJestConfig.transformIgnorePatterns[0] = "/node_modules/(?!uuid)/";
-
+  nextJestConfig.transformIgnorePatterns[0] = "/node_modules/(?!uuid)/";
   // Stryker ne fonctionne pas à cause de l'async https://github.com/stryker-mutator/stryker-js/issues/3480
   // Pour que les mutants naissent, il faut générer ce fichier donc lancer au moins un test
   fs.writeFileSync("jest-for-mutation-testing.json", JSON.stringify(nextJestConfig, null, 2));
