1ee7c35d9a6a2ac1418fd961c2b894295297345e
maeturki <mae.turki@cat-amania.com>
2024-10-22 11:09:16 +0200

    Hel 690/filtre capacite (#564)
    * (hel-690) hel-690/Commit initial
    
    * (hel-690) hel-690/Filtre capaciter + liaison avec filtre structure
    
    * (hel-690) hel-690/add capacite values to rechercheAvancee api
    
    * (hel-690) hel-690/Commit initial
    
    * (hel-690) hel-690/Filtre capaciter + liaison avec filtre structure
    
    * (hel-690) hel-690/add capacite values to rechercheAvancee api
    
    * (hel-690) hel-690/resolve conflit
    
    * (hel-690) hel-690/mise à jour du wording des liste des etablissements
    
    * (hel-690) hel-690/sonar css

--- src/backend/infrastructure/gateways/recherche-loader/TypeOrmRechercheLoader.ts
+++ src/backend/infrastructure/gateways/recherche-loader/TypeOrmRechercheLoader.ts
@@ -16,9 +16,9 @@ type RechercheTypeOrm = Readonly<{
 
 export class TypeOrmRechercheLoader implements RechercheLoader {
   private readonly NOMBRE_DE_RÉSULTATS_MAX_PAR_PAGE = 12;
-  private readonly NOMBRE_DE_RÉSULTATS_RECHERCHE_AVANCEE__MAX_PAR_PAGE = 2;
+  private readonly NOMBRE_DE_RÉSULTATS_RECHERCHE_AVANCEE__MAX_PAR_PAGE = 20;
 
-  constructor(private readonly orm: Promise<DataSource>) { }
+  constructor(private readonly orm: Promise<DataSource>) {}
 
   async recherche(terme: string, page: number): Promise<RésultatDeRecherche> {
     const termeSansEspaces = terme.replaceAll(/\s/g, "");
@@ -48,11 +48,19 @@ export class TypeOrmRechercheLoader implements RechercheLoader {
     return this.construisLesRésultatsDeLaRecherche(rechercheModelRésultats, nombreDeRésultats);
   }
 
-  async rechercheAvancee(terme: string, commune: string, type: string, statutJuridique: string[], capaciteSMS: CapaciteSMS[], orderBy: string, order: OrderDir, page: number): Promise<RésultatDeRecherche> {
+  async rechercheAvancee(
+    terme: string,
+    commune: string,
+    type: string,
+    statutJuridique: string[],
+    capaciteSMS: CapaciteSMS[],
+    orderBy: string,
+    order: OrderDir,
+    page: number
+  ): Promise<RésultatDeRecherche> {
     const termeSansEspaces = terme.replaceAll(/\s/g, "");
     const termeSansTirets = terme.replaceAll(/-/g, " ");
     const majusCommune = commune.replaceAll(/\b(?:-|')\b/gi, " ").toLocaleUpperCase();
-
     const conditions = [];
     let parameters: any = {};
 
@@ -66,54 +74,57 @@ export class TypeOrmRechercheLoader implements RechercheLoader {
       .addSelect("recherche.statut_juridique", "statutJuridique")
       .from(RechercheModel, "recherche");
 
-
-
     if (majusCommune) {
       conditions.push("recherche.commune = :commune");
-      parameters = { ...parameters, commune: majusCommune }
+      parameters = { ...parameters, commune: majusCommune };
     }
 
     if (terme) {
-      conditions.push("(recherche.termes @@ plainto_tsquery('unaccent_helios', :terme) OR recherche.termes @@ plainto_tsquery('unaccent_helios', :termeSansEspaces) OR recherche.termes @@ plainto_tsquery('unaccent_helios', :termeSansTirets))");
+      conditions.push(
+        "(recherche.termes @@ plainto_tsquery('unaccent_helios', :terme) OR recherche.termes @@ plainto_tsquery('unaccent_helios', :termeSansEspaces) OR recherche.termes @@ plainto_tsquery('unaccent_helios', :termeSansTirets))"
+      );
       parameters.terme = terme;
-      parameters = { ...parameters, termeSansEspaces: termeSansEspaces, termeSansTirets: termeSansTirets }
+      parameters = { ...parameters, termeSansEspaces: termeSansEspaces, termeSansTirets: termeSansTirets };
     }
 
     if (type) {
       conditions.push("recherche.type = :type");
-      parameters = { ...parameters, type: type }
+      parameters = { ...parameters, type: type };
     }
 
     if (statutJuridique.length > 0) {
       conditions.push("(recherche.statut_juridique IN (:...statutJuridique) OR recherche.statut_juridique = '')");
-      parameters = { ...parameters, statutJuridique: statutJuridique }
+      parameters = { ...parameters, statutJuridique: statutJuridique };
     }
 
     if (capaciteSMS.length !== 0) {
       const capaciteSMSConditions: string[] = [];
-      requêteDeLaRecherche.innerJoin(AutorisationMédicoSocialModel, 'capacite_sms', 'recherche.numero_finess = capacite_sms.numero_finess_etablissement_territorial');
+      requêteDeLaRecherche.innerJoin(
+        AutorisationMédicoSocialModel,
+        "capacite_sms",
+        "recherche.numero_finess = capacite_sms.numero_finess_etablissement_territorial"
+      );
       capaciteSMS.forEach((capacite, index) => {
         const conditionsSMS = this.construisLesConditionsCapacitesSMS(capacite, index);
 
-        capaciteSMSConditions.push(conditionsSMS.capaciteSMSConditions)
-        parameters = { ...parameters, ...conditionsSMS.parameters }
+        capaciteSMSConditions.push(conditionsSMS.capaciteSMSConditions);
+        parameters = { ...parameters, ...conditionsSMS.parameters };
       });
-      conditions.push(capaciteSMS.length === 1 ? capaciteSMSConditions[0] : '(' + capaciteSMSConditions.join(' OR ') + ')');
+      conditions.push(capaciteSMS.length === 1 ? capaciteSMSConditions[0] : "(" + capaciteSMSConditions.join(" OR ") + ")");
     }
 
-    if (conditions.length > 0) requêteDeLaRecherche.where(conditions.join(' AND '), parameters);
+    if (conditions.length > 0) requêteDeLaRecherche.where(conditions.join(" AND "), parameters);
 
-    const nombreDeRésultats = await requêteDeLaRecherche
-      .clone()
-      .select('COUNT(DISTINCT recherche.numero_finess)', 'count')
-      .getRawOne();
+    const nombreDeRésultats = await requêteDeLaRecherche.clone().select("COUNT(DISTINCT recherche.numero_finess)", "count").getRawOne();
 
     if (orderBy && order) {
-      requêteDeLaRecherche.orderBy(orderBy, order)
+      requêteDeLaRecherche
+        .orderBy(orderBy, order)
         .limit(this.NOMBRE_DE_RÉSULTATS_RECHERCHE_AVANCEE__MAX_PAR_PAGE)
         .offset(this.NOMBRE_DE_RÉSULTATS_RECHERCHE_AVANCEE__MAX_PAR_PAGE * (page - 1));
     } else {
-      requêteDeLaRecherche.orderBy("type", "ASC")
+      requêteDeLaRecherche
+        .orderBy("type", "ASC")
         .addOrderBy("numero_finess", "ASC")
         .limit(this.NOMBRE_DE_RÉSULTATS_RECHERCHE_AVANCEE__MAX_PAR_PAGE)
         .offset(this.NOMBRE_DE_RÉSULTATS_RECHERCHE_AVANCEE__MAX_PAR_PAGE * (page - 1));
@@ -130,26 +141,26 @@ export class TypeOrmRechercheLoader implements RechercheLoader {
     const parameters: { [key: string]: any } = {};
 
     capaciteSMS.ranges.forEach((range, index) => {
-      if (range.includes('>')) {
+      if (range.includes(">")) {
         conditions.push(`capacite_sms.capacite_installee_totale > :range${capaciteSMS.classification}${index}`);
         parameters[`range${capaciteSMS.classification}${index}`] = range.substring(1);
-
       } else {
-        const limits = range.split(',');
-        conditions.push(`capacite_sms.capacite_installee_totale BETWEEN :min${capaciteSMS.classification}${index} AND :max${capaciteSMS.classification}${index}`);
+        const limits = range.split(",");
+        conditions.push(
+          `capacite_sms.capacite_installee_totale BETWEEN :min${capaciteSMS.classification}${index} AND :max${capaciteSMS.classification}${index}`
+        );
         parameters[`min${capaciteSMS.classification}${index}`] = limits[0];
         parameters[`max${capaciteSMS.classification}${index}`] = limits[1];
       }
     });
-    if (capaciteSMS.ranges.length === 1) capaciteSMSConditions = conditions[0]
+    if (capaciteSMS.ranges.length === 1) capaciteSMSConditions = conditions[0];
     else {
-      capaciteSMSConditions = '(' + conditions.join(' OR ') + ')';
+      capaciteSMSConditions = "(" + conditions.join(" OR ") + ")";
     }
-    capaciteSMSConditions = '(' + capaciteSMSConditions + ` AND recherche.classification = :classification${indexClassification})`;
+    capaciteSMSConditions = "(" + capaciteSMSConditions + ` AND recherche.classification = :classification${indexClassification})`;
     parameters[`classification${indexClassification}`] = capaciteSMS.classification;
     return { capaciteSMSConditions, parameters };
   }
-
 
   private async compteLeNombreDeRésultats(requêteDeLaRecherche: SelectQueryBuilder<RechercheModel>): Promise<number> {
     return await requêteDeLaRecherche.getCount();
--- src/frontend/ui/recherche-avancee/useRechercheAvancee.ts
+++ src/frontend/ui/recherche-avancee/useRechercheAvancee.ts
@@ -6,126 +6,134 @@ import { ExtendedRésultatDeRecherche } from "../../../pages/recherche-avancee";
 import { RechercheAvanceeContext } from "../commun/contexts/RechercheAvanceeContext";
 import { useDependencies } from "../commun/contexts/useDependencies";
 import { RechercheViewModel } from "../home/RechercheViewModel";
+import { CapaciteEtablissement } from "./model/CapaciteEtablissement";
 
 type RechercheAvanceeState = Readonly<{
-    estCeEnAttente: boolean;
-    estCeQueLeBackendNeRépondPas: boolean;
-    estCeQueLesRésultatsSontReçus: boolean;
-    estCeQueLaRechercheEstLancee: boolean
-    nombreRésultats: number;
-    lastPage: number
-    résultats: RechercheViewModel[];
-    termeFixe: string;
+  estCeEnAttente: boolean;
+  estCeQueLeBackendNeRépondPas: boolean;
+  estCeQueLesRésultatsSontReçus: boolean;
+  estCeQueLaRechercheEstLancee: boolean;
+  nombreRésultats: number;
+  lastPage: number;
+  résultats: RechercheViewModel[];
+  termeFixe: string;
 }>;
 
 export function useRechercheAvancee(data: ExtendedRésultatDeRecherche) {
-    const { paths } = useDependencies();
+  const { paths } = useDependencies();
 
-    const rechercheAvanceeContext = useContext(RechercheAvanceeContext);
-    
-    const pageInitiale = 1;
-    const lastPage = data.nombreDeRésultats > 0 ? Math.ceil(data.nombreDeRésultats / 3) : 1;
-    
-    const [page, setPage] = useQueryState("page", parseAsInteger.withDefault(data.page).withOptions({history: "push"}));
-    const [terme, setTerme] = useQueryState<string>("terme", parseAsString.withDefault(""));
+  const rechercheAvanceeContext = useContext(RechercheAvanceeContext);
 
-    const construisLesRésultatsDeLaRecherche = (data: RésultatDeRecherche): RechercheViewModel[] => {
-        return data.résultats.map((résultat: Résultat) => new RechercheViewModel(résultat, paths));
-    };
+  const pageInitiale = 1;
+  const lastPage = data.nombreDeRésultats > 0 ? Math.ceil(data.nombreDeRésultats / 3) : 1;
 
-    const [state, setState] = useState<RechercheAvanceeState>({
-        estCeEnAttente: false,
-        estCeQueLeBackendNeRépondPas: false,
+  const [page, setPage] = useQueryState("page", parseAsInteger.withDefault(data.page).withOptions({ history: "push" }));
+  const [terme, setTerme] = useQueryState<string>("terme", parseAsString.withDefault(""));
+
+  const construisLesRésultatsDeLaRecherche = (data: RésultatDeRecherche): RechercheViewModel[] => {
+    return data.résultats.map((résultat: Résultat) => new RechercheViewModel(résultat, paths));
+  };
+
+  const [state, setState] = useState<RechercheAvanceeState>({
+    estCeEnAttente: false,
+    estCeQueLeBackendNeRépondPas: false,
+    estCeQueLesRésultatsSontReçus: false,
+    estCeQueLaRechercheEstLancee: false,
+    nombreRésultats: data.nombreDeRésultats || 0,
+    lastPage,
+    résultats: construisLesRésultatsDeLaRecherche(data),
+    termeFixe: "",
+  });
+
+  const lancerLaRecherche = (event: MouseEvent<HTMLButtonElement>): void => {
+    if (terme !== "") {
+      event.preventDefault();
+      setState({
+        ...state,
+        estCeEnAttente: true,
         estCeQueLesRésultatsSontReçus: false,
-        estCeQueLaRechercheEstLancee: false,
-        nombreRésultats: data.nombreDeRésultats || 0,
-        lastPage,
-        résultats: construisLesRésultatsDeLaRecherche(data),
-        termeFixe: "",
+      });
+      rechercher(
+        terme,
+        rechercheAvanceeContext?.zoneGeo,
+        rechercheAvanceeContext?.typeStructure,
+        rechercheAvanceeContext?.statutJuridiqueStructure,
+        rechercheAvanceeContext?.capaciteSMS,
+        pageInitiale
+      );
+    }
+  };
+
+  const rechercheOnChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setTerme(event.target.value);
+  };
+
+  const rechercher = async (
+    terme: string,
+    commune: string = "",
+    type: string = "",
+    statutJuridique: string[] = [],
+    capaciteSMS: CapaciteEtablissement[] = [],
+    page: number
+  ) => {
+    setPage(page, { shallow: false });
+    setTerme(terme, { shallow: false });
+    fetch("/api/recherche-avancee", {
+      body: JSON.stringify({ page, terme, commune, type, statutJuridique, capaciteSMS }),
+      headers: { "Content-Type": "application/json" },
+      method: "POST",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setState({
+          ...state,
+          estCeEnAttente: false,
+          estCeQueLesRésultatsSontReçus: true,
+          estCeQueLaRechercheEstLancee: true,
+          nombreRésultats: data.nombreDeRésultats,
+          lastPage: Math.ceil(data.nombreDeRésultats / 20),
+          résultats: construisLesRésultatsDeLaRecherche(data),
+        });
+      })
+      .catch(() => {
+        setState({
+          ...state,
+          estCeEnAttente: false,
+          estCeQueLeBackendNeRépondPas: true,
+          // estCeQueLaRechercheEstLancee: true,
+        });
+      });
+  };
+
+  const setPageWithSearch = (page: number) => {
+    setState({
+      ...state,
+      estCeEnAttente: false,
+      estCeQueLesRésultatsSontReçus: true,
+      estCeQueLaRechercheEstLancee: true,
     });
 
-    const lancerLaRecherche = (event: MouseEvent<HTMLButtonElement>): void => {
-        if (terme !== "") {
-            event.preventDefault();
-            setState({
-                ...state,
-                estCeEnAttente: true,
-                estCeQueLesRésultatsSontReçus: false,
-            });
-            rechercher(
-                terme,
-                rechercheAvanceeContext?.zoneGeo,
-                rechercheAvanceeContext?.typeStructure,
-                rechercheAvanceeContext?.statutJuridiqueStructure,
-                pageInitiale
-            );
-        }
-    };
+    rechercher(
+      terme,
+      rechercheAvanceeContext?.zoneGeo,
+      rechercheAvanceeContext?.typeStructure,
+      rechercheAvanceeContext?.statutJuridiqueStructure,
+      rechercheAvanceeContext?.capaciteSMS,
+      page
+    );
+  };
 
-    const rechercheOnChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setTerme(event.target.value)
-    };
-
-    const rechercher = async (terme: string, commune: string = "", type: string = "", statutJuridique: string[] = [], page: number) => {
-        setPage(page, {shallow: false});
-        setTerme(terme, {shallow: false});
-
-        fetch("/api/recherche-avancee", {
-            body: JSON.stringify({ page, terme, commune, type, statutJuridique }),
-            headers: { "Content-Type": "application/json" },
-            method: "POST",
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                setState({
-                    ...state,
-                    estCeEnAttente: false,
-                    estCeQueLesRésultatsSontReçus: true,
-                    estCeQueLaRechercheEstLancee: true,
-                    nombreRésultats: data.nombreDeRésultats,
-                    lastPage: Math.ceil(data.nombreDeRésultats / 20),
-                    résultats: construisLesRésultatsDeLaRecherche(data),
-                });
-            })
-            .catch(() => {
-                setState({
-                    ...state,
-                    estCeEnAttente: false,
-                    estCeQueLeBackendNeRépondPas: true,
-                    // estCeQueLaRechercheEstLancee: true,
-                });
-            });
-    };
-
-    const setPageWithSearch = (page: number) => {
-        setState({
-            ...state,
-            estCeEnAttente: false,
-            estCeQueLesRésultatsSontReçus: true,
-            estCeQueLaRechercheEstLancee: true,
-        })
-
-        rechercher(
-            terme, 
-            rechercheAvanceeContext?.zoneGeo, 
-            rechercheAvanceeContext?.typeStructure,
-            rechercheAvanceeContext?.statutJuridiqueStructure, 
-            page
-        );
-    };
-
-
-    return {
-        estCeEnAttente: state.estCeEnAttente,
-        estCeQueLeBackendNeRépondPas: state.estCeQueLeBackendNeRépondPas,
-        estCeQueLesRésultatsSontReçus: state.estCeQueLesRésultatsSontReçus,
-        estCeQueLaRechercheEstLancee: state.estCeQueLaRechercheEstLancee,
-        lancerLaRecherche,
-        rechercheOnChange,
-        lastPage: state.lastPage,
-        terme,
-        setPage: setPageWithSearch,
-        page,
-        construisLesRésultatsDeLaRecherche,
-    };
+  return {
+    estCeEnAttente: state.estCeEnAttente,
+    estCeQueLeBackendNeRépondPas: state.estCeQueLeBackendNeRépondPas,
+    estCeQueLesRésultatsSontReçus: state.estCeQueLesRésultatsSontReçus,
+    estCeQueLaRechercheEstLancee: state.estCeQueLaRechercheEstLancee,
+    lancerLaRecherche,
+    rechercheOnChange,
+    lastPage: state.lastPage,
+    terme,
+    setPage: setPageWithSearch,
+    page,
+    construisLesRésultatsDeLaRecherche,
+  };
 }
