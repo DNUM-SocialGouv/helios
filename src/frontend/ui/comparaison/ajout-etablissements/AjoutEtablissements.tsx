a2d9bce06e1609b5a91f350429ff4fd7d3b35d89
maeturki <mae.turki@cat-amania.com>
2024-12-30 09:58:26 +0100

    Hel 681/ajout etablissement (#623)
    * (hel-681) initiale changes
    
    * (hel-681) in progress
    
    * (hel-681) 681/ajout etablissement suite
    
    * (hel-681) 681/ajout etablissement + ajustement merge
    
    * (hel-681) css sonar corrections
    
    * (hel-681) 681/suite dev
    
    * (hel-681) 681/fix issues
    
    * (hel-681) 681/fixe effacer filters
    
    * (hel-681) 681/actions en fermant le composent
    
    * (hel-681) 681/bouton ajouter établissement
    
    * (hel-681) hel-681/disable button ajout when no etablissements selected

--- src/frontend/ui/comparaison/ajout-etablissements/AjoutEtablissements.tsx
+++ src/frontend/ui/comparaison/ajout-etablissements/AjoutEtablissements.tsx
@@ -54,6 +54,7 @@ export const AjoutEtablissements = ({ setIsShowAjoutEtab, setReloadTable }: Ajou
     setReload(false);
   }, [prevPage, reload]);
 
+  // detect filtre(s) changes to update results
   useEffect(() => {
     if (isChangedZG || isChangedCapacite || comparaisonContext?.terme) {
       comparaisonContext?.setPage(1);
@@ -135,7 +136,7 @@ export const AjoutEtablissements = ({ setIsShowAjoutEtab, setReloadTable }: Ajou
           <button
             aria-controls="fr-modal-Capacite-Filtre"
             className="fr-btn fr-btn--primary"
-            disabled={false}
+            disabled={newEtablissements.length < 1}
             id="ajouter-etablissement-botton"
             onClick={onClickAjouter}
           >
