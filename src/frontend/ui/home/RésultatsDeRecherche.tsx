a42dfe53e475ef69a49011dc53a3fe5f85542975
yelhouakmi <y.elhouakmi@cat-amania.com>
2025-01-24 09:52:44 +0100

    (hel-668) Mise à jour de la page de recherche pour utiliser les nouveaux composants

--- src/frontend/ui/home/RésultatsDeRecherche.tsx
+++ src/frontend/ui/home/RésultatsDeRecherche.tsx
@@ -1,14 +1,16 @@
-import { useDependencies } from "../commun/contexts/useDependencies";
+import { useContext } from "react";
+
 import "@gouvfr/dsfr/dist/component/tile/tile.min.css";
-import { Establishment } from "./Establishment";
-import styles from "./Recherche.module.css";
-import { RechercheViewModel } from "./RechercheViewModel";
+import { useDependencies } from "../commun/contexts/useDependencies";
+import { UserContext } from "../commun/contexts/userContext";
+import { GrilleEtablissements } from "../commun/GrilleEtablissements/GrilleEtablissements";
+import { TuileEtablissementViewModel } from "../commun/TuileEtablissement/TuileEtablissementViewModel";
 
 type RésultatsDeRechercheProps = Readonly<{
   estCeQueLesRésultatsSontTousAffichés: boolean;
   chargeLesRésultatsSuivants: () => void;
   nombreRésultats: number;
-  résultats: RechercheViewModel[];
+  résultats: TuileEtablissementViewModel[];
   termeFixe: string;
 }>;
 
@@ -20,26 +22,15 @@ export const RésultatsDeRecherche = ({
   termeFixe,
 }: RésultatsDeRechercheProps) => {
   const { wording } = useDependencies();
+  const context = useContext(UserContext);
+  const favorisListId = context?.favorisLists.find(list => list.isFavoris)?.id || -1;
 
   return (
     <section aria-label={wording.RÉSULTAT_DE_RECHERCHE}>
       <p className="fr-h6 fr-mt-4w">
         {(nombreRésultats === 0 && wording.aucunRésultat(termeFixe)) || wording.rechercheNombreRésultats(nombreRésultats, termeFixe)}
       </p>
-      <ul className={"fr-grid-row fr-grid-row--gutters " + styles["tuiles"]}>
-        {résultats.map((résultatViewModel, index) => (
-          <li className="fr-col-3" key={résultatViewModel.numéroFiness + index}>
-            <Establishment résultatViewModel={résultatViewModel} />
-          </li>
-        ))}
-      </ul>
-      {!estCeQueLesRésultatsSontTousAffichés && (
-        <div className={styles["voir-plus-de-résultats"]}>
-          <button className="fr-btn fr-btn--secondary" onClick={chargeLesRésultatsSuivants}>
-            {wording.VOIR_PLUS_RÉSULTATS}
-          </button>
-        </div>
-      )}
+      <GrilleEtablissements chargeLesRésultatsSuivants={chargeLesRésultatsSuivants} currentListId={favorisListId} estCeQueLesRésultatsSontTousAffichés={estCeQueLesRésultatsSontTousAffichés} résultats={résultats} />
     </section>
   );
 };
