40d4a960e1af90a2c26b70efd33c493c4252d750
schlaifa <s.chlaifa@cat-amania.com>
2025-01-24 16:16:17 +0100

    (hel-667) retirer la partie server side de la page favoris

--- src/pages/mes-listes.tsx
+++ src/pages/mes-listes.tsx
@@ -1,23 +1,12 @@
-import { GetServerSidePropsContext, GetStaticPropsResult } from "next";
-import { getSession } from "next-auth/react";
 import { useContext, useEffect } from "react";
 
-import { rechercheParNumeroFinessEndpoint } from "../backend/infrastructure/controllers/rechercheParNumeroFinessEndpoints";
-import { getAll } from "../backend/infrastructure/controllers/userListEndpoint";
-import { Résultat } from "../backend/métier/entities/RésultatDeRecherche";
 import { BackToSearchContext, BackToSearchContextValue } from "../frontend/ui/commun/contexts/BackToSearchContext";
 import { useDependencies } from "../frontend/ui/commun/contexts/useDependencies";
 import { useBreadcrumb } from "../frontend/ui/commun/hooks/useBreadcrumb";
 import { FavorisPage } from "../frontend/ui/favoris/FavorisPage";
-import { RechercheViewModel } from "../frontend/ui/home/RechercheViewModel";
 
-type RouterProps = Readonly<{
-    listId: number;
-    etablissements: Résultat[];
-}>;
-
-export default function Router({ listId, etablissements }: RouterProps) {
-    const { wording, paths } = useDependencies();
+export default function Favoris() {
+    const { wording } = useDependencies();
     const backToSearchContext = useContext(BackToSearchContext) as BackToSearchContextValue;
 
     useBreadcrumb([
@@ -34,44 +23,5 @@ export default function Router({ listId, etablissements }: RouterProps) {
         }
     }, [backToSearchContext])
 
-    const elements = etablissements.map((elmt) => {
-        return new RechercheViewModel(elmt, paths);
-    });
-
-    return <FavorisPage favoris={elements} listId={listId} />;
-}
-
-export async function getServerSideProps(context: GetServerSidePropsContext): Promise<GetStaticPropsResult<RouterProps>> {
-    try {
-
-        const session = await getSession(context);
-        const userUuid = session?.user?.idUser as string;
-        const lists = await getAll(userUuid);
-        if (!lists) {
-            return { notFound: true };
-        }
-
-        // On ne traite que les favoris pour le moment
-        const list = lists.find(list => list.isFavoris);
-        if (!list) {
-            return { notFound: true };
-        }
-        const finessNumber = list?.userListEtablissements.map((etablissement) => etablissement.finessNumber);
-        if (finessNumber) {
-            const etablissementList = (await rechercheParNumeroFinessEndpoint(finessNumber));
-
-
-
-            return {
-                props: {
-                    listId: list.id,
-                    etablissements: etablissementList,
-                },
-            };
-        } else {
-            return { notFound: true };
-        }
-    } catch (error) {
-        return { notFound: true };
-    }
+    return <FavorisPage />;
 }
