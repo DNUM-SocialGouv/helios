96633658edf3dbb46ccc18e946181b54ad4180fe
schlaifa <s.chlaifa@cat-amania.com>
2025-02-06 15:54:42 +0100

    (hel-720) lancer la comparaison

--- src/frontend/ui/liste/ListActionsButton.tsx
+++ src/frontend/ui/liste/ListActionsButton.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router";
 import { useState } from "react";
 
 import { useDependencies } from "../commun/contexts/useDependencies";
@@ -11,8 +12,19 @@ type ListActionsButtonProps = Readonly<{
 
 export const ListActionsButton = ({ selectedRows }: ListActionsButtonProps) => {
     const { wording } = useDependencies();
+    const router = useRouter();
 
     const [displayActions, setDisplayActions] = useState<boolean>(false);
+
+    const lancerComparaison = () => {
+        const firstType = selectedRows[0].type;
+        const listFinessNumbers = selectedRows.map((row) => row.numéroFiness);
+        sessionStorage.setItem("listFinessNumbers", JSON.stringify(listFinessNumbers));
+        sessionStorage.setItem("comparaisonType", firstType);
+        document.cookie = `list=${encodeURIComponent(JSON.stringify(listFinessNumbers))}; path=/`;
+        document.cookie = `type=${encodeURIComponent(firstType)}; path=/`;
+        router.push("/comparaison");
+    }
 
     return (
         <div className={styles["button-position"]}>
@@ -20,7 +32,7 @@ export const ListActionsButton = ({ selectedRows }: ListActionsButtonProps) => {
             {displayActions &&
                 <ul className={styles["menu"]}>
                     <li className={styles["menu-item"]}>
-                        <button className="fr-btn fr-btn--tertiary-no-outline" disabled={selectedRows.length < 2}>
+                        <button className="fr-btn fr-btn--tertiary-no-outline" disabled={selectedRows.length < 2 || selectedRows.some((row) => row.type !== "Médico-social")} onClick={lancerComparaison}>
                             {wording.COMPARER}
                         </button>
                     </li>
