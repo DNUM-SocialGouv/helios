7a7b4e74fc6c6afa8281070bd1b99f8d43a3b23a
nbenyahiadev <117667297+nbenyahiadev@users.noreply.github.com>
2025-02-12 18:52:21 +0100

    Release v1.4.0 (#679)
    * change version to v1.4.0
    
    * fix bug in build

--- database/models/UserListModel.ts
+++ database/models/UserListModel.ts
@@ -24,7 +24,7 @@ export class UserListModel {
     @JoinColumn({ name: 'ut_id', referencedColumnName: 'code' })
     public user!: UtilisateurModel;
 
-    @OneToMany('UserListEtablissementModel', 'userList', { eager: true })
+    @OneToMany(() => UserListEtablissementModel, 'userList', { eager: true })
     public userListEtablissements!: UserListEtablissementModel[];
 
 }
