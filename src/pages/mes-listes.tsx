9f1888ea4e14c0fb6241e27d9b04d2f95dab6537
yelhouakmi <y.elhouakmi@cat-amania.com>
2025-02-12 15:01:19 +0100

    Rechargement de la liste des favoris au chargement de la page favoris pour synchroniser l’état avec la BDD

--- src/pages/mes-listes.tsx
+++ src/pages/mes-listes.tsx
@@ -1,13 +1,24 @@
+import { GetServerSidePropsContext, GetStaticPropsResult } from "next";
+import { getSession } from "next-auth/react";
 import { useContext, useEffect } from "react";
 
+import { getAll } from "../backend/infrastructure/controllers/userListEndpoint";
 import { BackToSearchContext, BackToSearchContextValue } from "../frontend/ui/commun/contexts/BackToSearchContext";
 import { useDependencies } from "../frontend/ui/commun/contexts/useDependencies";
+import { UserContext } from "../frontend/ui/commun/contexts/userContext";
 import { useBreadcrumb } from "../frontend/ui/commun/hooks/useBreadcrumb";
 import { FavorisPage } from "../frontend/ui/favoris/FavorisPage";
+import { UserListViewModel } from "../frontend/ui/user-list/UserListViewModel";
 
-export default function Favoris() {
+type RouterProps = Readonly<{
+    favoris: UserListViewModel[];
+}>;
+
+export default function Router({ favoris }: RouterProps) {
     const { wording } = useDependencies();
+    const userContext = useContext(UserContext);
     const backToSearchContext = useContext(BackToSearchContext) as BackToSearchContextValue;
+
 
     useBreadcrumb([
         {
@@ -21,7 +32,29 @@ export default function Favoris() {
             backToSearchContext.setIsInfoPage(false);
             localStorage.clear();
         }
+        userContext?.setFavorisLists(favoris);
     }, [backToSearchContext])
 
     return <FavorisPage />;
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext): Promise<GetStaticPropsResult<RouterProps>> {
+    try {
+        const session = await getSession(context);
+        const userUuid = session?.user?.idUser as string;
+
+        const list = await getAll(userUuid);
+        if (!list) {
+            return { notFound: true };
+        }
+
+        return {
+            props: {
+                favoris: JSON.parse(JSON.stringify(list)),
+            },
+        };
+
+    } catch (error) {
+        return { notFound: true };
+    }
 }
