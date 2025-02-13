f012ea33422c9ce9ef89f2d279ea68a826d7d34a
Adolato <a.elechi@cat-amania.com>
2025-02-13 17:49:21 +0100

    automatisation d'intégrations des données : profession + profession_ref + check numéros_finess_connus

--- datacrawler/import_vigie_rh_profession_filiere.py
+++ datacrawler/import_vigie_rh_profession_filiere.py
@@ -1,30 +1,36 @@
 import os
 import pandas as pd
+from datetime import datetime
 from sqlalchemy.engine import Engine, create_engine
 from datacrawler import supprimer_donnees_existantes, inserer_nouvelles_donnees
 from datacrawler.dependencies.dépendances import initialise_les_dépendances
 from datacrawler.extract.lecteur_parquet import lis_le_fichier_parquet
+from datacrawler.extract.trouve_le_nom_du_fichier import trouve_le_nom_du_fichier
+from datacrawler.extract.lecteur_sql import récupère_les_numéros_finess_des_établissements_de_la_base
 
-def filter_contrat_data(donnees: pd.DataFrame) -> pd.DataFrame:
+def filter_profession_filiere_data(donnees: pd.DataFrame, base_de_donnees) -> pd.DataFrame:
+    numéros_finess_des_établissements_connus = récupère_les_numéros_finess_des_établissements_de_la_base(base_de_donnees)
+    numéros_finess_liste = numéros_finess_des_établissements_connus['numero_finess_etablissement_territorial'].astype(str).tolist()
+
+    # Année actuelle pour le filtre
     annee_actuelle = datetime.now().year
     year_regex = r"((20[012]\d{1}|19\d{2}))"
 
-    return donnees[
-        (donnees["numero_finess"].astype(str).str.len() == 9) &
-        (donnees["annee"].astype(str).str.match(year_regex)) &
-        (donnees["annee"] >= annee_actuelle - 10)  # Garde seulement les 3 dernières années
+    # Filtrer les données
+    donnees_filtrées = donnees[
+        (donnees["numero_finess"].astype(str).str.len() == 9) &  
+        (donnees["annee"].astype(str).str.match(year_regex)) &    
+        (donnees["annee"] >= annee_actuelle - 10) &              
+        (donnees["numero_finess"].astype(str).isin(numéros_finess_liste))
     ]
 
-def import_vigie_rh_profession_filiere(dbEngine: Engine, logger_helios) -> None:
-    # Chemin Parquet
-    FILE_PATH_PARQUET = os.path.join(
-        os.path.dirname(__file__),
-        'exploration-des-données/vigie_rh/fichiers/parquet/vigierh_anneemois_profession1_2024_01_01.parquet'
-    )
+    return donnees_filtrées
 
-    # Paramètres
-    TABLE_NAME = "vigierh_profession_filiere"
-    COLUMN_MAPPING = {
+if __name__ == "__main__":
+    source = 'VigieRh'
+    # Paramètres pour les données de profession_filiere
+    table_profession_filiere = "vigierh_profession_filiere"
+    colum_mapping_profession_filiere = {
         'finess_et': 'numero_finess',
         'year': 'annee',
         'month': 'mois',
@@ -38,21 +44,33 @@ def import_vigie_rh_profession_filiere(dbEngine: Engine, logger_helios) -> None:
         'nation_turnover':'nation_turnover',
         'groupe_turnover':'groupe_turnover',
     }
+    prefix_fichier_profession_filiere = "vigierh_anneemois_profession1_"
 
-    df = lis_le_fichier_parquet(FILE_PATH_PARQUET, COLUMN_MAPPING)
-    df_filtré = filter_contrat_data(df)
+    # Paramètres pour les données de référence des types de profession_filiere
+    table_ref = "vigierh_ref_profession_filiere"
+    colum_mapping_ref = {
+        'profession1_code': 'code',
+        'profession1': 'label'
+    }
+    prefix_fichier_ref = "vigierh_ref_profession1"
 
-    supprimer_donnees_existantes(dbEngine, 'VigieRh', TABLE_NAME, logger_helios)
-    inserer_nouvelles_donnees(dbEngine, 'VigieRh', TABLE_NAME, df_filtré, logger_helios)
-
-if __name__ == "__main__":
+    # Initialisations
     logger_helios, variables_d_environnement = initialise_les_dépendances()
-    base_de_données_helios = create_engine(variables_d_environnement["DATABASE_URL"])
+    base_de_données = create_engine(variables_d_environnement["DATABASE_URL"])
 
     vegie_rh_data_path = variables_d_environnement["VIGIE_RH_DATA_PATH"]
     fichiers = os.listdir(vegie_rh_data_path)
 
-    prefix_fichier = "vigierh_anneemois_profession1_"
-    chemin_local_du_fichier_contrat = os.path.join(vegie_rh_data_path, trouve_le_nom_du_fichier(fichiers, prefix_fichier, logger_helios))
+    chemin_local_du_fichier_profession_filiere = os.path.join(vegie_rh_data_path, trouve_le_nom_du_fichier(fichiers, prefix_fichier_profession_filiere, logger_helios))
+    chemin_local_du_fichier_ref = os.path.join(vegie_rh_data_path, trouve_le_nom_du_fichier(fichiers, prefix_fichier_ref, logger_helios))
 
-    import_vigie_rh_profession_filiere(base_de_données_helios, chemin_local_du_fichier_contrat, logger_helios)
+    # Traitements des données
+    supprimer_donnees_existantes(table_profession_filiere, base_de_données, source, logger_helios)
+    supprimer_donnees_existantes(table_ref, base_de_données, source, logger_helios)
+
+    df_ref = lis_le_fichier_parquet(chemin_local_du_fichier_ref, colum_mapping_ref)
+    inserer_nouvelles_donnees(table_ref, base_de_données, source, df_ref, logger_helios)
+
+    df = lis_le_fichier_parquet(chemin_local_du_fichier_profession_filiere, colum_mapping_profession_filiere)
+    df_filtré = filter_profession_filiere_data(df, base_de_données)
+    inserer_nouvelles_donnees(table_profession_filiere, base_de_données, source, df_filtré, logger_helios)
--- datacrawler/import_vigie_rh_profession_groupe.py
+++ datacrawler/import_vigie_rh_profession_groupe.py
@@ -1,25 +1,36 @@
 import os
 import pandas as pd
+from datetime import datetime
 from sqlalchemy.engine import Engine, create_engine
 from datacrawler import supprimer_donnees_existantes, inserer_nouvelles_donnees
 from datacrawler.dependencies.dépendances import initialise_les_dépendances
 from datacrawler.extract.lecteur_parquet import lis_le_fichier_parquet
+from datacrawler.extract.trouve_le_nom_du_fichier import trouve_le_nom_du_fichier
+from datacrawler.extract.lecteur_sql import récupère_les_numéros_finess_des_établissements_de_la_base
 
-def filter_contrat_data(donnees: pd.DataFrame) -> pd.DataFrame:
-    return donnees[
-        donnees["numero_finess"].astype(str).str.len() == 9
+def filter_profession_groupe_data(donnees: pd.DataFrame, base_de_donnees) -> pd.DataFrame:
+    numéros_finess_des_établissements_connus = récupère_les_numéros_finess_des_établissements_de_la_base(base_de_donnees)
+    numéros_finess_liste = numéros_finess_des_établissements_connus['numero_finess_etablissement_territorial'].astype(str).tolist()
+
+    # Année actuelle pour le filtre
+    annee_actuelle = datetime.now().year
+    year_regex = r"((20[012]\d{1}|19\d{2}))"
+
+    # Filtrer les données
+    donnees_filtrées = donnees[
+        (donnees["numero_finess"].astype(str).str.len() == 9) &  
+        (donnees["annee"].astype(str).str.match(year_regex)) &    
+        (donnees["annee"] >= annee_actuelle - 10) &              
+        (donnees["numero_finess"].astype(str).isin(numéros_finess_liste))
     ]
 
-def import_vigie_rh_profession_groupe(dbEngine: Engine, logger_helios) -> None:
-    # Chemin Parquet
-    FILE_PATH_PARQUET = os.path.join(
-        os.path.dirname(__file__),
-        'exploration-des-données/vigie_rh/fichiers/parquet/vigierh_anneemois_profession2_2024_01_01.parquet'
-    )
+    return donnees_filtrées
 
-    # Paramètres
-    TABLE_NAME = "vigierh_profession_groupe"
-    COLUMN_MAPPING = {
+if __name__ == "__main__":
+    source = 'VigieRh'
+    # Paramètres pour les données de profession_groupe
+    table_profession_groupe = "vigierh_profession_groupe"
+    colum_mapping_profession_groupe = {
         'finess_et': 'numero_finess',
         'year': 'annee',
         'month': 'mois',
@@ -29,15 +40,72 @@ def import_vigie_rh_profession_groupe(dbEngine: Engine, logger_helios) -> None:
         'ind_redr_effectif_code':'indic_redressement_effectif_code',
         'ind_masq_effectif_code':'indic_masque_secret_effectif_code',
     }
+    prefix_fichier_profession_groupe = "vigierh_anneemois_profession2"
 
-    df = lis_le_fichier_parquet(FILE_PATH_PARQUET, COLUMN_MAPPING)
-    df_filtré = filter_contrat_data(df)
+    # Paramètres pour les données de référence des types de ref_masque
+    table_ref_masque = "vigierh_ref_masque"
+    colum_mapping_ref_masque = {
+        'ind_masque_code': 'code',
+        'ind_masque': 'label'
+    }
+    prefix_fichier_ref_masque = "vigierh_ref_masque"
 
-    supprimer_donnees_existantes(dbEngine, 'VigieRh', TABLE_NAME, logger_helios)
-    inserer_nouvelles_donnees(dbEngine, 'VigieRh', TABLE_NAME, df_filtré, logger_helios)
+    # Paramètres pour les données de référence des types de vigierh_ref_profession_groupe
+    table_ref_profession_groupe = "vigierh_ref_profession_groupe"
+    colum_mapping_ref_profession_groupe = {
+        'profession2_code': 'code',
+        'profession2': 'label'
+    }
+    prefix_fichier_ref_profession_groupe = "vigierh_ref_profession2"
 
-if __name__ == "__main__":
+    # Paramètres pour les données de référence des types de vigierh_ref_qualite
+    table_ref_qualite = "vigierh_ref_qualite"
+    colum_mapping_ref_qualite = {
+        'ind_qualite_code': 'code',
+        'ind_qualite': 'label'
+    }
+    prefix_fichier_ref_qualite = "vigierh_ref_qualite"
+
+    # Paramètres pour les données de référence des types de vigierh_ref_redressement
+    table_ref_redressement = "vigierh_ref_redressement"
+    colum_mapping_ref_redressement = {
+        'ind_redressement_code': 'code',
+        'ind_redressement': 'label'
+    }
+    prefix_fichier_ref_redressement = "vigierh_ref_redressement"
+
+    # Initialisations
     logger_helios, variables_d_environnement = initialise_les_dépendances()
-    base_de_données_helios = create_engine(variables_d_environnement["DATABASE_URL"])
+    base_de_données = create_engine(variables_d_environnement["DATABASE_URL"])
 
-    import_vigie_rh_profession_groupe(base_de_données_helios, logger_helios)
+    vegie_rh_data_path = variables_d_environnement["VIGIE_RH_DATA_PATH"]
+    fichiers = os.listdir(vegie_rh_data_path)
+
+    chemin_local_du_fichier_profession_groupe = os.path.join(vegie_rh_data_path, trouve_le_nom_du_fichier(fichiers, prefix_fichier_profession_groupe, logger_helios))
+    chemin_local_du_fichier_ref_masque = os.path.join(vegie_rh_data_path, trouve_le_nom_du_fichier(fichiers, prefix_fichier_ref_masque, logger_helios))
+    chemin_local_du_fichier_ref_profession_groupe = os.path.join(vegie_rh_data_path, trouve_le_nom_du_fichier(fichiers, prefix_fichier_ref_profession_groupe, logger_helios))
+    chemin_local_du_fichier_ref_qualite = os.path.join(vegie_rh_data_path, trouve_le_nom_du_fichier(fichiers, prefix_fichier_ref_qualite, logger_helios))
+    chemin_local_du_fichier_ref_redressement = os.path.join(vegie_rh_data_path, trouve_le_nom_du_fichier(fichiers, prefix_fichier_ref_redressement, logger_helios))
+
+    # Traitements des données
+    supprimer_donnees_existantes(table_profession_groupe, base_de_données, source, logger_helios)
+    supprimer_donnees_existantes(table_ref_masque, base_de_données, source, logger_helios)
+    supprimer_donnees_existantes(table_ref_profession_groupe, base_de_données, source, logger_helios)
+    supprimer_donnees_existantes(table_ref_qualite, base_de_données, source, logger_helios)
+    supprimer_donnees_existantes(table_ref_redressement, base_de_données, source, logger_helios)
+
+    df_ref_masque = lis_le_fichier_parquet(chemin_local_du_fichier_ref_masque, colum_mapping_ref_masque)
+    inserer_nouvelles_donnees(table_ref_masque, base_de_données, source, df_ref_masque, logger_helios)
+
+    df_ref_profession_groupe = lis_le_fichier_parquet(chemin_local_du_fichier_ref_profession_groupe, colum_mapping_ref_profession_groupe)
+    inserer_nouvelles_donnees(table_ref_profession_groupe, base_de_données, source, df_ref_profession_groupe, logger_helios)
+
+    df_ref_qualite = lis_le_fichier_parquet(chemin_local_du_fichier_ref_qualite, colum_mapping_ref_qualite)
+    inserer_nouvelles_donnees(table_ref_qualite, base_de_données, source, df_ref_qualite, logger_helios)
+
+    df_ref_redressement = lis_le_fichier_parquet(chemin_local_du_fichier_ref_redressement, colum_mapping_ref_redressement)
+    inserer_nouvelles_donnees(table_ref_redressement, base_de_données, source, df_ref_redressement, logger_helios)
+
+    df = lis_le_fichier_parquet(chemin_local_du_fichier_profession_groupe, colum_mapping_profession_groupe)
+    df_filtré = filter_profession_groupe_data(df, base_de_données)
+    inserer_nouvelles_donnees(table_profession_groupe, base_de_données, source, df_filtré, logger_helios)
