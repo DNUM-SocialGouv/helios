2debe8762110ac3e36dbce6876fdf95c89427b20
nbenyahiadev <117667297+nbenyahiadev@users.noreply.github.com>
2025-02-14 12:15:22 +0100

    (hel-730) HEL-730 add delete many ets (#673)
    * (hel-730) HEL-730 add delete many ets
    
    * (hel-730) HEL-730 fix unit test of delete list ets

--- src/frontend/ui/liste/ListActionsButton.tsx
+++ src/frontend/ui/liste/ListActionsButton.tsx
@@ -2,28 +2,39 @@ import { useRouter } from "next/router";
 import { useState } from "react";
 
 import { useDependencies } from "../commun/contexts/useDependencies";
+import { SelectedRows } from "../commun/Table/Table";
+import { useFavoris } from "../favoris/useFavoris";
 import { ComparaisonViewModel } from "../home/ComparaisonViewModel";
 import { RechercheViewModel } from "../home/RechercheViewModel";
 import styles from "./ListActionsButton.module.css"
 
 type ListActionsButtonProps = Readonly<{
     selectedRows: RechercheViewModel[] | ComparaisonViewModel[] | (RechercheViewModel | ComparaisonViewModel)[];
+    setSelectedRows: React.Dispatch<React.SetStateAction<SelectedRows>>
+    listId: number;
 }>;
 
-export const ListActionsButton = ({ selectedRows }: ListActionsButtonProps) => {
+export const ListActionsButton = ({ selectedRows, setSelectedRows, listId }: ListActionsButtonProps) => {
     const { wording } = useDependencies();
     const router = useRouter();
-
+    const { removeFromFavorisList } = useFavoris()
     const [displayActions, setDisplayActions] = useState<boolean>(false);
+    const listFinessNumbers = selectedRows.map((row) => row.numéroFiness);
 
     const lancerComparaison = () => {
         const firstType = selectedRows[0].type;
-        const listFinessNumbers = selectedRows.map((row) => row.numéroFiness);
         sessionStorage.setItem("listFinessNumbers", JSON.stringify(listFinessNumbers));
         sessionStorage.setItem("comparaisonType", firstType);
         document.cookie = `list=${encodeURIComponent(JSON.stringify(listFinessNumbers))}; path=/`;
         document.cookie = `type=${encodeURIComponent(firstType)}; path=/`;
         router.push("/comparaison");
+    }
+
+    const onClickDelete = async () => {
+        await removeFromFavorisList(listFinessNumbers, listId);
+        setSelectedRows([]);
+        setDisplayActions(false);
+        router.replace(router.asPath);
     }
 
     return (
@@ -37,7 +48,7 @@ export const ListActionsButton = ({ selectedRows }: ListActionsButtonProps) => {
                         </button>
                     </li>
                     <li className={styles["menu-item"]}>
-                        <button className="fr-btn fr-btn--tertiary-no-outline">
+                        <button className="fr-btn fr-btn--tertiary-no-outline" disabled={selectedRows.length === 0} onClick={onClickDelete}>
                             {wording.SUPPRIMER_DE_LA_LISTE}
                         </button>
                     </li>
