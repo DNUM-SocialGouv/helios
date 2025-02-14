6c80f5d273fc95d775892441ca2a5150dcbf0f81
Adolato <a.elechi@cat-amania.com>
2025-02-14 16:35:13 +0100

    upate file .env

--- datacrawler/dependencies/variables_d_environnement/variables_d_environnement.py
+++ datacrawler/dependencies/variables_d_environnement/variables_d_environnement.py
@@ -16,6 +16,7 @@ class VariablesDEnvironnement(TypedDict):
     SIICEA_DATA_PATH: str
     CHECKED_SIICEA_DATA_PATH: str
     HAPI_DATA_PATH: str
+    VIGIE_RH_DATA_PATH: str
 
 
 def récupère_les_variables_d_environnement(
