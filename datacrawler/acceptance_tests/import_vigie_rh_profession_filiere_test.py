e85b7c4afc00c2b6db5ef40bf10513a65c4606e8
Adolato <a.elechi@cat-amania.com>
2025-02-18 12:05:49 +0100

    traitement des retours v2

--- datacrawler/acceptance_tests/import_vigie_rh_profession_filiere_test.py
+++ datacrawler/acceptance_tests/import_vigie_rh_profession_filiere_test.py
@@ -13,7 +13,7 @@ from datacrawler.test_helpers import (
 from datacrawler.extract.extrais_la_date_du_nom_de_fichier import extrais_la_date_du_nom_de_fichier_vigie_rh
 from datacrawler.extract.lecteur_parquet import lis_le_fichier_parquet
 from datacrawler.transform.equivalence_vigierh_helios import SOURCE, Table, ColumMapping
-from datacrawler.import_vigie_rh_contrat import filter_contrat_data
+from datacrawler.import_vigie_rh_profession_filiere import filter_profession_filiere_data
 from datacrawler import supprimer_donnees_existantes, inserer_nouvelles_donnees
 
 NUMÉRO_FINESS_ENTITÉ_JURIDIQUE = "010008407"
@@ -21,7 +21,7 @@ NUMÉRO_FINESS_ÉTABLISSEMENT_1 = "010001261"
 NUMÉRO_FINESS_ÉTABLISSEMENT_2 = "010001436"
 NUMÉRO_FINESS_ÉTABLISSEMENT_3 = "010001741"
 
-class TestImportVigieRhContrat:
+class TestImportVigiePprofessionFiliere:
     def setup_method(self) -> None:
         supprime_les_données_des_tables(base_de_données_test)
         sauvegarde_une_entité_juridique_en_base(NUMÉRO_FINESS_ENTITÉ_JURIDIQUE, base_de_données_test)
@@ -30,63 +30,62 @@ class TestImportVigieRhContrat:
         sauvegarde_un_établissement_en_base(NUMÉRO_FINESS_ÉTABLISSEMENT_3, NUMÉRO_FINESS_ENTITÉ_JURIDIQUE, base_de_données_test)
 
 
-    def test_import_vigie_rh_contrat(self) -> None:
+    def test_import_vigie_rh_profession_filiere_test(self) -> None:
         # Initialisations
         vegie_rh_data_path = 'data_test/entrée/vigie_rh'
         fichiers = os.listdir(vegie_rh_data_path)
 
-        chemin_local_du_fichier_contrat = os.path.join(
+        chemin_local_du_fichier_profession_filiere = os.path.join(
             vegie_rh_data_path,
-            trouve_le_nom_du_fichier(fichiers, FichierSource.VIGIE_RH_CONTRAT.value, mocked_logger)
+            trouve_le_nom_du_fichier(fichiers, FichierSource.VIGIE_RH_PROFESSION_FILIERE.value, mocked_logger)
         )
 
-        assert chemin_local_du_fichier_contrat == 'data_test/entrée/vigie_rh/vigierh_contrat_2024_01_01.parquet'
+        assert chemin_local_du_fichier_profession_filiere == 'data_test/entrée/vigie_rh/vigierh_profession1_2024_01_01.parquet'
 
         chemin_local_du_fichier_ref = os.path.join(
             vegie_rh_data_path,
-            trouve_le_nom_du_fichier(fichiers, FichierSource.VIGIE_RH_REF_TYPE_CONTRAT.value, mocked_logger)
+            trouve_le_nom_du_fichier(fichiers, FichierSource.VIGIE_RH_REF_PROFESSION_FILIERE.value, mocked_logger)
         )
 
-        assert chemin_local_du_fichier_ref == 'data_test/entrée/vigie_rh/vigierh_ref_nature_contrat.parquet'
+        assert chemin_local_du_fichier_ref == 'data_test/entrée/vigie_rh/vigierh_ref_profession1.parquet'
 
-        date_de_mise_à_jour = extrais_la_date_du_nom_de_fichier_vigie_rh(chemin_local_du_fichier_contrat)
+        date_de_mise_à_jour = extrais_la_date_du_nom_de_fichier_vigie_rh(chemin_local_du_fichier_profession_filiere)
 
         assert date_de_mise_à_jour == '2024-01-01'
 
         # Traitements des données
-        df_ref = lis_le_fichier_parquet(chemin_local_du_fichier_ref, ColumMapping.REF_TYPE_CONTRAT.value)
+        df_ref = lis_le_fichier_parquet(chemin_local_du_fichier_ref, ColumMapping.REF_PROFESSION_FILIERE.value)
 
         nombre_de_lignes = df_ref.shape[0]
-        assert nombre_de_lignes == 3
+        assert nombre_de_lignes == 5
 
-        data_frame = lis_le_fichier_parquet(chemin_local_du_fichier_contrat, ColumMapping.CONTRAT.value)
-        df_filtré = filter_contrat_data(data_frame, base_de_données_test)
+        data_frame = lis_le_fichier_parquet(chemin_local_du_fichier_profession_filiere, ColumMapping.PROFESSION_FILIERE.value)
+        df_filtré = filter_profession_filiere_data(data_frame, base_de_données_test)
 
         nombre_de_lignes = df_filtré.shape[0]
-        assert nombre_de_lignes == 190
+        assert nombre_de_lignes == 74
 
-        supprimer_donnees_existantes(Table.CONTRAT.value, base_de_données_test, SOURCE, mocked_logger)
+        supprimer_donnees_existantes(Table.PROFESSION_FILIERE.value, base_de_données_test, SOURCE, mocked_logger)
 
-        nb_lignes = compte_nombre_de_lignes(Table.CONTRAT.value, base_de_données_test)
+        nb_lignes = compte_nombre_de_lignes(Table.PROFESSION_FILIERE.value, base_de_données_test)
         assert nb_lignes == 0
 
-        supprimer_donnees_existantes(Table.REF_TYPE_CONTRAT.value, base_de_données_test, SOURCE, mocked_logger)
+        supprimer_donnees_existantes(Table.REF_PROFESSION_FILIERE.value, base_de_données_test, SOURCE, mocked_logger)
 
-        nb_lignes = compte_nombre_de_lignes(Table.REF_TYPE_CONTRAT.value, base_de_données_test)
+        nb_lignes = compte_nombre_de_lignes(Table.REF_PROFESSION_FILIERE.value, base_de_données_test)
         assert nb_lignes == 0
 
-        inserer_nouvelles_donnees(Table.REF_TYPE_CONTRAT.value, base_de_données_test, SOURCE, df_ref, mocked_logger)
+        inserer_nouvelles_donnees(Table.REF_PROFESSION_FILIERE.value, base_de_données_test, SOURCE, df_ref, mocked_logger)
 
-        nb_lignes = compte_nombre_de_lignes(Table.REF_TYPE_CONTRAT.value, base_de_données_test)
-        assert nb_lignes == 3
+        nb_lignes = compte_nombre_de_lignes(Table.REF_PROFESSION_FILIERE.value, base_de_données_test)
+        assert nb_lignes == 5
 
         inserer_nouvelles_donnees(
-            Table.CONTRAT.value,
+            Table.PROFESSION_FILIERE.value,
             base_de_données_test,
-            SOURCE,
-            df_filtré,
+            SOURCE, df_filtré,
             mocked_logger,
-            FichierSource.VIGIE_RH_CONTRAT,
+            FichierSource.VIGIE_RH_PROFESSION_FILIERE,
             date_de_mise_à_jour
         )
         nb_lignes = compte_nombre_de_lignes(Table.CONTRAT.value, base_de_données_test)
