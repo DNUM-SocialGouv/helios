e0e6e8dd01749f4d0172e1f46cd6fefd8511ad46
Adolato <a.elechi@cat-amania.com>
2025-02-21 19:13:10 +0100

    update profession_groupe

--- datacrawler/acceptance_tests/import_vigie_rh_profession_groupe_test.py
+++ datacrawler/acceptance_tests/import_vigie_rh_profession_groupe_test.py
@@ -1,6 +1,7 @@
 import os
 from typing import Dict, Tuple
 import pandas as pd
+import numpy as np
 from datacrawler.extract.trouve_le_nom_du_fichier import trouve_le_nom_du_fichier
 from datacrawler.test_helpers import (
     base_de_données_test,
@@ -16,18 +17,16 @@ from datacrawler.transform.equivalence_vigierh_helios import SOURCE, ColumMappin
 from datacrawler.load.nom_des_tables import (
     FichierSource,
     TABLE_PROFESSION_GROUPE,
-    TABLE_REF_MASQUE,
     TABLE_REF_PROFESSION_GROUPE,
-    TABLE_REF_QUALITE,
-    TABLE_REF_REDRESSEMENT
 )
 from datacrawler.import_vigie_rh_profession_groupe import filter_profession_groupe_data
 from datacrawler import supprimer_donnees_existantes, inserer_nouvelles_donnees
 
 NUMÉRO_FINESS_ENTITÉ_JURIDIQUE = "010008407"
-NUMÉRO_FINESS_ÉTABLISSEMENT_1 = "010001261"
-NUMÉRO_FINESS_ÉTABLISSEMENT_2 = "010001436"
-NUMÉRO_FINESS_ÉTABLISSEMENT_3 = "010001741"
+NUMÉRO_FINESS_ÉTABLISSEMENT_1 = "010002228"
+NUMÉRO_FINESS_ÉTABLISSEMENT_2 = "470001702"
+NUMÉRO_FINESS_ÉTABLISSEMENT_3 = "470005356"
+
 class TestImportVigiePprofessionGroupe:
     def setup_method(self) -> None:
         supprime_les_données_des_tables(base_de_données_test)
@@ -47,141 +46,70 @@ class TestImportVigiePprofessionGroupe:
                     mocked_logger
                 )
             ),
-            'ref_masque': os.path.join(
-                vegie_rh_data_path,
-                trouve_le_nom_du_fichier(
-                    fichiers,
-                    FichierSource.VIGIE_RH_REF_MASQUE.value,
-                    mocked_logger
-                )
-            ),
             'ref_profession_groupe': os.path.join(
                 vegie_rh_data_path, trouve_le_nom_du_fichier(
                     fichiers,
                     FichierSource.VIGIE_RH_REF_PROFESSION_GROUPE.value,
                     mocked_logger
                 )
-            ),
-            'ref_qualite': os.path.join(
-                vegie_rh_data_path, trouve_le_nom_du_fichier(
-                    fichiers,
-                    FichierSource.VIGIE_RH_REF_QUALITE.value,
-                    mocked_logger
-                )
-            ),
-            'ref_redressement': os.path.join(
-                vegie_rh_data_path, trouve_le_nom_du_fichier(
-                    fichiers,
-                    FichierSource.VIGIE_RH_REF_REDRESSEMENT.value,
-                    mocked_logger
-                )
-            ),
+            )
         }
 
     def _assert_file_paths(self, file_paths: Dict[str, str]) -> None:
-        assert file_paths['profession_groupe'] == 'data_test/entrée/vigie_rh/vigierh_profession2_2024_01_01.parquet'
-        assert file_paths['ref_masque'] == 'data_test/entrée/vigie_rh/vigierh_ref_masque_2024_01_01.parquet'
-        assert file_paths['ref_profession_groupe'] == 'data_test/entrée/vigie_rh/vigierh_ref_profession2_2024_01_01.parquet'
-        assert file_paths['ref_qualite'] == 'data_test/entrée/vigie_rh/vigierh_ref_qualite_2024_01_01.parquet'
-        assert file_paths['ref_redressement'] == 'data_test/entrée/vigie_rh/vigierh_ref_redressement_2024_01_01.parquet'
+        assert file_paths['profession_groupe'] == 'data_test/entrée/vigie_rh/vigierh_profession2_2025_02_13.parquet'
+        assert file_paths['ref_profession_groupe'] == 'data_test/entrée/vigie_rh/vigierh_ref_profession2_2025_02_13.parquet'
 
     def _extract_dates(self, file_paths: Dict[str, str]) -> Dict[str, str]:
         return {
             'profession_groupe': extrais_la_date_du_nom_de_fichier_vigie_rh(file_paths['profession_groupe']),
-            'ref_masque': extrais_la_date_du_nom_de_fichier_vigie_rh(file_paths['ref_masque']),
             'ref_profession_groupe': extrais_la_date_du_nom_de_fichier_vigie_rh(file_paths['ref_profession_groupe']),
-            'ref_qualite': extrais_la_date_du_nom_de_fichier_vigie_rh(file_paths['ref_qualite']),
-            'ref_redressement': extrais_la_date_du_nom_de_fichier_vigie_rh(file_paths['ref_redressement']),
         }
 
     def _assert_dates(self, dates: Dict[str, str]) -> None:
-        assert dates['profession_groupe'] == '2024-01-01'
-        assert dates['ref_masque'] == '2024-01-01'
-        assert dates['ref_profession_groupe'] == '2024-01-01'
-        assert dates['ref_qualite'] == '2024-01-01'
-        assert dates['ref_redressement'] == '2024-01-01'
+        assert dates['profession_groupe'] == '2025-02-13'
+        assert dates['ref_profession_groupe'] == '2025-02-13'
 
-    def _read_and_assert_dataframes(self, file_paths: Dict[str, str]) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
-        df_ref_masque = lis_le_fichier_parquet(file_paths['ref_masque'], ColumMapping.REF_MASQUE.value)
+    def _read_and_assert_dataframes(self, file_paths: Dict[str, str]) -> Tuple[pd.DataFrame, pd.DataFrame]:
         df_ref_profession_groupe = lis_le_fichier_parquet(file_paths['ref_profession_groupe'], ColumMapping.REF_PROFESSION_GROUPE.value)
-        df_ref_qualite = lis_le_fichier_parquet(file_paths['ref_qualite'], ColumMapping.REF_QUALITE.value)
-        df_ref_redressement = lis_le_fichier_parquet(file_paths['ref_redressement'], ColumMapping.REF_REDRESSEMENT.value)
+        code_list_ref_profession_groupe = np.array(df_ref_profession_groupe['code'].tolist())
+
         data_frame = lis_le_fichier_parquet(file_paths['profession_groupe'], ColumMapping.PROFESSION_GROUPE.value)
-        df_filtré = filter_profession_groupe_data(data_frame, base_de_données_test)
+        df_filtré = filter_profession_groupe_data(data_frame, code_list_ref_profession_groupe, base_de_données_test).head(200)
 
-        assert df_ref_masque.shape[0] == 1
         assert df_ref_profession_groupe.shape[0] == 16
-        assert df_ref_qualite.shape[0] == 1
-        assert df_ref_redressement.shape[0] == 2
-        assert df_filtré.shape[0] == 198
+        assert df_filtré.shape[0] == 200
 
-        return df_ref_masque, df_ref_profession_groupe, df_ref_qualite, df_ref_redressement, df_filtré
+        return df_ref_profession_groupe, df_filtré
 
     def _clear_existing_data(self) -> None:
         supprimer_donnees_existantes(TABLE_PROFESSION_GROUPE, base_de_données_test, SOURCE, mocked_logger)
-        supprimer_donnees_existantes(TABLE_REF_MASQUE, base_de_données_test, SOURCE, mocked_logger)
         supprimer_donnees_existantes(TABLE_REF_PROFESSION_GROUPE, base_de_données_test, SOURCE, mocked_logger)
-        supprimer_donnees_existantes(TABLE_REF_QUALITE, base_de_données_test, SOURCE, mocked_logger)
-        supprimer_donnees_existantes(TABLE_REF_REDRESSEMENT, base_de_données_test, SOURCE, mocked_logger)
 
         assert compte_nombre_de_lignes(TABLE_PROFESSION_GROUPE, base_de_données_test) == 0
-        assert compte_nombre_de_lignes(TABLE_REF_MASQUE, base_de_données_test) == 0
         assert compte_nombre_de_lignes(TABLE_REF_PROFESSION_GROUPE, base_de_données_test) == 0
-        assert compte_nombre_de_lignes(TABLE_REF_QUALITE, base_de_données_test) == 0
-        assert compte_nombre_de_lignes(TABLE_REF_REDRESSEMENT, base_de_données_test) == 0
 
-    def _insert_new_data(self, data_frames: Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame], dates: Dict[str, str]) -> None:
-        inserer_nouvelles_donnees(
-            TABLE_REF_MASQUE,
-            base_de_données_test,
-            SOURCE,
-            data_frames[0],
-            mocked_logger,
-            FichierSource.VIGIE_RH_REF_MASQUE,
-            dates['ref_masque']
-        )
+    def _insert_new_data(self, data_frames: Tuple[pd.DataFrame, pd.DataFrame], dates: Dict[str, str]) -> None:
         inserer_nouvelles_donnees(
             TABLE_REF_PROFESSION_GROUPE,
             base_de_données_test,
             SOURCE,
-            data_frames[1],
+            data_frames[0],
             mocked_logger,
             FichierSource.VIGIE_RH_REF_PROFESSION_GROUPE,
             dates['ref_profession_groupe']
         )
         inserer_nouvelles_donnees(
-            TABLE_REF_QUALITE,
-            base_de_données_test,
-            SOURCE,
-            data_frames[2],
-            mocked_logger,
-            FichierSource.VIGIE_RH_REF_QUALITE,
-            dates['ref_qualite']
-        )
-        inserer_nouvelles_donnees(
-            TABLE_REF_REDRESSEMENT,
-            base_de_données_test,
-            SOURCE,
-            data_frames[3],
-            mocked_logger,
-            FichierSource.VIGIE_RH_REF_REDRESSEMENT,
-            dates['ref_redressement']
-        )
-        inserer_nouvelles_donnees(
             TABLE_PROFESSION_GROUPE,
             base_de_données_test,
             SOURCE,
-            data_frames[4],
+            data_frames[1],
             mocked_logger,
             FichierSource.VIGIE_RH_PROFESSION_GROUPE,
             dates['profession_groupe']
         )
 
-        assert compte_nombre_de_lignes(TABLE_REF_MASQUE, base_de_données_test) == 1
         assert compte_nombre_de_lignes(TABLE_REF_PROFESSION_GROUPE, base_de_données_test) == 16
-        assert compte_nombre_de_lignes(TABLE_REF_QUALITE, base_de_données_test) == 1
-        assert compte_nombre_de_lignes(TABLE_REF_REDRESSEMENT, base_de_données_test) == 2
-        assert compte_nombre_de_lignes(TABLE_PROFESSION_GROUPE, base_de_données_test) == 198
+        assert compte_nombre_de_lignes(TABLE_PROFESSION_GROUPE, base_de_données_test) == 200
 
     def test_import_vigie_rh_profession_groupe_test(self) -> None:
         vegie_rh_data_path = 'data_test/entrée/vigie_rh'
--- datacrawler/import_vigie_rh_profession_groupe.py
+++ datacrawler/import_vigie_rh_profession_groupe.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import numpy as np
 from sqlalchemy.engine import create_engine, Engine
 from datacrawler import supprimer_donnees_existantes, inserer_nouvelles_donnees, verifie_si_le_fichier_est_traite
 from datacrawler.dependencies.dépendances import initialise_les_dépendances
@@ -10,24 +11,29 @@ from datacrawler.transform.equivalence_vigierh_helios import SOURCE, ColumMappin
 from datacrawler.load.nom_des_tables import (
     FichierSource,
     TABLE_PROFESSION_GROUPE,
-    TABLE_REF_MASQUE,
     TABLE_REF_PROFESSION_GROUPE,
-    TABLE_REF_QUALITE,
-    TABLE_REF_REDRESSEMENT
 )
 from datacrawler.extract.extrais_la_date_du_nom_de_fichier import extrais_la_date_du_nom_de_fichier_vigie_rh
 
-def filter_profession_groupe_data(donnees: pd.DataFrame, base_de_donnees: Engine) -> pd.DataFrame:
+def filter_profession_groupe_data(donnees: pd.DataFrame, ref_code: np.ndarray, base_de_donnees: Engine) -> pd.DataFrame:
+
     numéros_finess_des_établissements_connus = récupère_les_numéros_finess_des_établissements_de_la_base(base_de_donnees)
     numéros_finess_liste = numéros_finess_des_établissements_connus['numero_finess_etablissement_territorial'].astype(str).tolist()
 
     year_regex = r"(19\d{2}|2\d{3})"
 
+   # Convertir 'mois' et 'quarter' en nombres entiers après gestion des flottants
+    donnees["mois"] = pd.to_numeric(donnees["mois"], errors='coerce').fillna(0).astype(int)
+    donnees["quarter"] = pd.to_numeric(donnees["quarter"], errors='coerce').fillna(0).astype(int)
+
     # Filtrer les données
     donnees_filtrées = donnees[
         (donnees["numero_finess"].astype(str).str.len() == 9) &
+        (donnees["numero_finess"].astype(str).isin(numéros_finess_liste)) &
         (donnees["annee"].astype(str).str.match(year_regex)) &
-        (donnees["numero_finess"].astype(str).isin(numéros_finess_liste))
+        (donnees["mois"].astype(str).astype(int).between(1, 12)) &
+        (donnees["quarter"].astype(str).astype(int).between(1, 4)) &
+        (donnees["profession_code"].isin(ref_code))
     ]
 
     return donnees_filtrées
@@ -45,21 +51,9 @@ if __name__ == "__main__":
         vegie_rh_data_path,
         trouve_le_nom_du_fichier(fichiers, FichierSource.VIGIE_RH_PROFESSION_GROUPE.value, logger_helios)
     )
-    chemin_local_du_fichier_ref_masque = os.path.join(
-        vegie_rh_data_path,
-        trouve_le_nom_du_fichier(fichiers, FichierSource.VIGIE_RH_REF_MASQUE.value, logger_helios)
-    )
     chemin_local_du_fichier_ref_profession_groupe = os.path.join(
         vegie_rh_data_path,
         trouve_le_nom_du_fichier(fichiers, FichierSource.VIGIE_RH_REF_PROFESSION_GROUPE.value, logger_helios)
-    )
-    chemin_local_du_fichier_ref_qualite = os.path.join(
-        vegie_rh_data_path,
-        trouve_le_nom_du_fichier(fichiers, FichierSource.VIGIE_RH_REF_QUALITE.value, logger_helios)
-    )
-    chemin_local_du_fichier_ref_redressement = os.path.join(
-        vegie_rh_data_path,
-        trouve_le_nom_du_fichier(fichiers, FichierSource.VIGIE_RH_REF_REDRESSEMENT.value, logger_helios)
     )
 
     date_de_mise_à_jour_profession_groupe = extrais_la_date_du_nom_de_fichier_vigie_rh(chemin_local_du_fichier_profession_groupe)
@@ -69,13 +63,6 @@ if __name__ == "__main__":
         FichierSource.VIGIE_RH_PROFESSION_GROUPE.value
     )
 
-    date_de_mise_à_jour_ref_masque = extrais_la_date_du_nom_de_fichier_vigie_rh(chemin_local_du_fichier_ref_masque)
-    traite_ref_masque = verifie_si_le_fichier_est_traite(
-        date_de_mise_à_jour_ref_masque,
-        base_de_données,
-        FichierSource.VIGIE_RH_REF_MASQUE.value
-    )
-
     date_de_mise_à_jour_ref_profession_groupe = extrais_la_date_du_nom_de_fichier_vigie_rh(chemin_local_du_fichier_ref_profession_groupe)
     traite_ref_profession_groupe = verifie_si_le_fichier_est_traite(
         date_de_mise_à_jour_ref_profession_groupe,
@@ -83,52 +70,23 @@ if __name__ == "__main__":
         FichierSource.VIGIE_RH_REF_PROFESSION_GROUPE.value
     )
 
-    date_de_mise_à_jour_ref_qualite = extrais_la_date_du_nom_de_fichier_vigie_rh(chemin_local_du_fichier_ref_qualite)
-    traite_ref_qualite = verifie_si_le_fichier_est_traite(date_de_mise_à_jour_ref_qualite, base_de_données, FichierSource.VIGIE_RH_REF_QUALITE.value)
-
-    date_de_mise_à_jour_ref_redressement = extrais_la_date_du_nom_de_fichier_vigie_rh(chemin_local_du_fichier_ref_redressement)
-    traite_ref_redressement = verifie_si_le_fichier_est_traite(
-        date_de_mise_à_jour_ref_redressement,
-        base_de_données,
-        FichierSource.VIGIE_RH_REF_REDRESSEMENT.value
-    )
-
     # Traitements des données
-    if traite_profession_groupe and traite_ref_masque and traite_ref_profession_groupe and traite_ref_qualite and traite_ref_redressement:
+    if traite_profession_groupe and traite_ref_profession_groupe:
         logger_helios.info(f"Le fichier {FichierSource.VIGIE_RH_PROFESSION_GROUPE.value} a été déjà traité")
-        logger_helios.info(f"Le fichier {FichierSource.VIGIE_RH_REF_MASQUE.value} a été déjà traité")
         logger_helios.info(f"Le fichier {FichierSource.VIGIE_RH_REF_PROFESSION_GROUPE.value} a été déjà traité")
-        logger_helios.info(f"Le fichier {FichierSource.VIGIE_RH_REF_QUALITE.value} a été déjà traité")
-        logger_helios.info(f"Le fichier {FichierSource.VIGIE_RH_REF_REDRESSEMENT.value} a été déjà traité")
     else:
         if len({date_de_mise_à_jour_profession_groupe,
-        date_de_mise_à_jour_ref_masque,
-        date_de_mise_à_jour_ref_profession_groupe,
-        date_de_mise_à_jour_ref_qualite,
-        date_de_mise_à_jour_ref_redressement}) == 1:
+        date_de_mise_à_jour_ref_profession_groupe}) == 1:
 
-            df_ref_masque = lis_le_fichier_parquet(chemin_local_du_fichier_ref_masque, ColumMapping.REF_MASQUE.value)
             df_ref_profession_groupe = lis_le_fichier_parquet(chemin_local_du_fichier_ref_profession_groupe, ColumMapping.REF_PROFESSION_GROUPE.value)
-            df_ref_qualite = lis_le_fichier_parquet(chemin_local_du_fichier_ref_qualite, ColumMapping.REF_QUALITE.value)
-            df_ref_redressement = lis_le_fichier_parquet(chemin_local_du_fichier_ref_redressement, ColumMapping.REF_REDRESSEMENT.value)
+            code_list_ref_profession_groupe = np.array(df_ref_profession_groupe['code'].tolist())
+
             data_frame = lis_le_fichier_parquet(chemin_local_du_fichier_profession_groupe, ColumMapping.PROFESSION_GROUPE.value)
-            df_filtré = filter_profession_groupe_data(data_frame, base_de_données)
+            df_filtré = filter_profession_groupe_data(data_frame, code_list_ref_profession_groupe, base_de_données).head(1000)
 
             supprimer_donnees_existantes(TABLE_PROFESSION_GROUPE, base_de_données, SOURCE, logger_helios)
-            supprimer_donnees_existantes(TABLE_REF_MASQUE, base_de_données, SOURCE, logger_helios)
             supprimer_donnees_existantes(TABLE_REF_PROFESSION_GROUPE, base_de_données, SOURCE, logger_helios)
-            supprimer_donnees_existantes(TABLE_REF_QUALITE, base_de_données, SOURCE, logger_helios)
-            supprimer_donnees_existantes(TABLE_REF_REDRESSEMENT, base_de_données, SOURCE, logger_helios)
 
-            inserer_nouvelles_donnees(
-                TABLE_REF_MASQUE,
-                base_de_données,
-                SOURCE,
-                df_ref_masque,
-                logger_helios,
-                FichierSource.VIGIE_RH_REF_MASQUE,
-                date_de_mise_à_jour_ref_masque
-            )
             inserer_nouvelles_donnees(
                 TABLE_REF_PROFESSION_GROUPE,
                 base_de_données,
@@ -137,24 +95,6 @@ if __name__ == "__main__":
                 logger_helios,
                 FichierSource.VIGIE_RH_REF_PROFESSION_GROUPE,
                 date_de_mise_à_jour_ref_profession_groupe
-            )
-            inserer_nouvelles_donnees(
-                TABLE_REF_QUALITE,
-                base_de_données,
-                SOURCE,
-                df_ref_qualite,
-                logger_helios,
-                FichierSource.VIGIE_RH_REF_QUALITE,
-                date_de_mise_à_jour_ref_qualite
-            )
-            inserer_nouvelles_donnees(
-                TABLE_REF_REDRESSEMENT,
-                base_de_données,
-                SOURCE,
-                df_ref_redressement,
-                logger_helios,
-                FichierSource.VIGIE_RH_REF_REDRESSEMENT,
-                date_de_mise_à_jour_ref_redressement
             )
             inserer_nouvelles_donnees(
                 TABLE_PROFESSION_GROUPE,
@@ -169,8 +109,5 @@ if __name__ == "__main__":
             logger_helios.info(
                 f"[{SOURCE}]❌ Les dates des fichiers sources ne sont pas cohérentes. "
                 f"({FichierSource.VIGIE_RH_PROFESSION_GROUPE.value}, "
-                f"{FichierSource.VIGIE_RH_REF_REDRESSEMENT.value}, "
-                f"{FichierSource.VIGIE_RH_REF_QUALITE.value}, "
-                f"{FichierSource.VIGIE_RH_REF_PROFESSION_GROUPE.value}, "
-                f"{FichierSource.VIGIE_RH_REF_MASQUE.value})"
+                f"{FichierSource.VIGIE_RH_REF_PROFESSION_GROUPE.value})"
             )
