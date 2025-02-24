7a5c749edcdf4487b3f2adf1b59333881767cfa6
Adolato <a.elechi@cat-amania.com>
2025-02-24 11:54:12 +0100

    update migrations v3

--- database/migrations/vigie_rh/1738328763147-AjoutVigieRhProfessionFiliere.ts
+++ database/migrations/vigie_rh/1738328763147-AjoutVigieRhProfessionFiliere.ts
@@ -5,27 +5,23 @@ export class AjoutVigieRhProfessionFiliere1738328763147 implements MigrationInte
     // Création de la table
     await queryRunner.query(`
       CREATE TABLE vigierh_profession_filiere (
-        numero_finess VARCHAR(9) NOT NULL,
-        annee INT NOT NULL,
-        mois INT NOT NULL,
-        profession_code int NOT NULL,
-        turnover FLOAT,
-        taux_entrees FLOAT,
-        taux_sorties FLOAT,
-        nombre_entrees INT,
-        nombre_sorties INT,
-        region_turnover FLOAT,
-        nation_turnover FLOAT,
-        groupe_turnover FLOAT,
-        dt_creation DATE NOT NULL DEFAULT CURRENT_DATE,
-
-        PRIMARY KEY (numero_finess, annee, mois, profession_code)
+          numero_finess VARCHAR(9) NOT NULL,
+          annee INT NOT NULL,
+          mois INT NOT NULL,
+          profession_code INT NOT NULL,
+          turnover FLOAT,
+          taux_entrees FLOAT,
+          taux_sorties FLOAT,
+          nombre_entrees INT,
+          nombre_sorties INT,
+          region_turnover FLOAT,
+          nation_turnover FLOAT,
+          groupe_turnover FLOAT,
+          dt_creation DATE NOT NULL DEFAULT CURRENT_DATE,
+          PRIMARY KEY (numero_finess, annee, mois, profession_code),
+          CONSTRAINT fk_profession_filiere_profession FOREIGN KEY (profession_code) 
+          REFERENCES vigierh_ref_profession_filiere(code)
       );
-
-      ALTER TABLE vigierh_profession_filiere
-      ADD CONSTRAINT fk_profession_filiere_profession
-      FOREIGN KEY (profession_code)
-      REFERENCES vigierh_ref_profession_filiere(code);
     `);
 
     // TODO :  Ajout de l'index éventuellement   
@@ -33,9 +29,6 @@ export class AjoutVigieRhProfessionFiliere1738328763147 implements MigrationInte
 
   async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-      ALTER TABLE vigierh_profession_filiere
-      DROP CONSTRAINT fk_profession_filiere_profession;
-
       DROP TABLE IF EXISTS vigierh_profession_filiere;
     `);
   }
--- database/migrations/vigie_rh/1738328796095-AjoutVigieRhProfessionGroupe.ts
+++ database/migrations/vigie_rh/1738328796095-AjoutVigieRhProfessionGroupe.ts
@@ -4,22 +4,18 @@ export class AjoutVigieRhProfessionGroupe1738328796095 implements MigrationInter
   async up(queryRunner: QueryRunner): Promise<void> {
     // Création de la table vigie_rh_profession_groupe
     await queryRunner.query(`
-      CREATE TABLE vigierh_profession_groupe (
-        numero_finess VARCHAR(9) NOT NULL,
-        annee INT NOT NULL,
-        mois INT NOT NULL,
-        quarter INT NOT NULL,
-        profession_code int NOT NULL,
-        effectif INT, 
-        effectif_filiere INT, 
-
-        PRIMARY KEY (numero_finess, annee, mois, profession_code)
+        CREATE TABLE vigierh_profession_groupe (
+          numero_finess VARCHAR(9) NOT NULL,
+          annee INT NOT NULL,
+          mois INT NOT NULL,
+          quarter INT NOT NULL,
+          profession_code INT NOT NULL,
+          effectif INT, 
+          effectif_filiere INT, 
+          PRIMARY KEY (numero_finess, annee, mois, profession_code),
+          CONSTRAINT fk_profession_groupe_profession FOREIGN KEY (profession_code) 
+          REFERENCES vigierh_ref_profession_groupe(code)
       );
-
-      ALTER TABLE vigierh_profession_groupe
-      ADD CONSTRAINT fk_profession_groupe_profession
-      FOREIGN KEY (profession_code)
-      REFERENCES vigierh_ref_profession_groupe(code);
     `);
 
     // TODO :  Ajout de l'index éventuellement
@@ -27,9 +23,6 @@ export class AjoutVigieRhProfessionGroupe1738328796095 implements MigrationInter
 
   async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-      ALTER TABLE vigierh_profession_groupe
-      DROP CONSTRAINT fk_profession_groupe_profession;
-
       DROP TABLE IF EXISTS vigierh_profession_groupe;  
     `);
   }
