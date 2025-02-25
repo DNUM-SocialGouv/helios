93664ed5804bce4ae8d56c0aed878cb6c21a9b7a
yelhouakmi <y.elhouakmi@cat-amania.com>
2025-02-25 17:10:44 +0100

    (hel-654) Montée de version Next (v14)

--- jest.setup.ts
+++ jest.setup.ts
@@ -3,3 +3,8 @@ import "@testing-library/jest-dom";
 // Cela permet de pouvoir tester ChartJS.
 jest.mock("react-chartjs-2", () => ({ Bar: () => null, Doughnut: () => null }));
 jest.mock("next/router", () => require("next-router-mock"));
+
+// Cela permet de faire des mock sur les appels « fetch ».
+// Sans ça, la methode fetch n’existe pas dans global et ne peut donc pas être mock
+// @ts-ignore
+global.fetch = () => { };
