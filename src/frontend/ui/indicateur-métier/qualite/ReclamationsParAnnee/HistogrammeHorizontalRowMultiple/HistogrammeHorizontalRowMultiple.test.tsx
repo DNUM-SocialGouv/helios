163b4a910828b2280fcc71ebae77a700d69b3434
yelhouakmi <y.elhouakmi@cat-amania.com>
2025-02-25 11:09:50 +0100

    (hel-654) Montée de version Jest

--- src/frontend/ui/indicateur-métier/qualite/ReclamationsParAnnee/HistogrammeHorizontalRowMultiple/HistogrammeHorizontalRowMultiple.test.tsx
+++ src/frontend/ui/indicateur-métier/qualite/ReclamationsParAnnee/HistogrammeHorizontalRowMultiple/HistogrammeHorizontalRowMultiple.test.tsx
@@ -2,7 +2,6 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 
-import '@testing-library/jest-dom/extend-expect';
 import { HistogrammeHorizontalRowMultiple } from './HistogrammeHorizontalRowMultiple';
 
 // Mock des styles pour éviter les erreurs liées aux modules CSS
