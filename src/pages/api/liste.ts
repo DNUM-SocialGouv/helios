d2f336879d49a437b5b31df82fb87dd25fb968da
yelhouakmi <y.elhouakmi@cat-amania.com>
2025-03-07 17:09:52 +0100

    (hel-669) Ajout de la limite de 11 listes côté back

--- src/pages/api/liste.ts
+++ src/pages/api/liste.ts
@@ -1,37 +1,44 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth";
 
-import { create, getAll } from "../../backend/infrastructure/controllers/userListEndpoint";
+import { create, getAll, countLists } from "../../backend/infrastructure/controllers/userListEndpoint";
 import { authOptions } from "./auth/[...nextauth]";
 
-export default async function handler(request: NextApiRequest, response: NextApiResponse) {
-    try {
-        const userSession = await getServerSession(request, response, authOptions);
-        const idUser = userSession?.user?.idUser;
+//Nombre max de list = 10 + favoris
+const MAX_LIST = 11;
 
-        if (idUser) {
-            if (request.method === "POST") {
-                return doCreate(request, response, idUser);
-            } else if (request.method === "GET") {
-                return doGetAll(response, idUser);
-            } else {
-                return response.status(405).send("Method not allowed");
-            }
-        } else {
-            return response.status(500)
-        }
-    } catch (error) {
-        return response.status(500);
+export default async function handler(request: NextApiRequest, response: NextApiResponse) {
+  try {
+    const userSession = await getServerSession(request, response, authOptions);
+    const idUser = userSession?.user?.idUser;
+
+    if (idUser) {
+      if (request.method === "POST") {
+        return doCreate(request, response, idUser);
+      } else if (request.method === "GET") {
+        return doGetAll(response, idUser);
+      } else {
+        return response.status(405).send("Method not allowed");
+      }
+    } else {
+      return response.status(500)
     }
+  } catch (error) {
+    return response.status(500);
+  }
 };
 
 async function doCreate(request: NextApiRequest, response: NextApiResponse, idUser: string) {
-    const { listName, isFavoris } = request.body;
-    create(idUser, listName, isFavoris);
-    return response.status(201).send("Created");
+  const { listName, isFavoris } = request.body;
+  const listCount = await countLists(idUser);
+  if (listCount >= MAX_LIST) {
+    return response.status(403).send("Forbidden");
+  }
+  create(idUser, listName, isFavoris);
+  return response.status(201).send("Created");
 }
 
 async function doGetAll(response: NextApiResponse, idUser: string) {
-    const allList = await getAll(idUser);
-    return response.status(200).json(allList);
+  const allList = await getAll(idUser);
+  return response.status(200).json(allList);
 }
