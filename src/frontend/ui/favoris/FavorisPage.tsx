dbb8b89b07115350cf348894f2f2b2fc10c3247c
yelhouakmi <y.elhouakmi@cat-amania.com>
2025-03-28 15:10:18 +0100

    (hel-667) Tri des listes dans la page « Mes listes » de la même manière que dans la popup d’ajout d’une liste

--- src/frontend/ui/favoris/FavorisPage.tsx
+++ src/frontend/ui/favoris/FavorisPage.tsx
@@ -1,10 +1,10 @@
 import { useContext, useEffect, useState } from "react";
 
+import styles from "./Favoris.module.css";
+import { FavorisBlock } from "./FavorisBlock";
 import { useDependencies } from "../commun/contexts/useDependencies";
 import { UserContext } from "../commun/contexts/userContext";
 import { UserListViewModel } from "../user-list/UserListViewModel";
-import styles from "./Favoris.module.css";
-import { FavorisBlock } from "./FavorisBlock";
 
 export const FavorisPage = () => {
   const { wording } = useDependencies();
@@ -13,17 +13,23 @@ export const FavorisPage = () => {
   const [sortedFavorisList, setSortedFavorisList] = useState(userContext?.favorisLists);
 
   useEffect(() => {
-    setSortedFavorisList(userContext?.favorisLists?.sort((a, b) =>
-      (b.isFavoris === a.isFavoris ? 0 : b.isFavoris ? 1 : -1)
-    ))
+    let list = userContext?.favorisLists.slice();
+    if (list) {
+      const favorisListIndex = list.findIndex((list) => list.isFavoris);
+      const favorisList = list.splice(favorisListIndex, 1);
+      list.sort((a: UserListViewModel, b: UserListViewModel) => new Date(a.dateCreation).getTime() - new Date(b.dateCreation).getTime());
+      list = favorisList.concat(...list);
+    }
+
+    setSortedFavorisList(list);
   }, [userContext?.favorisLists])
 
   return (
     <main className="fr-container" id="content">
       <h1 className={styles["title"]}>{wording.FAVORIS_LIST} ({userContext?.favorisLists?.length})</h1>
-      {sortedFavorisList?.map((etablissement: UserListViewModel, index: number) => (
-        <div key={index}>
-          <FavorisBlock currentListId={etablissement.id} favorisList={etablissement.userListEtablissements} title={etablissement.nom} />
+      {sortedFavorisList?.map((liste: UserListViewModel) => (
+        <div key={liste.id}>
+          <FavorisBlock currentListId={liste.id} favorisList={liste.userListEtablissements} title={liste.nom} />
         </div>
       ))}
     </main>
