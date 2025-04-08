d67b032bd4281e0b3755baf6fcfe537a983f22ec
yelhouakmi <y.elhouakmi@cat-amania.com>
2025-04-08 16:02:26 +0200

    Passage à la version 1.5.4

--- src/frontend/ui/commun/Footer/Footer.tsx
+++ src/frontend/ui/commun/Footer/Footer.tsx
@@ -111,7 +111,7 @@ export const Footer = () => {
               </a>
             </li>
             <li className="fr-footer__bottom-item">
-              <span className="fr-footer__bottom-link"> v1.5.3</span>
+              <span className="fr-footer__bottom-link"> v1.5.4</span>
             </li>
           </ul>
           <div className="fr-footer__bottom-copy">
