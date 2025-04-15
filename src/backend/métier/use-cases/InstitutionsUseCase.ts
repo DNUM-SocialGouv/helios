b71bc639b453e16da23831c8547d14f1bc1e4f80
schlaifa <s.chlaifa@cat-amania.com>
2025-04-15 16:34:59 +0200

    fix sonar issues

--- src/backend/métier/use-cases/InstitutionsUseCase.ts
+++ src/backend/métier/use-cases/InstitutionsUseCase.ts
@@ -2,7 +2,7 @@ import { InstitutionModel } from "../../../../database/models/InstitutionModel";
 import { InstitutionLoader } from "../gateways/InstitutionLoader";
 
 export class InstitutionsUseCase {
-  constructor(private institutionLoader: InstitutionLoader) {}
+  constructor(private readonly institutionLoader: InstitutionLoader) { }
 
   async getInstitutionByCode(code: string): Promise<InstitutionModel | null> {
     return await this.institutionLoader.getInstitutionByCode(code);
--- src/backend/métier/use-cases/changePasswordUseCase.ts
+++ src/backend/métier/use-cases/changePasswordUseCase.ts
@@ -1,7 +1,7 @@
 import { ChangePasswordLoader } from "../gateways/ChangePasswordLoader";
 
 export class ChangePasswordUseCase {
-  constructor(private changePasswordLoader: ChangePasswordLoader) { }
+  constructor(private readonly changePasswordLoader: ChangePasswordLoader) { }
 
   async exécute(loginToken: string, password: string): Promise<string> {
     return this.changePasswordLoader.updatePassword(loginToken, password, '');
