6f649df8746ba07bc41ac629ac3cf51805cfe5db
schlaifa <s.chlaifa@cat-amania.com>
2025-04-18 10:22:27 +0200

    (hel-676) fix telechargement pdf

--- src/frontend/ui/commun/IndicateurGraphique/BlocIndicateurVide.tsx
+++ src/frontend/ui/commun/IndicateurGraphique/BlocIndicateurVide.tsx
@@ -1,12 +1,16 @@
 import { Bloc } from "../Bloc/Bloc";
 import { useDependencies } from "../contexts/useDependencies";
 
-export function BlocIndicateurVide({ title, opnedBloc, toggelBlocs }: { title: string, opnedBloc?: boolean, toggelBlocs?: () => void }) {
-  const { wording } = useDependencies();
 
+
+export function BlocIndicateurVide({ title, opnedBloc, toggelBlocs }: Readonly<{ title: string, opnedBloc?: boolean, toggelBlocs?: () => void }>) {
+  const { wording } = useDependencies();
   return (
-    <Bloc  /* isExpandable={false} */ opnedBloc={opnedBloc} titre={title} toggelBlocs={toggelBlocs}>
-      {wording.INDICATEURS_VIDES}
+    <Bloc opnedBloc={opnedBloc} titre={title} toggelBlocs={toggelBlocs}>
+      <ul>
+        {wording.INDICATEURS_VIDES}
+      </ul>
+
     </Bloc>
   );
 }
