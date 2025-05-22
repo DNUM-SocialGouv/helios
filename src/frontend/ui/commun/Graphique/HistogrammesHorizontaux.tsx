d258b5a79dba8893476eb7d53a7e6fdf366b2694
yelhouakmi <y.elhouakmi@cat-amania.com>
2025-05-22 16:57:03 +0200

    (hel-803) Affichage de « 1 à 5 » dans l’histogramme du nombre de jours en urgence si la valeur est entre 1 et 5

--- src/frontend/ui/commun/Graphique/HistogrammesHorizontaux.tsx
+++ src/frontend/ui/commun/Graphique/HistogrammesHorizontaux.tsx
@@ -11,22 +11,36 @@ import { Transcription } from "../Transcription/Transcription";
 
 type Stack = { label?: string; data: number[]; backgroundColor: string[]; isError?: boolean[] };
 
-function useChartData(charts: HistogrammeData[]) {
+function useChartData(charts: HistogrammeData[], cacheLesValeursBasse?: boolean) {
   const [chartsData, setChartsData] = useState(charts);
   useEffect(() => setChartsData(charts), charts);
 
 
+
+  const hideLowValueFormatter = (value: number, _context: Context): any => {
+    if (cacheLesValeursBasse && value > 0 && value <= 5) {
+      return "1 à 5";
+    }
+    return value;
+  };
+
   return {
-    histogrammes: chartsData.map((chartData) => ({
-      transcriptionTitles: chartData.transcriptionTitles,
-      chartData: chartData.chartData,
-      optionsHistogramme: chartData.optionsHistogramme,
-      legendColors: chartData.legendColors,
-      labels: chartData.labels,
-      transcriptionsValeurs: chartData.transcriptionValeurs,
-      nom: chartData.nom,
-      areStacksVisible: chartData.areStacksVisible,
-    })),
+    histogrammes: chartsData.map((chartData) => {
+      const inputOptions = chartData.optionsHistogramme;
+      inputOptions.plugins.datalabels.formatter = hideLowValueFormatter;
+
+      return ({
+        transcriptionTitles: chartData.transcriptionTitles,
+        chartData: chartData.chartData,
+        optionsHistogramme: cacheLesValeursBasse ? inputOptions : chartData.optionsHistogramme,
+        legendColors: chartData.legendColors,
+        labels: chartData.labels,
+        transcriptionsValeurs: chartData.transcriptionValeurs,
+        nom: chartData.nom,
+        areStacksVisible: chartData.areStacksVisible,
+      })
+    }
+    ),
     toggleStackVisibility: (stackIndex: number, isVisible: boolean) => {
       setChartsData((chartsData): HistogrammeData[] => {
         chartsData.forEach((chart) => chart.toggleStack(stackIndex, isVisible));
@@ -186,6 +200,7 @@ type HistogrammeHorizontalNewProps = {
   légende?: string[];
   epaisseur?: "FIN" | "EPAIS";
   identifiant?: string;
+  cacheLesValeursBasse?: boolean;
 };
 export const HistogrammesHorizontaux = ({
   nom,
@@ -195,9 +210,10 @@ export const HistogrammesHorizontaux = ({
   légende,
   identifiant,
   epaisseur = "EPAIS",
+  cacheLesValeursBasse
 }: HistogrammeHorizontalNewProps): ReactElement => {
   const { wording } = useDependencies();
-  const { histogrammes, toggleStackVisibility } = useChartData(valeursDesHistogrammes);
+  const { histogrammes, toggleStackVisibility } = useChartData(valeursDesHistogrammes, cacheLesValeursBasse);
 
   function transcriptionTitles(): string[] {
     return histogrammes.map((histogramme) => histogramme.transcriptionTitles).flat() as string[];
