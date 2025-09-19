bbca0a295463de35109038f95b834c6d10a86842
schlaifa <s.chlaifa@cat-amania.com>
2025-09-19 14:53:40 +0200

    (vr-30) fix python unit tests

--- datacrawler/extract/lecteur_parquet_test.py
+++ datacrawler/extract/lecteur_parquet_test.py
@@ -2,15 +2,16 @@ import pandas as pd
 
 from datacrawler.extract.lecteur_parquet import lis_le_fichier_parquet
 from datacrawler.test_helpers.config_path import get_absolute_file_path
-from datacrawler.transform.equivalence_vigierh_helios import ColumMapping
-
 class TestLisLeFichierParquet:
     def test_lis_le_fichier_ref_masque_parquet(self) -> None:
         # GIVEN
         chemin_du_fichier = get_absolute_file_path("data_test/entrée/vigie_rh/vigierh_ref_tranche_age_2025_02_13.parquet")
 
         # WHEN
-        donnees = lis_le_fichier_parquet(chemin_du_fichier, ColumMapping.REF_TRANCHE_AGE.value)
+        donnees = lis_le_fichier_parquet(chemin_du_fichier, {
+        'tranche_code': 'code_tranche_age',
+        'tranche_age': 'tranche_age'
+    })
 
         # THEN
         pd.testing.assert_frame_equal(
