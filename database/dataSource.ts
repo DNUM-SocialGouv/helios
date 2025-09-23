f9373237daa5d844593e4b67f528271476b8f6c3
Mohamed MOUSSI <m.moussi@catamania.com>
2025-09-23 15:42:31 +0200

    Suppression de fichier dupliqué AjoutDonneesMensuellesPsy1756720758341

--- database/dataSource.ts
+++ database/dataSource.ts
@@ -323,7 +323,6 @@ const datasource = new DataSource({
     AjoutTableVigieRhMouvementsRH1757337280336,
     AjoutTableVigieRhMouvementsTrimestriels1757662996841,
     SupressionColonneEffectifDePyramideAges1758270710076,
-    AjoutDonneesMensuellesPsy1756720758341,
     AjoutTableRessourcesHumainesEJ1757946729822,
     ModificationValeurProfilAjoutBlocRhEj1758098318274
   ],
