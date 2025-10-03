0aebd8f4c7a3c8466c3b2a6a7039ecd45db867b1
yelhouakmi <y.elhouakmi@cat-amania.com>
2025-10-03 15:38:09 +0200

    Hel 887/import changement message erreur (#850)
    * (hel-887) Correction du message d’erreur d’import
    
    * (hel-887) Correction d’un clignottement lors de la validation des finess

--- src/frontend/ui/liste/ImportListModal.tsx
+++ src/frontend/ui/liste/ImportListModal.tsx
@@ -36,7 +36,6 @@ export const ImportListModal = ({ onSuccess, listId }: ImportListModalProps) => 
 
   const importEts = () => {
     const finessList = finessAImporter.split(/\r?\n/).filter(ligne => ligne.trim() !== "");
-    setFinessEnErreur([]);
 
     if (finessList && finessList.length > 0) {
       fetch("/api/recherche-par-finess", {
@@ -63,6 +62,8 @@ export const ImportListModal = ({ onSuccess, listId }: ImportListModalProps) => 
           setFinessEnErreur(finessKo);
           setFinessAImporter(finessKo.join("\n"))
         });
+    } else {
+      setFinessEnErreur([]);
     }
   }
 
