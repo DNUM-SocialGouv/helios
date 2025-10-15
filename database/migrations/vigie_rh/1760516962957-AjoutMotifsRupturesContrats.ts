0935119ae50929d2cdc81ac1af9c3aaf501713ae
Amine Aouffen <10002061+amofen@users.noreply.github.com>
2025-10-15 11:52:01 +0200

    correction des contraintes de la table

--- database/migrations/vigie_rh/1760516962957-AjoutMotifsRupturesContrats.ts
+++ database/migrations/vigie_rh/1760516962957-AjoutMotifsRupturesContrats.ts
@@ -5,13 +5,19 @@ export class AjoutMotifsRupturesContrats1760516962957 implements MigrationInterf
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
       CREATE TABLE IF NOT EXISTS vigierh_motifs_ruptures(
-              finess_et varchar PRIMARY KEY ,
-              annee int NOT NULL ,
-              trimestre int NOT NULL,
-              motif_code int REFERENCES vigierh_ref_motifs_ruptures(code),
-              effectif int,
-              effectif_ref int
-              );`);
+          finess_et varchar NOT NULL,
+          annee int NOT NULL ,
+          trimestre int NOT NULL,
+          motif_code int NOT NULL,
+          effectif int,
+          effectif_ref int,
+          PRIMARY KEY (finess_et,annee,trimestre,motif_code),
+          FOREIGN KEY (motif_code)
+                        REFERENCES vigierh_ref_motifs_ruptures (code)
+                        ON DELETE CASCADE,
+          FOREIGN KEY (finess_et)
+                        REFERENCES etablissement_territorial (numero_finess_etablissement_territorial)
+                        ON DELETE CASCADE);`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
