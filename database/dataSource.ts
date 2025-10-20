d6763bb5f2b9593f3f1d1dd6df0cb7e213a98047
schlaifa <s.chlaifa@cat-amania.com>
2025-10-20 15:48:04 +0200

    (vr-90) creation des tables de nature des contrats

--- database/dataSource.ts
+++ database/dataSource.ts
@@ -104,6 +104,7 @@ import { SupressionColonneEffectifDePyramideAges1758270710076 } from "./migratio
 import { SuppressionEffectifEtabDeVigieRhProfessionFiliere1758292560238 } from "./migrations/vigie_rh/1758292560238-SuppressionEffectifEtabDeVigieRhProfessionFiliere";
 import { AjoutTableVigieRhDureeCdd1759821521368 } from "./migrations/vigie_rh/1759821521368-AjoutTableVigieRhDureeCdd";
 import { AjoutMotifsRupturesContrats1760516962957 } from "./migrations/vigie_rh/1760516962957-AjoutMotifsRupturesContrats";
+import { VigierhNatureContratsTables1760966102359 } from "./migrations/vigie_rh/1760966102359-vigierhNatureContratsTables";
 import { AjoutReferentielTypeContrat1738227384991 } from "./migrations/vigie_rh/referentiels/1738227384991-AjoutReferentielTypeContrat";
 import { AjoutReferentielProfessionFiliere1738328597314 } from "./migrations/vigie_rh/referentiels/1738328597314-AjoutReferentielProfessionFiliere";
 import { AjoutReferentielProfessionGroupe1738328713502 } from "./migrations/vigie_rh/referentiels/1738328713502-AjoutReferentielProfessionGroupe";
@@ -350,7 +351,8 @@ const datasource = new DataSource({
     AjoutReferentielMotifsRupturesContrats1760516933359,
     AjoutMotifsRupturesContrats1760516962957,
     AjoutTableVigieRhDureeCdd1759821521368,
-    AjoutTableVigieRhRefDureeCdd1759821492361
+    AjoutTableVigieRhRefDureeCdd1759821492361,
+    VigierhNatureContratsTables1760966102359
   ],
   type: "postgres",
   url: environmentVariables.DATABASE_URL,
