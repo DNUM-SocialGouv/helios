f3d34e0e5683347458be83473a37a5021b879f93
yelhouakmi <y.elhouakmi@cat-amania.com>
2025-10-24 14:18:57 +0200

    (hel-897) Modification du detail des autres activités

--- src/frontend/ui/établissement-territorial-sanitaire/InfoBulle/ContenuAutresActivités.tsx
+++ src/frontend/ui/établissement-territorial-sanitaire/InfoBulle/ContenuAutresActivités.tsx
@@ -16,12 +16,12 @@ export const ContenuAutresActivités = ({ dateDeMiseÀJour, source, estEntitéJu
       <p>{wording.miseÀJourEtSource(dateDeMiseÀJour, source)}</p>
       <section aria-label={wording.ÉLÉMENTS_DE_COMPRÉHENSION}>
         <p>
-          Seules les autorisations non caduques sont transmises à FINESS par ARHGOS et sont donc publiées. Hormis les autorisations en attente de première mise
-          en œuvre, toutes les autorisations publiées peuvent donc être considérées comme actives. Dans le domaine sanitaire les autorisations sont données à
-          une entité juridique puis mises en oeuvre dans un établissement. Pour indiquer les services offerts, FINESS identifie ceux-ci à l’aide d’un triplet
-          composé des éléments suivants : Activité/Modalité/Forme. Les autres activités de soins sont soumises à l’autorisation du Directeur Général (DG) de
-          l’ARS selon des modalités et des durées différentes particulières.
+          Une autorisation d’autre activité de soins est une décision administrative,
+          délivrée par arrêté du Directeur général de l’Agence régionale de santé,
+          permettant à un établissement de santé d’exercer ces autres activités (chirurgie esthétique, dépôt de sang, lactarium, etc.).
+          Ces autres activités ne relèvent pas du régime juridique des autorisations d’activités de soins et sont encadrées par des textes spécifiques du Code de la santé publique.
         </p>
+        <p>Les autres activités de soins sont composées de trois niveaux : Autre activité / Modalité / Forme.</p>
       </section>
       <section aria-label={wording.FRÉQUENCE}>
         <p>
@@ -31,8 +31,7 @@ export const ContenuAutresActivités = ({ dateDeMiseÀJour, source, estEntitéJu
       {estEntitéJuridique && (
         <section aria-label={wording.MODE_DE_CALCUL}>
           <p>
-            <span className="fr-text--bold">Mode de calcul :</span> Les autorisations et capacités affichées au niveau de l’entité juridique (EJ) correspondent à l’ensemble des autorisations et capacités des
-            établissements géographiques sanitaires rattachés à l’EJ.
+            <span className="fr-text--bold">Mode de calcul :</span> Les autorisations affichées au niveau de l’entité juridique (EJ) correspondent à l’ensemble des autorisations et capacités des établissements géographiques sanitaires rattachés à l’EJ.
           </p>
         </section>
       )}
@@ -43,10 +42,9 @@ export const ContenuAutresActivités = ({ dateDeMiseÀJour, source, estEntitéJu
       </section>
       <section aria-label={wording.INFOS_COMPLÉMENTAIRES}>
         <p>
-          Ces données prennent leur source dans le SI agence régionale hospitalière gestion des objectifs sanitaire (ARHGOS) et sont reprises dans le fichier
-          national des établissements sanitaires et sociaux (FINESS). ARHGOS est l’outil de gestion des autorisations des activités de soins et des équipements
-          matériels lourds (EML) pour les établissements sanitaires, ainsi que des activités soumises à reconnaissance contractuelle et des autres activités
-          (pharmacies à usage intérieur, chirurgie esthétique, prélèvements de cellules, tissus et organes…).
+          Ces données prennent leur source dans ARHGOS et sont reprises dans le fichier national des établissements sanitaires et sociaux (FINESS).
+          <br />
+          ARHGOS est l’outil de gestion des reconnaissances contractuelles, des autres activités de soins ainsi que des pharmacies à usage intérieur (PUI).
         </p>
       </section>
     </>
