fe5f1a77886aa5968562b3097f92ddee0b12115e
yelhouakmi <y.elhouakmi@cat-amania.com>
2025-11-24 16:47:53 +0100

    (hel-930) Rewording «Numéro ARHGOS» en «Numéro autorisation» pour les établissements sanitaire

--- src/frontend/configuration/wording/Wording.tsx
+++ src/frontend/configuration/wording/Wording.tsx
@@ -272,6 +272,7 @@ export interface Wording {
   readonly DATE_DE_FIN_CPOM: ReactElement;
   readonly NUMÉRO_CPOM: ReactElement;
   readonly NUMÉRO_ARHGOS: string;
+  readonly NUMÉRO_AUTORISATION: string;
   readonly ACTIVITÉS: string;
   readonly CHIRURGIE: string;
   readonly MÉDECINE: string;
