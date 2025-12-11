27e884e7f130968a45c2a89aa0a7665c7fa36386
yelhouakmi <y.elhouakmi@cat-amania.com>
2025-12-11 15:55:10 +0100

    (hel-949) Ajout du nouveau message (#947)

--- src/frontend/ui/commun/NewFeaturesNotice/NewFeaturesNotice.tsx
+++ src/frontend/ui/commun/NewFeaturesNotice/NewFeaturesNotice.tsx
@@ -14,7 +14,7 @@ export const NewFeaturesNotice = () => {
           <div className="fr-notice__body">
             <p>
               <span className="fr-notice__title">
-                <span className={styles["notice-badge"] + " fr-badge fr-badge--new fr-badge--no-icon"}>{wording.NOUVEAU}</span>
+                <span className={styles["notice-badge"] + " fr-badge fr-badge--new fr-badge--no-icon"}>{wording.NOUVELLES_FONCTIONNALITÉS_TAG}</span>
               </span>
               <span className="fr-notice__desc">{wording.NOUVELLES_FONCTIONNALITÉS_TEXT}</span>
 
@@ -24,7 +24,7 @@ export const NewFeaturesNotice = () => {
                 target="_blank"
                 title='En savoir plus'
               >
-                En savoir plus
+                {wording.NOUVELLES_FONCTIONNALITÉS_TEXT_LIEN}
               </a>
             </p>
             <button className="fr-btn--close fr-btn" onClick={() => setRemoveNotice(true)} title="Masquer le message" />
