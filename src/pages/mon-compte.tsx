9d75a23fcf314ac10d5f60ff7af50ad65aad5cec
yelhouakmi <y.elhouakmi@cat-amania.com>
2026-01-07 17:45:53 +0100

    (hel-959) Correction de la page « mon compte »

--- src/pages/mon-compte.tsx
+++ src/pages/mon-compte.tsx
@@ -1,15 +1,24 @@
+import Head from "next/head";
+
 import { useDependencies } from "../frontend/ui/commun/contexts/useDependencies";
 import { useBreadcrumb } from "../frontend/ui/commun/hooks/useBreadcrumb";
 import { ProfilePage } from "../frontend/ui/profile/ProfilePage";
 
 export default function Profile() {
-    const { wording } = useDependencies();
-    useBreadcrumb([
-        {
-            label: wording.MON_COMPTE,
-            path: "",
-        },
-    ]);
+  const { wording } = useDependencies();
+  useBreadcrumb([
+    {
+      label: wording.MON_COMPTE,
+      path: "",
+    },
+  ]);
 
-    return <ProfilePage />;
+  return (
+    <main className="fr-container" id="content">
+      <Head>
+        <title>{wording.MON_COMPTE}</title>
+      </Head>
+      <ProfilePage />
+    </main>
+  );
 }
