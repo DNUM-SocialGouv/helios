959d6e57915e9de7509506df6422f7a25dd7e9af
schlaifa <s.chlaifa@cat-amania.com>
2026-01-08 15:00:45 +0100

    version 1.7.7

--- src/frontend/ui/commun/Footer/Footer.tsx
+++ src/frontend/ui/commun/Footer/Footer.tsx
@@ -116,7 +116,7 @@ export const Footer = () => {
               </a>
             </li>
             <li className="fr-footer__bottom-item">
-              <span className="fr-footer__bottom-link"> v1.7.6</span>
+              <span className="fr-footer__bottom-link"> v1.7.7</span>
             </li>
           </ul>
           <div className="fr-footer__bottom-copy">
