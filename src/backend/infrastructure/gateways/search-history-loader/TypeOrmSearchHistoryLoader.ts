a0f8251745b0b5906656742dde2431c4dda36873
yelhouakmi <y.elhouakmi@cat-amania.com>
2026-02-26 11:01:15 +0100

    (hel-990) Modification du code de suppression des ancienne entrée de l’historique pour prendre en compte les cas ou il y a plus de 10 résultats

--- src/backend/infrastructure/gateways/search-history-loader/TypeOrmSearchHistoryLoader.ts
+++ src/backend/infrastructure/gateways/search-history-loader/TypeOrmSearchHistoryLoader.ts
@@ -4,35 +4,37 @@ import { SearchHistoryModel } from "../../../../../database/models/SearchHistory
 import { SearchHistoryLoader } from "../../../métier/gateways/SearchHistoryLoader";
 
 export class TypeOrmSearchHistoryLoader implements SearchHistoryLoader {
-    constructor(private readonly orm: Promise<DataSource>) { }
+  constructor(private readonly orm: Promise<DataSource>) { }
 
 
-    async saveSearchHistory(title: string, idUser: string, finessNumber: string, type: string) {
-        const history = await (await this.orm).getRepository(SearchHistoryModel).findOne({ where: { userId: idUser, finessNumber: finessNumber } });
-        if (history) {
-            await (await this.orm).getRepository(SearchHistoryModel).update(history.id, { date: new Date() });
-        } else {
-            // make sure to keep only the last 10 search
-            const count = await (await this.orm).getRepository(SearchHistoryModel).countBy({ userId: idUser });
-            const searchHistory = new SearchHistoryModel();
-            searchHistory.date = new Date();
-            searchHistory.finessNumber = finessNumber;
-            searchHistory.title = title;
-            searchHistory.userId = idUser;
-            searchHistory.type = type;
-            if (count === 10) {
-                // delete the oldest line
-                const oldestElement = await (await this.orm).getRepository(SearchHistoryModel).findOne({
-                    where: { userId: idUser },
-                    order: { date: 'ASC' },
-                });
-                if (oldestElement) await (await this.orm).getRepository(SearchHistoryModel).remove(oldestElement);
-            }
-            await (await this.orm).getRepository(SearchHistoryModel).save(searchHistory);
-        }
+  async saveSearchHistory(title: string, idUser: string, finessNumber: string, type: string) {
+
+    const ds = await this.orm;
+    const repo = (await this.orm).getRepository(SearchHistoryModel);
+    const history = await repo.findOne({ where: { userId: idUser, finessNumber: finessNumber } });
+    if (history) {
+      await repo.update(history.id, { date: new Date() });
+    } else {
+      const searchHistory = new SearchHistoryModel();
+      searchHistory.date = new Date();
+      searchHistory.finessNumber = finessNumber;
+      searchHistory.title = title;
+      searchHistory.userId = idUser;
+      searchHistory.type = type;
+      await repo.save(searchHistory);
     }
 
-    async getAllUserSearchHistory(idUser: string) {
-        return await (await this.orm).getRepository(SearchHistoryModel).find({ where: { userId: idUser }, order: { date: 'DESC' } });
-    }
+    // Supprime les 10 entrées les plus anciennes
+    await ds.query(
+      `DELETE FROM search_history WHERE id IN (
+           SELECT id FROM search_history WHERE user_id = $1 ORDER BY date DESC, id DESC OFFSET 10
+         )`,
+      [idUser]
+    );
+
+  }
+
+  async getAllUserSearchHistory(idUser: string) {
+    return await (await this.orm).getRepository(SearchHistoryModel).find({ where: { userId: idUser }, order: { date: 'DESC' } });
+  }
 }
