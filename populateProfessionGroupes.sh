d91667fd670042c377d4d30c08616767690f36e6
yelhouakmi <y.elhouakmi@cat-amania.com>
2026-03-02 16:44:56 +0100

    Changement des shell d’appel des batch pour un appel direct à python sans passer par yarn

--- populateProfessionGroupes.sh
+++ populateProfessionGroupes.sh
@@ -4,4 +4,5 @@ echo "$SSH_KEY" | base64 --decode >$SFTP_PRIVATE_KEY
 echo "$DIAMANT_PRIVATE_KEY" | base64 --decode | gpg --import
 echo "$SFTP_DNUM_KEY" | base64 --decode >$DNUM_SFTP_PRIVATE_KEY
 
-yarn populateProfessionGroupes
+python -m datacrawler.download_vigierh_data &&
+	python -m datacrawler.import_vigie_rh_profession_groupe
