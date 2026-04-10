d7e295a8748bc4f244f73fd2d51bad1ff3d2e231
schlaifa <136464784+schlaifa@users.noreply.github.com>
2026-04-10 14:07:23 +0200

    fix les retours demo (#1086)

--- src/frontend/ui/établissement-territorial-médico-social/bloc-ressources-humaines/bloc-vigie-rh/info-bulles/ContenuTopEffectifVigieRh.tsx
+++ src/frontend/ui/établissement-territorial-médico-social/bloc-ressources-humaines/bloc-vigie-rh/info-bulles/ContenuTopEffectifVigieRh.tsx
@@ -1,4 +1,6 @@
 import { ContenuVigieRhInfoBulleProps } from "./ContenuVigieRhInfoBulleProps";
+import styles from "./InfoBulles.module.css";
+
 import { useDependencies } from "../../../../commun/contexts/useDependencies";
 
 export const ContenuTopEffectifVigieRh = ({ dateDeMiseAJour, dateDonneesArretees, source }: ContenuVigieRhInfoBulleProps) => {
@@ -18,6 +20,69 @@ export const ContenuTopEffectifVigieRh = ({ dateDeMiseAJour, dateDonneesArretees
           physique. De la même manière, un salarié ou agent ayant effectué plusieurs contrats dans   
           l’établissement sur le mois est compté comme 1 personne physique.  
         </p>
+        <div className="fr-table fr-table--layout-fixed fr-table--no-scroll">
+          <div className={`fr-table--caption-bottom fr-mb-2w ${styles["centredCaption"]}`}>
+            Découpage en filières et catégories de profession
+          </div>
+          <div className="fr-table__wrapper">
+            <div className="fr-table__container">
+              <div className="fr-table__content">
+                <table>
+                  <thead>
+                    <tr>
+                      <th scope="col">
+                        Filière
+                      </th>
+                      <th className="fr-col-8" scope="col">
+                        Catégories de professions concernées
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>
+                        Médical
+                      </td>
+                      <td>
+                        Médecins hospitaliers . Médecins non hospitaliers . Sage-femmes .
+                        Pharmaciens . Dentistes  . Internes
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        Soins
+                      </td>
+                      <td>
+                        Infirmières . Rééducation . Techniciens médicaux .
+                        Psychologues . Aides-soignantes et auxiliaires de puériculture .
+                        Autres auxiliaires de soins . Préparateurs en pharmacie et
+                        spécialistes de l’appareillage médical
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        Social & Éducatif
+                      </td>
+                      <td>
+                        Intervention sociale et éducative . Moniteurs-éducateurs (social et sport) .
+                        Aide à la personne . Animation socio-culturelle . Education et formation
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        Administration/Technique/Logistique
+                      </td>
+                      <td>
+                        Dirigeants et cadres . Agents des services hospitaliers .
+                        Intendance . Technique . Administratif . Logistique . Autres
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
       <section aria-label={wording.INDICATION_VARIATION}>
         <p>
