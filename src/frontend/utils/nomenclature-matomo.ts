1867365fb58e39939ef065d6fc8167e063e888fc
Sana CHLAIFA <s.chlaifa@cat-amania.com>
2026-06-30 11:39:45 +0200

    (hel-1051) implemenation evenements recherche avancee (#1141)
    * (hel-1051) implemenation evenements recherche avancee
    
    * (hel-1051) update changelog
    
    * (hel-1051) ne pas comptabiliser les recherches avancee lancee a partir de la comparaison dans les evenements matomo

--- src/frontend/utils/nomenclature-matomo.ts
+++ src/frontend/utils/nomenclature-matomo.ts
@@ -16,5 +16,22 @@ export const AIDE = {category: 'navigation', action: 'aide'};
 // Recherche 
 export const RECHERCHE_SIMPLE = {category: 'recherche', action: 'simple'};
 
+/** Recheche avancée */
+export const RECHERCHE_REINITIALISER = {category: 'recherche', action: 'reinitialiser'};
+export const RECHERCHE_AVANCEE = {category: 'recherche', action: 'avancee'};
+export const RECHERCHE_AVANCEE_FILTRE_GEO = {category: 'filtre', action: 'appliquer', name: 'zone geo'};
+export const RECHERCHE_AVANCEE_FILTRE_STRUCTURE = {category: 'filtre', action: 'appliquer', name: 'type structure'};
+export const RECHERCHE_AVANCEE_FILTRE_CATEGORIES = {category: 'filtre', action: 'appliquer', name: 'categorie finess'};
+export const RECHERCHE_AVANCEE_FILTRE_CAPACITE = {category: 'filtre', action: 'appliquer', name: 'capacites'};
+export const RECHERCHE_AVANCEE_FILTRE_ACTIVITE = {category: 'filtre', action: 'appliquer', name: 'activites'};
+/** Filtre structure */
+export const FILTRE_STRUCTURE_EJ = {category: 'filtre', action: 'valeur_type_structure', name: 'ej'};
+export const FILTRE_STRUCTURE_SAN = {category: 'filtre', action: 'valeur_type_structure', name: 'sanitaire'};
+export const FILTRE_STRUCTURE_MS = {category: 'filtre', action: 'valeur_type_structure', name: 'medico_social'};
+/** Statut juridique */ 
+export const FILTRE_STATUT_JURIDIQUE_PUBLIC = {category: 'filtre', action: 'valeur_statut_juridique', name: 'public'};
+export const FILTRE_STATUT_JURIDIQUE_PL= {category: 'filtre', action: 'valeur_statut_juridique', name: 'prive_lucratif'};
+export const FILTRE_STATUT_JURIDIQUE_PNL = {category: 'filtre', action: 'valeur_statut_juridique', name: 'prive_non_lucratif'};
+
 // création de liste
 export const CREATION_LISTE = {category: 'liste', action: 'creer'};
