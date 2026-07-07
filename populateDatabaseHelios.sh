3b4bb772f9b59555d681c205c415cc5461e29bb2
s.chlaifa <s.chlaifa@cat-amania.com>
2026-07-07 17:07:32 +0200

    (hel-1048) ajustement batch prod

--- populateDatabaseHelios.sh
+++ populateDatabaseHelios.sh
@@ -15,7 +15,6 @@ python -m datacrawler.download_finess_data &&
 	python -m datacrawler.import_activites_mensuel &&
 	python -m datacrawler.import_activites_mensuel_entite_juridique &&
 	python -m datacrawler.ajoute_les_autorisations_des_établissements_médico_sociaux &&
-	python -m datacrawler.ajoute_les_autorisations_des_établissements_sanitaires &&
 	python -m datacrawler.ajoute_le_bloc_budget_et_finances_des_établissements_médico_sociaux &&
 	python -m datacrawler.ajoute_le_bloc_ressources_humaines_des_établissements_médico_sociaux &&
 	python -m datacrawler.agrégation_activité_sanitaire &&
@@ -33,4 +32,5 @@ python -m datacrawler.download_finess_data &&
 	python -m datacrawler.ajoute_le_bloc_ressources_humaines_des_entites_juridiques &&
 	python -m datacrawler.ajoute_le_bloc_ressources_humaines_des_etablissements_sanitaires &&
 	python -m datacrawler.import_les_donnees_qualite_has &&
-	python -m datacrawler.import_les_ms_pas_qualite_has
+	python -m datacrawler.import_les_ms_pas_qualite_has &&
+	python -m datacrawler.ajoute_les_autorisations_des_établissements_sanitaires
